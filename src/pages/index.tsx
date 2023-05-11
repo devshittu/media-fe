c582ed9c2f72c51e4200282281ce0bf8d208f4d9
Muhammed Shittu <devshittu@gmail.com>
2023-05-11 21:40:48 +0100

    installed and setup the tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
+import Head from 'next/head';
 // import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -16,7 +16,8 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <h1>Hello Media</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </>
-  )
+  );
 }
