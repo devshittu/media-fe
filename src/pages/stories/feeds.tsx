215f69aed6e38b76b05dbe63bd710fa09c931b83
Muhammed Shittu <devshittu@gmail.com>
2023-05-18 03:38:42 +0100

    Add right nav to feeds

--- src/pages/stories/feeds.tsx
+++ src/pages/stories/feeds.tsx
@@ -337,209 +337,131 @@ const Feeds = () => {
             </div>
           </div>
           <div className={feedsStyles.feeds_sidenav}>
-            <aside className={styles.draft_sidenav}>
-              <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                <ul className="space-y-2 font-medium">
-                  <li>
-                    <Link
-                      href="#"
-                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <svg
-                        aria-hidden="true"
-                        className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                        <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                      </svg>
-                      <span className="ml-3">Dashboard</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <svg
-                        aria-hidden="true"
-                        className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-                      </svg>
-                      <span className="flex-1 ml-3 whitespace-nowrap">
-                        Kanban
-                      </span>
-                      <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                        Pro
-                      </span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <svg
-                        aria-hidden="true"
-                        className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
-                        <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
-                      </svg>
-                      <span className="flex-1 ml-3 whitespace-nowrap">
-                        Inbox
-                      </span>
-                      <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                        3
-                      </span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <svg
-                        aria-hidden="true"
-                        className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                      <span className="flex-1 ml-3 whitespace-nowrap">
-                        Users
-                      </span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <svg
-                        aria-hidden="true"
-                        className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                      <span className="flex-1 ml-3 whitespace-nowrap">
-                        Products
-                      </span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <svg
-                        aria-hidden="true"
-                        className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                      <span className="flex-1 ml-3 whitespace-nowrap">
-                        Sign In
-                      </span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <svg
-                        aria-hidden="true"
-                        className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                      <span className="flex-1 ml-3 whitespace-nowrap">
-                        Sign Up
-                      </span>
-                    </Link>
-                  </li>
-                </ul>
-                <div
-                  id="dropdown-cta"
-                  className="p-4 mt-6 rounded-lg bg-blue-50 dark:bg-blue-900"
-                  role="alert"
-                >
-                  <div className="flex items-center mb-3">
-                    <span className="bg-orange-100 text-orange-800 text-sm font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-orange-200 dark:text-orange-900">
-                      Beta
-                    </span>
-                    <button
-                      type="button"
-                      className="ml-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-900 rounded-lg focus:ring-2 focus:ring-blue-400 p-1 hover:bg-blue-200 inline-flex h-6 w-6 dark:bg-blue-900 dark:text-blue-400 dark:hover:bg-blue-800"
-                      data-dismiss-target="#dropdown-cta"
-                      aria-label="Close"
-                    >
-                      <span className="sr-only">Close</span>
-                      <svg
-                        aria-hidden="true"
-                        className="w-4 h-4"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </button>
-                  </div>
-                  <p className="mb-3 text-sm text-blue-800 dark:text-blue-400">
-                    Preview the new Flowbite dashboard navigation! You can turn
-                    the new navigation off for a limited time in your profile.
-                  </p>
-                  <Link
-                    className="text-sm text-blue-800 underline font-medium hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-300"
-                    href="#"
-                  >
-                    Turn new navigation off
-                  </Link>
+            <div className="flex-1 py-4x pl-10x pr-28x space-y-4 bg-white hidden md:block">
+              <div className="sticky top-0 bg-white pb-2">
+                <input
+                  type="text"
+                  className="block text-black border border-grey-light bg-gray-200 w-full py-2 pl-3 rounded-3xl"
+                  name="fullname"
+                  placeholder="Search Twiiter"
+                />
+              </div>
+              <div className="">
+                <div className="block bg-gray-50 py-2  px-2 rounded-2xl shadow-sm">
+                  <p className="font-extrabold text-lg">Trends for you</p>
                 </div>
               </div>
-            </aside>
+              <div className="block bg-gray-50 py-2  sticky top-16 px-2 rounded-2xl shadow-sm">
+                <div className="border-b py-2">
+                  <h1 className="font-extrabold text-lg">Who to follow</h1>
+                </div>
+                <div className=" blockx border-b items-center justify-between flex group py-4 px-2 mr-6 text-gray-700  hover:text-blue-400">
+                  <div className="flex space-x-4">
+                    <Image
+                      width="48"
+                      height="48"
+                      className="rounded-full"
+                      src="https://pbs.twimg.com/profile_images/1005827060617695233/XiH6yzF5_normal.jpg"
+                      alt=""
+                      srcset=""
+                    />
+                    <div>
+                      <p className="font-semibold text-gray-800">MaseruMetro</p>
+                      <span className="text-gray-600">@MaseruMetro</span>
+                    </div>
+                  </div>
+
+                  <div>
+                    <Link
+                      href="/"
+                      className="inline-block border border-blue-400 text-blue-400 py-1 px-4 rounded-full font-bold"
+                    >
+                      Follow
+                    </Link>
+                  </div>
+                </div>
+                <div className=" block border-b items-center justify-between flex group py-4 px-2 mr-6 text-gray-700  hover:text-blue-400">
+                  <div className="flex space-x-4">
+                    <Image
+                      width="48"
+                      height="48"
+                      className="rounded-full"
+                      src="https://pbs.twimg.com/profile_images/1005827060617695233/XiH6yzF5_normal.jpg"
+                      alt=""
+                      srcset=""
+                    />
+                    <div>
+                      <p className="font-semibold text-gray-800">MaseruMetro</p>
+                      <span className="text-gray-600">@MaseruMetro</span>
+                    </div>
+                  </div>
+
+                  <div>
+                    <Link
+                      href="/"
+                      className="inline-block border border-blue-400 text-blue-400 py-1 px-4 rounded-full font-bold"
+                    >
+                      Follow
+                    </Link>
+                  </div>
+                </div>
+                <div className=" block border-b items-center justify-between flex group py-4 px-2 mr-6 text-gray-700  hover:text-blue-400">
+                  <div className="flex space-x-4">
+                    <Image
+                      width="48"
+                      height="48"
+                      className="rounded-full"
+                      src="https://pbs.twimg.com/profile_images/1005827060617695233/XiH6yzF5_normal.jpg"
+                      alt=""
+                      srcset=""
+                    />
+                    <div>
+                      <p className="font-semibold text-gray-800">MaseruMetro</p>
+                      <span className="text-gray-600">@MaseruMetro</span>
+                    </div>
+                  </div>
+
+                  <div>
+                    <Link
+                      href="/"
+                      className="inline-block border border-blue-400 text-blue-400 py-1 px-4 rounded-full font-bold"
+                    >
+                      Follow
+                    </Link>
+                  </div>
+                </div>
+                <div className=" block border-b items-center justify-between flex group py-4 px-2 mr-6 text-gray-700  hover:text-blue-400">
+                  <div className="flex space-x-4">
+                    <Image
+                      width="48"
+                      height="48"
+                      className="rounded-full"
+                      src="https://pbs.twimg.com/profile_images/1005827060617695233/XiH6yzF5_normal.jpg"
+                      alt=""
+                      srcset=""
+                    />
+                    <div>
+                      <p className="font-semibold text-gray-800">MaseruMetro</p>
+                      <span className="text-gray-600">@MaseruMetro</span>
+                    </div>
+                  </div>
+
+                  <div>
+                    <Link
+                      href="/"
+                      className="inline-block border border-blue-400 text-blue-400 py-1 px-4 rounded-full font-bold"
+                    >
+                      Follow
+                    </Link>
+                  </div>
+                </div>
+                <div className="px-2">
+                  <p className="text-sm font-bold text-blue-400 py-3">
+                    See more
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
