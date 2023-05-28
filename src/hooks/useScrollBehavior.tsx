23339747c3d4918f607f813113b12f00cdd5d966
Muhammed Shittu <devshittu@gmail.com>
2023-05-28 04:32:29 +0100

    Update the useScrollBehaviour

--- src/hooks/useScrollBehavior.tsx
+++ src/hooks/useScrollBehavior.tsx
@@ -2,9 +2,11 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 export const useScrollBehavior = () => {
   const [isScrolledUp, setIsScrolledUp] = useState(false);
-
   const [y, setY] = useState<number>(
     typeof window !== 'undefined' ? window.scrollY : 0,
+  );
+  const [screenHeight, setScreenHeight] = useState<number>(
+    typeof window !== 'undefined' ? window.innerHeight : 0,
   );
 
   const handleNavigation = useCallback(
@@ -21,13 +23,16 @@ export const useScrollBehavior = () => {
     },
     [y],
   );
+
   useEffect(() => {
     setY(typeof window !== 'undefined' ? window.scrollY : 0);
+    setScreenHeight(typeof window !== 'undefined' ? window.innerHeight : 0);
     window.addEventListener('scroll', handleNavigation);
 
     return () => {
       window.removeEventListener('scroll', handleNavigation);
     };
   }, [handleNavigation]);
-  return { isScrolledUp, yPosition: y };
+
+  return { isScrolledUp, yPosition: y, screenHeight };
 };
