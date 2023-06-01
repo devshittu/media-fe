afffc8d7323d549fbee348259137d25772b2829f
Muhammed Shittu <mshittu@Tawakalitus-MacBook-Pro.local>
2023-06-01 23:35:05 +0100

    Updated the fonts page

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Roboto_Mono } from 'next/font/google';
+import { Inter, Roboto_Mono, Source_Serif_Pro } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin'],
@@ -8,6 +8,13 @@ export const inter = Inter({
 export const roboto_mono = Roboto_Mono({
   subsets: ['latin'],
   display: 'swap',
+});
+
+export const source_serif_pro = Source_Serif_Pro({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+  style: ['normal', 'italic'],
 });
 
 // Path: src/fonts/fonts.ts
