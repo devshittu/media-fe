f46cd77b08702066dac1c59fb9c8e24fcec2f11d
Muhammed Shittu <devshittu@gmail.com>
2023-06-02 21:57:28 +0100

    Update the checkbox-group to accept the checkbox default values if available.

--- src/components/form/checkbox-group.tsx
+++ src/components/form/checkbox-group.tsx
@@ -2,9 +2,28 @@ import { useState } from 'react';
 import Checkbox from './checkbox';
 import { CheckboxGroupProps, CheckboxOption } from './types';
 
-const CheckboxGroup = ({ options, onChange }: CheckboxGroupProps) => {
-  const [checkedItems, setCheckedItems] = useState<Record<string, boolean>>({});
-  const [selectedOption, setSelectedOption] = useState<string[]>([]);
+const CheckboxGroup = ({
+  options,
+  defaultValues,
+  onChange,
+}: CheckboxGroupProps) => {
+  const [checkedItems, setCheckedItems] = useState<Record<string, boolean>>(
+    () => {
+      const initialCheckedItems: Record<string, boolean> = {};
+
+      // Set initial checked state based on defaultValues prop
+      if (defaultValues) {
+        defaultValues.forEach((value) => {
+          initialCheckedItems[value] = true;
+        });
+      }
+
+      return initialCheckedItems;
+    },
+  );
+  const [selectedOption, setSelectedOption] = useState<string[]>(
+    defaultValues || [],
+  );
 
   const handleCheckboxChange = (option: CheckboxOption) => {
     const newCheckedItems = { ...checkedItems };
