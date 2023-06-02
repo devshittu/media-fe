3bfc5eb0351e57ee2a6fcdc08a4d8c83ef2a7bb1
Muhammed Shittu <devshittu@gmail.com>
2023-06-02 05:16:01 +0100

    Updated types with the radio-group component types

--- src/components/form/types.d.ts
+++ src/components/form/types.d.ts
@@ -11,7 +11,33 @@ export type CheckboxProps = {
   children?: React.ReactNode;
 };
 
-type CheckboxGroupProps = {
+export type CheckboxGroupProps = {
   options: Option[];
   onChange?: (checkedItems: { [key: string]: boolean }) => void;
 };
+
+export type RadioOption = {
+  label: string;
+  value: string;
+  disabled?: boolean;
+};
+
+export type RadioProps = {
+  id: string;
+  name: string;
+  value: string;
+  selectedValue?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+type RadioGroupProps = {
+  name: string;
+  defaultValue?: string;
+  options: RadioOption[];
+  disabled?: boolean;
+  size?: 'small' | 'medium' | 'large';
+};
+
+// Path: src/components/form/types.d.ts
