f8256245c4d027e26e720bbe658f9df0dca14561
Muhammed Shittu <devshittu@gmail.com>
2023-06-02 21:40:56 +0100

    Output selected option.

--- src/components/form/checkbox-group.tsx
+++ src/components/form/checkbox-group.tsx
@@ -4,12 +4,15 @@ import { CheckboxGroupProps, CheckboxOption } from './types';
 
 const CheckboxGroup = ({ options, onChange }: CheckboxGroupProps) => {
   const [checkedItems, setCheckedItems] = useState<Record<string, boolean>>({});
+  const [selectedOption, setSelectedOption] = useState<string[]>([]);
+  
 
   const handleCheckboxChange = (option: CheckboxOption) => {
     const newCheckedItems = { ...checkedItems };
     newCheckedItems[option.value] = !newCheckedItems[option.value];
     setCheckedItems(newCheckedItems);
     console.log('newCheckedItems', newCheckedItems);
+    setSelectedOption(Object.keys(newCheckedItems).filter((key) => newCheckedItems[key]));
 
     if (onChange) {
       onChange(newCheckedItems);
@@ -26,6 +29,10 @@ const CheckboxGroup = ({ options, onChange }: CheckboxGroupProps) => {
           onChange={() => handleCheckboxChange(option)}
         />
       ))}
+
+      <p>
+        Selected option: <strong>{selectedOption}</strong>
+      </p>
     </div>
   );
 };
