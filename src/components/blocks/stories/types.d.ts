ff003d2ac0838166d614662db36a762c1fa53e06
Muhammed Shittu <devshittu@gmail.com>
2023-06-05 23:52:10 +0100

    Refactored the story-list item to load dynamically content..

---
+++ src/components/blocks/stories/types.d.ts
@@ -0,0 +1,14 @@
+export type StoryItem = {
+  id: string;
+  title: string;
+  body: string;
+  createdAt: number;
+  updatedAt: number;
+};
+export type StoryListItemProps = {
+  story?: StoryItem;
+  className?: string;
+};
+export type StoryListProps = {
+  data?: StoryItem[];
+};
