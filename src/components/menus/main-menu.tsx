c5b6d61f00c98dfcd0e5e629583639c59f500420
Muhammed Shittu <devshittu@gmail.com>
2023-06-06 23:44:45 +0100

    Updated main menu to old icon

--- src/components/menus/main-menu.tsx
+++ src/components/menus/main-menu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import ThemeSwitch from '@/components/theme-switch/theme-switch';
+import { Home } from '@/components/blocks/icons';
 
 const MainMenu = () => {
   return (
@@ -12,16 +13,7 @@ const MainMenu = () => {
               href="/stories"
               className="flex items-center p-2 text-slate-900 rounded-lg dark:text-white hover:bg-slate-100 dark:hover:bg-slate-700"
             >
-              <svg
-                aria-hidden="true"
-                className="w-6 h-6 text-slate-500 transition duration-75 dark:text-slate-400 group-hover:text-slate-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-              </svg>
+              <Home className="w-6 h-6 text-slate-500 transition duration-75 dark:text-slate-400 group-hover:text-slate-900 dark:group-hover:text-white" />
               <span className="ml-3">Stories</span>
             </Link>
           </li>
@@ -30,6 +22,7 @@ const MainMenu = () => {
               href="/draft/"
               className="flex items-center p-2 text-slate-900 rounded-lg dark:text-white hover:bg-slate-100 dark:hover:bg-slate-700"
             >
+              <Home className="w-6 h-6 text-slate-500 transition duration-75 dark:text-slate-400 group-hover:text-slate-900 dark:group-hover:text-white" />
               <svg
                 aria-hidden="true"
                 className="w-6 h-6 text-slate-500 transition duration-75 dark:text-slate-400 group-hover:text-slate-900 dark:group-hover:text-white"
