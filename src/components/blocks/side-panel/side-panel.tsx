19e2784ef17c35bf8b9df2b1526489622376333f
Muhammed Shittu <devshittu@gmail.com>
2023-06-08 00:43:59 +0100

    Adjusted speed on the side-panel.

--- src/components/blocks/side-panel/side-panel.tsx
+++ src/components/blocks/side-panel/side-panel.tsx
@@ -7,34 +7,27 @@ import { HashtagList } from '../hashtags';
 import { rangeLimit } from '@/utils';
 
 export const SidePanel = () => {
-  const { isScrolledUp, yPosition, screenHeight } = useScrollBehavior();
+  const { isScrolledUp, screenHeight } = useScrollBehavior();
   const sidePanelRef = useRef<HTMLDivElement>(null);
   const [sidePanelHeight, setSidePanelHeight] = useState<number>(0);
-
   const [sidebarTop, setSidebarTop] = useState(0); // Initial top position of the sidebar
 
   useEffect(() => {
     function handleScroll() {
-      // console.log('screenHeight', screenHeight);
       if (sidePanelRef.current) {
-        setSidePanelHeight(sidePanelRef.current.clientHeight);
-        // console.log('sidePanelHeight', sidePanelHeight);
-        // console.log(
-        //   `screenHeight (${screenHeight}) - sidePanelHeight(${sidePanelHeight}) = `,
-        //   screenHeight - sidePanelHeight,
-        // );
+        setSidePanelHeight(sidePanelRef.current.getBoundingClientRect().height);
       }
-      // Increase or decrease the position of the header based on the scroll direction
+
       setSidebarTop((prevTopPosition) => {
+        const minTop = screenHeight - sidePanelHeight;
         const newTop = rangeLimit(
-          (isScrolledUp ? prevTopPosition + 1 : prevTopPosition - 1) * 10,
-          -200,
+          prevTopPosition + (isScrolledUp ? 1 : -1) * 10, // Adjust the scroll speed as desired
+          minTop,
           0,
-        ); // 1 is the speed
+        );
         return newTop;
       });
     }
-
     // Attach the event listener to the mouse wheel scroll event
     window.addEventListener('scroll', handleScroll);
 
@@ -42,20 +35,19 @@ export const SidePanel = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [isScrolledUp]);
+  }, [isScrolledUp, sidePanelHeight, screenHeight]);
   return (
     <div
       className="flex-1 pb-0 hidden lg:block lg:sticky top-0 min-h-screen"
       // style={{ top: `${sidebarTop}px` }}
     >
-      {/* <div className={`sticky top-0 pb-16 `}> */}
       <div className={`sticky top-0 z-10 bg-white dark:bg-slate-900`}>
         <div className="py-4">
           <InputField name="Search" placeholder="Search app" className="mb-4" />
         </div>
       </div>
       <div
-        className=" space-y-16 min-h-screen sticky top-24 overflow-hidden pb-60"
+        className=" space-y-16 min-h-screen sticky top-28 overflow-hidden pb-60  transition-all  duration-75"
         ref={sidePanelRef}
         style={{ transform: `translateY(${sidebarTop}px)` }}
         // style={{ top: `${sidebarTop}px` }}
