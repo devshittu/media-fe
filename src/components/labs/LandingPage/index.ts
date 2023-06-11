92f9c76a9a7cb5a08e1ceea416158a111131a416
Muhammed Shittu <devshittu@gmail.com>
2023-06-11 23:49:45 +0100

    Added common export via index.ts for landing page

---
+++ src/components/labs/LandingPage/index.ts
@@ -0,0 +1,6 @@
+export { default as Footer } from './footer';
+export { default as Nav } from './nav';
+export { default as Hero } from './hero';
+export { default as Gallery } from './gallery';
+export { default as Marquee } from './marquee';
+export { default as Feature } from './feature';
