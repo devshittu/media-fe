10806cbd19a316e0fe53c82a9612adc596d9d277
Muhammed Shittu <devshittu@gmail.com>
2023-06-12 23:55:25 +0100

    Modified and updated the testing data and exported values.

--- src/testing/test-data.ts
+++ src/testing/test-data.ts
@@ -10,7 +10,7 @@ export const testData = {
       categoryId: 'amYXmIyT9mD9GyO6CCr',
     },
   ],
-  categories: [
+  admin: [
     {
       id: 'amYXmIyT9mD9GyO6CCr',
       createdAt: 1645628972465,
@@ -103,6 +103,142 @@ export const testData = {
       updatedAt: 1657058400,
     },
   ],
+
+  categories: [
+    {
+      id: '1',
+      title: 'World News',
+      description: 'Latest news and events from around the world.',
+      slug: 'world-news',
+    },
+    {
+      id: '2',
+      title: 'Politics',
+      description:
+        'Updates on political developments, elections, and government policies.',
+      slug: 'politics',
+    },
+    {
+      id: '3',
+      title: 'Business',
+      description:
+        'News related to the business world, economy, and financial markets.',
+      slug: 'business',
+    },
+    {
+      id: '4',
+      title: 'Technology',
+      description:
+        'Information on the latest technological advancements, gadgets, and innovations.',
+      slug: 'technology',
+    },
+    {
+      id: '5',
+      title: 'Science',
+      description:
+        'Discoveries, research, and breakthroughs in various scientific fields.',
+      slug: 'science',
+    },
+    {
+      id: '6',
+      title: 'Health',
+      description:
+        'Updates on healthcare, medical research, and wellness tips.',
+      slug: 'health',
+    },
+    {
+      id: '7',
+      title: 'Sports',
+      description: 'Coverage of sports events, matches, and athlete profiles.',
+      slug: 'sports',
+    },
+    {
+      id: '8',
+      title: 'Entertainment',
+      description: 'Music, movies, TV shows, and celebrity news.',
+      slug: 'entertainment',
+    },
+    {
+      id: '9',
+      title: 'Lifestyle',
+      description:
+        'Tips and trends related to fashion, beauty, home, and personal development.',
+      slug: 'lifestyle',
+    },
+    {
+      id: '10',
+      title: 'Travel',
+      description:
+        'Inspiration, guides, and recommendations for travel destinations and experiences.',
+      slug: 'travel',
+    },
+    {
+      id: '11',
+      title: 'Food',
+      description: 'Recipes, restaurant reviews, and culinary trends.',
+      slug: 'food',
+    },
+    {
+      id: '12',
+      title: 'Fashion',
+      description: 'Fashion trends, runway coverage, and style tips.',
+      slug: 'fashion',
+    },
+    {
+      id: '13',
+      title: 'Art and Culture',
+      description:
+        'Art exhibitions, cultural events, and creative expressions.',
+      slug: 'art-culture',
+    },
+    {
+      id: '14',
+      title: 'Education',
+      description:
+        'News and updates in the field of education, learning, and academic institutions.',
+      slug: 'education',
+    },
+    {
+      id: '15',
+      title: 'Environment',
+      description:
+        'Coverage of environmental issues, conservation efforts, and sustainability.',
+      slug: 'environment',
+    },
+    {
+      id: '16',
+      title: 'Finance',
+      description:
+        'Insights into personal finance, investments, and money management.',
+      slug: 'finance',
+    },
+    {
+      id: '17',
+      title: 'Automotive',
+      description:
+        'News and reviews about cars, motorcycles, and the automotive industry.',
+      slug: 'automotive',
+    },
+    {
+      id: '18',
+      title: 'Gaming',
+      description: 'Updates on video games, gaming consoles, and esports.',
+      slug: 'gaming',
+    },
+    {
+      id: '19',
+      title: 'Social Issues',
+      description: 'Discussions on social justice, equality, and human rights.',
+      slug: 'social-issues',
+    },
+    {
+      id: '20',
+      title: 'Celebrity News',
+      description:
+        'Gossip, rumors, and news about celebrities and pop culture.',
+      slug: 'celebrity-news',
+    },
+  ],
 };
 
 const delayedFn =
@@ -115,10 +251,11 @@ const delayedFn =
 
 export const getUser = delayedFn(() => testData.users[0], 0);
 
-export const getCatgories = delayedFn(
+export const getCategory = delayedFn(
   (id: string) => testData.categories.find((o) => o.id === id) || null,
   300,
 );
+export const getCategories = delayedFn(() => testData.categories || null, 300);
 
 export const getStories = delayedFn(
   (categoryId: string) =>
@@ -148,7 +285,9 @@ const useTestData = <T>(promise: Promise<T>) => {
 
 export const useUser = () => useTestData(getUser());
 
-export const useCatgories = (id: string) => useTestData(getCatgories(id));
+export const useCategory = (id: string) => useTestData(getCategory(id));
+
+export const useCategories = () => useTestData(getCategories());
 
 export const useStories = (categoryId: string) =>
   useTestData(getStories(categoryId));
---
+++ src/testing/types.d.ts
@@ -0,0 +1,15 @@
+export type StoryItem = {
+  id: string;
+  title: string;
+  body: string;
+  categoryId: string;
+  createdAt: number;
+  updatedAt: number;
+};
+
+export type CategoryItem = {
+  id: string;
+  title: string;
+  description: string;
+  title: string;
+};
