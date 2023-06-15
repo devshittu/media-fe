f0e05fdf38b1004dde374c495866feb40bddeb2d
Muhammed Shittu <devshittu@gmail.com>
2023-06-15 21:49:19 +0100

    TableOfContents components Heading for the side bar.

---
+++ src/components/blocks/table-of-contents/heading.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface Heading {
+  id: string;
+  title: string;
+  items: Heading[];
+}
+
+interface HeadingProps {
+  heading: Heading;
+  activeId: string;
+}
+
+const Heading: React.FC<HeadingProps> = ({ heading, activeId }) => (
+  <li className={heading.id === activeId ? 'active' : ''}>
+    <Link
+      href={`#${heading.id}`}
+      onClick={(e) => {
+        e.preventDefault();
+        document.querySelector(`#${heading.id}`)?.scrollIntoView({
+          behavior: 'smooth',
+        });
+      }}
+    >
+      {heading.title}
+    </Link>
+    {heading.items.length > 0 && (
+      <ul>
+        {heading.items.map((child) => (
+          <Heading key={child.id} heading={child} activeId={activeId} />
+        ))}
+      </ul>
+    )}
+  </li>
+);
+
+export default Heading;
