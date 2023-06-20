cf3cab60d2578cb4a1fd29b5f647cbf768dca32b
Muhammed Shittu <devshittu@gmail.com>
2023-06-20 23:35:13 +0100

    Create a programmable modal.tsx using class invocation

--- src/components/blocks/modal/modal.tsx
+++ src/components/blocks/modal/modal.tsx
@@ -1,119 +1,87 @@
-import { useKeyPress } from '@/hooks';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { createRoot, Root } from 'react-dom/client';
+import { ModalComponent } from './modal-component';
+import { ModalProps } from './types';
 
-type ModalProps = {
-  id: string;
-  title: string;
-  children: React.ReactNode;
-  size?: 'small' | 'default' | 'large' | 'full';
-};
+class Modal {
+  private id: string;
+//   private type: ModalProps['type'];
+  private size: ModalProps['size'];
+  private title: ModalProps['title'];
+  private noOverlay: ModalProps['noOverlay'];
+  private children?: ModalProps['children'];
+  private onClose?: ModalProps['onClose'];
+  private container: HTMLElement;
+  private root: Root;
+  private isOpen: boolean;
+  private elementContainer: HTMLElement | null = null;
 
-export const Modal = ({
+  constructor({
   id,
   size = 'default',
+  noOverlay = false,
   title,
   children,
-}: ModalProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+  onClose
+  }: ModalProps) {
+    this.id = `modal-${Date.now().toString()}`;
+    // this.type = type;
+    // this.message = message;
+    this.size = size;
+    this.title = title;
+    this.children = children;
+    this.onClose = onClose;
+    this.container = document.createElement('div');
+    this.container.id = 'modal-container';
+    document.body.append(this.container);
+    this.root = createRoot(this.container);
+    this.isOpen = false;
+  }
 
-  const escapePressed = useKeyPress('Escape');
-  const sizeClassName = {
-    small: 'max-w-md',
-    default: 'max-w-lg',
-    large: 'max-w-4xl',
-    full: 'max-w-full',
-  };
-  const openModal = () => {
-    setIsOpen(true);
-    document.documentElement.style.overflow = 'hidden';
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-    document.documentElement.style.overflow = '';
-  };
-
-  useEffect(() => {
-    if (escapePressed) {
-      closeModal();
+  public open = () => {
+    if (!this.isOpen) {
+      this.isOpen = true;
+      this.render();
     }
-  }, [escapePressed]);
+  };
 
-  return (
-    <>
-      <button
-        data-modal-target={id}
-        data-modal-toggle={id}
-        className="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-        onClick={openModal}
-      >
-        {title}
-      </button>
+  private render = () => {
+    this.isOpen = true;
+    this.elementContainer = document.createElement('div');
+    this.container.appendChild(this.elementContainer);
 
-      {/* Overlay background */}
+    this.root.render(
+      <ModalComponent
+        id={this.id}
+        size={this.size}
+        title={this.title}
+        onClose={this.close}
+      >{this.children}</ModalComponent>
+    );
+  };
 
-      <div
-        className={`inset-0 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80 z-50 w-full h-screen overflow-y-hidden ${
-          isOpen ? 'fixed top-0 bottom-0 left-0 right-0' : 'hidden '
-        }`}
-        id={`${id}-dialog-overlay`}
-        aria-labelledby={`${id}-label`}
-        aria-hidden="true"
-        data-modal-state={`${isOpen ? 'open' : 'close'}`}
-      ></div>
+  public close = () => {
+    if (this.isOpen) {
+      this.isOpen = false;
+      const toastElement = document.getElementById(this.id);
+      if (toastElement) {
+        this.root.unmount();
+        toastElement.remove();
+      }
 
-      {/* Modal content */}
-      <div
-        id={id}
-        tabIndex={-1}
-        className={`fixed top-0 left-0 right-0 z-50 ${
-          isOpen ? '' : 'hidden'
-        } w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0  max-h-full 
-        ${
-          size !== 'small' && size !== 'full'
-            ? 'justify-center items-center flex '
-            : ''
-        }
-        h-[calc(100%-1rem)]`}
-        aria-labelledby={`${id}-label`}
-      >
-        <div
-          className={`relative w-full mx-auto ${sizeClassName[size]} max-h-full`}
-        >
-          <div className="relative border-2 md:border-4 border-slate-700 dark:border-slate-300 shadow-xl shadow-slate-400 dark:shadow-slate-800 bg-slate-100 dark:bg-slate-700">
-            <div
-              className={`flex items-center justify-between  p-5 border-b rounded-t dark:border-slate-600`}
-            >
-              <h3 className="text-xl font-extrabold text-slate-900 dark:text-white">
-                {title}
-              </h3>
-              <button
-                type="button"
-                className="text-slate-400 bg-transparent hover:bg-slate-200 hover:text-slate-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-slate-600 dark:hover:text-white"
-                data-modal-hide={id}
-                onClick={closeModal}
-              >
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
-            </div>
-            <div className="p-6 space-y-6">{children}</div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+      if (this.onClose) {
+        this.onClose();
+      }
+
+      // Clean up the container from the DOM
+      if (this.container.parentNode) {
+        this.container.parentNode.removeChild(this.container);
+      }
+    }
+  };
+
+}
+
+export default Modal;
+
+// Path: src/components/blocks/modal/Modal.tsx
