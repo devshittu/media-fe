01465dee0b5c3085deb27a41a9eed6c2d5cb0950
Muhammed Shittu <devshittu@gmail.com>
2023-06-23 23:54:07 +0100

    Updated the icons in the toast-component.

--- src/components/blocks/toast/toast-component.tsx
+++ src/components/blocks/toast/toast-component.tsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { ToastProps } from './types';
 import { useKeyPress } from '@/hooks';
 import Portal from '@/hoc/Portal';
+import {
+  AlertTriangleIcon,
+  CheckIcon,
+  XIcon,
+} from '@/components/illustrations';
 
 export const ToastComponent = ({
   id,
@@ -112,49 +117,11 @@ export const ToastComponent = ({
         <div className="flex justify-normal items-center">
           <div className={getIconClassName()}>
             {type === 'success' && (
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <CheckIcon className="w-5 h-5" strokeWidth={3} />
             )}
-            {type === 'danger' && (
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            )}
+            {type === 'danger' && <XIcon className="w-5 h-5" strokeWidth={3} />}
             {type === 'warning' && (
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <AlertTriangleIcon className="w-5 h-5" strokeWidth={3} />
             )}
             <span className="sr-only">{type} icon</span>
           </div>
@@ -167,19 +134,7 @@ export const ToastComponent = ({
           aria-label="Close"
         >
           <span className="sr-only">Close</span>
-          <svg
-            aria-hidden="true"
-            className="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <XIcon className="w-5 h-5" strokeWidth={3} />
         </button>
       </div>
     </Portal>
