a33c6cb2d19a77ba861dbec6aa0ae1828e5706a2
Muhammed Shittu <devshittu@gmail.com>
2023-06-23 23:55:42 +0100

    Updated the icons in the social pack colored collected.

---
+++ src/components/illustrations/icons/social/index.ts
@@ -0,0 +1,4 @@
+export { default as TwitterIcon } from './Twitter';
+export { default as WhatsAppIcon } from './WhatsApp';
+export { default as AppLogoIcon } from './AppLogo';
+export { default as GoogleIcon } from './Google';
