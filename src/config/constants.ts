b5fc9feec712e03a2226d2d4ccf3fdec393877b2
Muhammed Shittu <devshittu@gmail.com>
2023-06-24 18:00:25 +0100

    Added a config for constants.

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+
+export const THEME_DARK = 'dark';
+export const THEME_LIGHT = 'light';
+
+export const RECENT_ARTICLES_LIMIT = 2;
