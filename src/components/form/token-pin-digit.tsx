b68376a973909a47cfeb770d7ec13e3c21bc3d4b
Muhammed Shittu <devshittu@gmail.com>
2023-06-26 21:41:41 +0100

    Worked on the visibility for tokens

--- src/components/form/token-pin-digit.tsx
+++ src/components/form/token-pin-digit.tsx
@@ -1,42 +1,41 @@
-import Link from 'next/link';
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
-type PinDigitProps = {
-  value: string;
+interface PinDigitProps {
   focused: boolean;
-};
+  value: string | undefined;
+}
 
-function PinDigit(props: PinDigitProps) {
+function PinDigit({ focused, value }: PinDigitProps) {
   const [hidden, setHidden] = useState(false);
 
   useEffect(() => {
-    if (props.value) {
+    if (value) {
       const timeout = setTimeout(() => {
-        setHidden(true);
+        // setHidden(true);
       }, 500);
 
       return () => {
-        setHidden(false);
+        // setHidden(false);
         clearTimeout(timeout);
       };
     }
-  }, [props.value]);
+  }, [value]);
+
+  const digitClasses = `app-pin-digit focused items-center h-20 w-14 inline-flex relative border-white/50x border-2 bg-white-600x rounded justify-center after:content-[''] after:shadow-md after:z-20 after:absolute before:content-[''] before:shadow-md before:z-20 before:absolute ${
+    focused ? 'focused' : ''
+  } ${hidden ? 'hidden' : ''}`;
 
   return (
-    <div
-      className={`app-pin-digit focused items-center h-20 w-14 inline-flex relative border-slate-900/50 dark:border-white/50 border-2 bg-white-600 justify-center after:content-[''] after:shadow-md after:z-20 after:absolute before:content-[''] before:shadow-md before:z-20 before:absolute ${
-        props.focused ? 'focused' : ''
-      } ${hidden ? 'hidden' : ''}`}
-    >
+    <div className={digitClasses}>
       <span className="app-pin-digit-value text-5xl m-0 p-0">
-        {props.value || ''}
+        {value || ''}
       </span>
     </div>
   );
 }
 
 type TokenPinInputFieldProps = {
-  setUserStatus: UserLoginStatus; // (status: string) => void
+  userLoginStatus: UserLoginStatus; // (status: string) => void
 };
 
 export enum UserLoginStatus {
@@ -47,80 +46,75 @@ export enum UserLoginStatus {
   LOGGED_OUT = 'logged-out',
 }
 
-const TokenPinInputField = ({ setUserStatus }: TokenPinInputFieldProps) => {
-  const [userLoginState, setUserLoginState] =
-    useState<UserLoginStatus>(setUserStatus);
+function TokenPinInputField({ userLoginStatus }: TokenPinInputFieldProps) {
   const [pin, setPin] = useState('');
+  const pinInputRef = useRef<HTMLInputElement>(null);
+  const [userLoginState, setUserLoginState] =
+    useState<UserLoginStatus>(userLoginStatus);
   const [rememberMe, setRememberMe] = useState(false);
-  const tokenInputFieldRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (
       userLoginState === UserLoginStatus.LOGGING_IN ||
       userLoginState === UserLoginStatus.LOG_IN_ERROR
     ) {
-      tokenInputFieldRef.current?.focus();
+      pinInputRef.current?.focus();
     } else {
       setPin('');
     }
   }, [userLoginState]);
 
-  const verify = async () => {
-    try {
-      setUserLoginState(UserLoginStatus.VERIFYING_LOG_IN);
-
-      const verifyC = (pin: string) => {
-        return new Promise<boolean>((resolve, reject) => {
+  useEffect(() => {
+    const verifyPin = async (pin: string) => {
+      try {
+        setUserLoginState(UserLoginStatus.VERIFYING_LOG_IN);
+        await new Promise<boolean>((resolve, reject) => {
           setTimeout(() => {
             if (pin === '1234') {
+              if (rememberMe) console.log('Add it to the local storage.'); //setData(true); //Todo Add a remind me checkmark. and set it he localstorage.
               resolve(true);
             } else {
               reject(`Invalid pin: ${pin}`);
             }
           }, Math.floor(Math.random() * (700 - 300 + 1)) + 300);
         });
-      };
-
-      if (await verifyC(pin)) {
         setUserLoginState(UserLoginStatus.LOGGED_IN);
+      } catch (err) {
+        console.error(err);
+        setUserLoginState(UserLoginStatus.LOG_IN_ERROR);
       }
-    } catch (err) {
-      console.error(err);
-      setUserLoginState(UserLoginStatus.LOG_IN_ERROR);
-    }
-  };
+    };
 
-  useEffect(() => {
     if (pin.length === 4) {
-      verify();
+      verifyPin(pin);
     }
 
     if (userLoginState === UserLoginStatus.LOG_IN_ERROR) {
       setUserLoginState(UserLoginStatus.LOG_IN_ERROR);
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pin]);
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length <= 4) {
-      setPin(e.target.value.toString());
-    }
+  const handlePinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPin = e.target.value.substring(0, 4);
+    setPin(newPin);
   };
 
-  const handleOnChangeRememberMe = () => {
+  const handleRememberMeChange = () => {
     setRememberMe(!rememberMe);
   };
 
-  const handleOnClick = () => {
-    tokenInputFieldRef.current?.focus();
+  const handlePinDigitClick = () => {
+    pinInputRef.current?.focus();
   };
 
-  const handleOnCancel = () => {
+  const handleCancelClick = () => {
     setUserLoginState(UserLoginStatus.LOGGED_OUT);
   };
-  // Todo: remove the 'x' in the class names
+
   return (
-    <div className="app-pin-wrapper absolutex z-50 opacity-0x left-[50%] top-[50%] pointer-events-auto space-y-3 ">
+    <div className="absolutex z-30 opacity-0x left-[50%] top-[50%] pointer-events-auto app-pin-wrapper space-y-3">
       <input
         className="bg-transparent border-none ring-0 h-0 absolute w-0"
         disabled={
@@ -129,21 +123,19 @@ const TokenPinInputField = ({ setUserStatus }: TokenPinInputFieldProps) => {
         }
         id="app-pin-hidden-input"
         maxLength={4}
-        ref={tokenInputFieldRef}
+        ref={pinInputRef}
         type="number"
         value={pin}
-        onChange={handleOnChange}
+        onChange={handlePinChange}
       />
-
-      {/* //Todo add the useKeyPress hook */}
       <div
         role="button"
         tabIndex={0}
         className="flex gap-2"
-        onClick={handleOnClick}
+        onClick={handlePinDigitClick}
         onKeyDown={(e) => {
           if (e.key === 'Enter' || e.key === ' ') {
-            handleOnClick();
+            handlePinDigitClick();
           }
         }}
       >
@@ -153,14 +145,13 @@ const TokenPinInputField = ({ setUserStatus }: TokenPinInputFieldProps) => {
         <PinDigit focused={pin.length === 3} value={pin[3]} />
       </div>
       <h1>
-        {' Enter the "1234"'}
-        <Link
-          href="#"
+        {'Enter the "1234" '}
+        <button
           onClick={() => setUserLoginState(UserLoginStatus.LOGGED_OUT)}
           className="text-blue-600 dark:text-blue-500 hover:underline"
         >
           Cancel
-        </Link>
+        </button>
       </h1>
 
       <div className="flex items-center">
@@ -168,16 +159,16 @@ const TokenPinInputField = ({ setUserStatus }: TokenPinInputFieldProps) => {
           id="link-checkbox"
           type="checkbox"
           checked={rememberMe}
-          onChange={handleOnChangeRememberMe}
+          onChange={handleRememberMeChange}
           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
         />
         <label htmlFor="link-checkbox" className="ml-2 font-medium">
-          Remember me
+          Remember me{' '}
         </label>
       </div>
     </div>
   );
-};
+}
 
 export default TokenPinInputField;
 
