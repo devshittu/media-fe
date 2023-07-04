d49baf7ca59169d2888e57de94765bf5621b5a6c
Muhammed Shittu <devshittu@gmail.com>
2023-07-04 21:06:05 +0100

    Changed how switched theme is displayed (Changed Layout)

--- src/components/theme-switch/theme-switch.tsx
+++ src/components/theme-switch/theme-switch.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
-import { THEME_DARK, THEME_LIGHT } from '@/config/constants';
+import { THEME_DARK, THEME_LIGHT, THEME_SYSTEM } from '@/config/constants';
 import Head from 'next/head';
-import { MoonIcon, SunIcon } from '@/components/illustrations';
+import {
+  Icon,
+  MonitorIcon,
+  MoonIcon,
+  SunIcon,
+} from '@/components/illustrations';
 type ThemeSwitchProps = {
   showLabel?: boolean;
 };
@@ -28,7 +33,7 @@ const ThemeSwitch = ({ showLabel = false }: ThemeSwitchProps) => {
           content={theme === THEME_DARK ? '#0f172a' : '#FAFBFC'}
         />
       </Head>
-      <button
+      {/* <button
         onClick={() =>
           theme === THEME_DARK ? setTheme(THEME_LIGHT) : setTheme(THEME_DARK)
         }
@@ -54,7 +59,43 @@ const ThemeSwitch = ({ showLabel = false }: ThemeSwitchProps) => {
             {currentTheme === THEME_DARK ? THEME_LIGHT : THEME_DARK}
           </span>
         )}
-      </button>
+      </button> */}
+
+      <div
+        className="grid max-w-full grid-cols-3 gap-1 p-1 mx-auto my-2 bg-slate-100 rounded-lgx dark:bg-slate-600"
+        role="group"
+      >
+        <button
+          title="Light"
+          type="button"
+          className={`px-3 py-1.5 text-xs font-medium ${currentTheme === THEME_LIGHT ? ' text-white bg-slate-900 dark:bg-slate-300 dark:text-slate-900' : 'text-slate-900 hover:bg-slate-200 dark:text-white dark:hover:bg-slate-700'}`}
+          onClick={() => setTheme(THEME_LIGHT)}
+          disabled={disabled}
+          aria-label="Light Theme"
+        >
+          <Icon icon={<SunIcon />} className="w-4" strokeWidth={2.5} />
+        </button>
+        <button
+          title="System"
+          type="button"
+          className={`px-3 py-1.5 text-xs font-medium ${currentTheme === THEME_SYSTEM ? ' text-white bg-slate-900 dark:bg-slate-300 dark:text-slate-900' : 'text-slate-900 hover:bg-slate-200 dark:text-white dark:hover:bg-slate-700'}`}
+          onClick={() => setTheme(THEME_SYSTEM)}
+          disabled={disabled}
+          aria-label="System Theme"
+        >
+          <Icon icon={<MonitorIcon />} className="w-4" strokeWidth={2.5} />
+        </button>
+        <button
+          title="Dark"
+          type="button"
+          className={`px-3 py-1.5 text-xs font-medium ${currentTheme === THEME_DARK ? ' text-white bg-slate-900 dark:bg-slate-300 dark:text-slate-900' : 'text-slate-900 hover:bg-slate-200 dark:text-white dark:hover:bg-slate-700'}`}
+          onClick={() => setTheme(THEME_DARK)} 
+          disabled={disabled}
+          aria-label="Dark Theme"
+        >
+          <Icon icon={<MoonIcon />} className="w-4" strokeWidth={2.5} />
+        </button>
+      </div>
     </>
   );
 };
