33fe20745fe6976fb7136fc812ab770070888df9
Muhammed Shittu <devshittu@gmail.com>
2023-07-04 21:57:00 +0100

    Added system for the theme.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,5 +2,6 @@ export const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
 export const THEME_DARK = 'dark';
 export const THEME_LIGHT = 'light';
+export const THEME_SYSTEM = 'system';
 
 export const RECENT_ARTICLES_LIMIT = 2;
