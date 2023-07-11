df25890590f3146943c0805691dc24e51b7a67ed
Muhammed Shittu <devshittu@gmail.com>
2023-07-11 23:42:23 +0100

    Added tour-sequence

---
+++ src/components/blocks/tour/tour-sequence.tsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import TourBubble from './tour-bubble';
+import Portal from '@/hoc/Portal';
+
+type TourSequenceItem = {
+  forElement: string;
+  text: string;
+  placement?: string;
+};
+
+type TourSequenceProps = {
+  sequence?: TourSequenceItem[];
+  open: boolean;
+  onClose: () => void;
+};
+
+function isVisible(element: HTMLElement | null): boolean {
+  return !!(
+    element?.offsetWidth ||
+    element?.offsetHeight ||
+    (element?.getClientRects && element.getClientRects().length)
+  );
+}
+
+const TourSequence: React.FC<TourSequenceProps> = (props) => {
+  const [position, setPosition] = useState(0);
+  const [sequence, setSequence] = useState<TourSequenceItem[] | null>(null);
+
+  useEffect(() => {
+    if (props.sequence) {
+      const filter = props.sequence.filter((item) => {
+        if (!item.forElement) {
+          return false;
+        }
+        const element = document.querySelector(item.forElement) as HTMLElement;
+        return element && isVisible(element);
+      });
+      setSequence(filter);
+    } else {
+      setSequence(null);
+    }
+  }, [props.sequence, props.open]);
+
+  const data = sequence && sequence[position];
+
+  useEffect(() => {
+    setPosition(0);
+  }, [props.open]);
+
+  const onNext = () => {
+    setPosition((p) => {
+      if (p === (sequence?.length || 0) - 1) {
+        props.onClose && props.onClose();
+      }
+      return p + 1;
+    });
+  };
+
+  const onPrevious = () => {
+    setPosition((p) => {
+      if (p === 0) {
+        props.onClose && props.onClose();
+      }
+      return p - 1;
+    });
+  };
+
+  return (
+    <div className="TourSequence-container">
+      {data && (
+        <TourBubble
+          open={props.open}
+          forElement={data.forElement}
+          placement={data.placement}
+          onClose={props.onClose}
+          previousLabel={position > 0 ? 'Previous' : undefined}
+          nextLabel={position < (sequence?.length || 0) - 1 ? 'Next' : 'Finish'}
+          onPrevious={onPrevious}
+          onNext={onNext}
+          content={data.text}
+        />
+      )}
+    </div>
+  );
+};
+
+export default TourSequence;
