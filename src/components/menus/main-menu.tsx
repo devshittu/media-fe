87972bd9c2ca96d0447ac0769ac1fb831b562d36
Muhammed Shittu <devshittu@gmail.com>
2023-07-14 23:59:22 +0100

    Refactored out some useless code

--- src/components/menus/main-menu.tsx
+++ src/components/menus/main-menu.tsx
@@ -69,33 +69,41 @@ const MainMenu = () => {
   ];
 
   const [showHelp, setShowHelp] = useState(false);
+  const closeHelp = () => {
+    setShowHelp(false);
+  };
+  const openHelp = () => {
+    setShowHelp(true);
+  };
   return (
     <>
       <MenuList menu={mainMenuList} />
       <ThemeSwitch />
-      <button onClick={() => setShowHelp(true)}>Show help</button>
-      <TourSequence
-        sequence={[
-          {
-            forElement: '#page-title',
-            text: 'This is where you will find the page title for the current page.',
-          },
-          {
-            forElement: '#app-search',
-            text: 'Search stories here...',
-          },
-          {
-            forElement: '#trendsForYou',
-            text: 'This is where you will find some trends for you',
-          },
-          {
-            forElement: '.App-nowhere',
-            text: 'This help text will never appear',
-          },
-        ]}
-        open={showHelp}
-        onClose={() => setShowHelp(false)}
-      />
+      <button onClick={openHelp}>Show help</button>
+      {showHelp && (
+        <TourSequence
+          sequence={[
+            {
+              forElement: '#page-title',
+              text: 'This is where you will find the page title for the current page.',
+            },
+            {
+              forElement: '#app-search',
+              text: 'Search stories here...',
+            },
+            {
+              forElement: '#trendsForYouTitle',
+              text: 'This is where you will find some trends for you',
+            },
+            {
+              forElement: '.App-nowhere',
+              text: 'This help text will never appear',
+            },
+          ]}
+          open={showHelp}
+          onClose={closeHelp}
+        />
+      )}
       <div
         id="dropdown-cta"
         className="p-4 mt-6 rounded-lg bg-blue-50 dark:bg-blue-900"
