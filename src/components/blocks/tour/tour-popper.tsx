88e8dbc565435dd76a0eeb5dcace4e112e299203
Muhammed Shittu <devshittu@gmail.com>
2023-07-15 23:18:33 +0100

    Added overlay to tour-popper but commented till final solution

--- src/components/blocks/tour/tour-popper.tsx
+++ src/components/blocks/tour/tour-popper.tsx
@@ -18,8 +18,9 @@ import {
   arrow,
 } from '@floating-ui/react';
 import Portal from '@/hoc/Portal';
+import Overlay from '../overlay/overlay';
 
-type PopoverProps = {
+type TourPopperProps = {
   portaled?: boolean;
   strategy?: Strategy;
   placement?: Placement;
@@ -28,19 +29,28 @@ type PopoverProps = {
   isOpen?: boolean;
 };
 
-export function Popover({
+export function TourPopper({
   children,
   isOpen = false,
   portaled,
   refElement,
   strategy = 'absolute',
   placement = 'bottom',
-}: PopoverProps) {
+}: TourPopperProps) {
   const [open, setOpen] = useState(isOpen);
   const referenceRef = useRef<HTMLElement | null>(null);
-
   useEffect(() => {
-    const referenceElement = document.querySelector(refElement || '') as HTMLElement;
+    if (isOpen) {
+      const element = document.querySelector(refElement || '') as HTMLElement;
+      if (element) {
+        element.style.zIndex = '9999999'; // Set a high zIndex value when the element is open
+      }
+    }
+  }, [isOpen, refElement]);
+  useEffect(() => {
+    const referenceElement = document.querySelector(
+      refElement || '',
+    ) as HTMLElement;
     referenceRef.current = referenceElement;
   }, [refElement]);
 
@@ -74,26 +84,29 @@ export function Popover({
   const popoverWrapperJsx = (
     <>
       {open && (
-        <FloatingOverlay lockScroll style={{ zIndex:99 }}>
-          <FloatingFocusManager context={context} modal={false}>
-            <div
-              className="grid place-items-center bg-gray-1000 text-gray-50"
-              ref={refs.setFloating}
-              style={{
-                position: strategy,
-                top: y ?? 0,
-                left: x ?? 0,
-                // transform: `translate3d(${Math.round(x || 0)}px, ${Math.round(
-                //   y || 0
-                // )}px, 0)`,
-              }}
-              aria-labelledby={headingId}
-              {...getFloatingProps()}
-            >
+        <>
+          {/* TODO: <Overlay isActive={isOpen} id='som'/> */}
+          <FloatingOverlay lockScroll style={{ zIndex: 99 }}>
+            <FloatingFocusManager context={context} modal={false}>
+              <div
+                className="grid place-items-center bg-gray-1000 text-gray-50"
+                ref={refs.setFloating}
+                style={{
+                  position: strategy,
+                  top: y ?? 0,
+                  left: x ?? 0,
+                  // transform: `translate3d(${Math.round(x || 0)}px, ${Math.round(
+                  //   y || 0
+                  // )}px, 0)`,
+                }}
+                aria-labelledby={headingId}
+                {...getFloatingProps()}
+              >
                 {children ?? 'Floating'}
-            </div>
-          </FloatingFocusManager>
-        </FloatingOverlay>
+              </div>
+            </FloatingFocusManager>
+          </FloatingOverlay>
+        </>
       )}
     </>
   );
@@ -109,4 +122,4 @@ export function Popover({
   );
 }
 
-export default Popover;
+export default TourPopper;
