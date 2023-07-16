8b14398d748357dd31aa4b552952a6247bd8677f
Muhammed Shittu <devshittu@gmail.com>
2023-07-16 21:46:34 +0100

    Exported checkboxes

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,5 +1,5 @@
 export * from './input-field';
 export * from './checkbox-group';
-export * from './layout-checkbox';
+export * from './custom-checkbox';
 export * from './radio-group';
 export * from './token-pin-digit';
