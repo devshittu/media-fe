fedb1454c9278a22fb84d8d8606c1e12f5c44942
Muhammed Shittu <devshittu@gmail.com>
2023-07-17 23:24:32 +0100

    Seed the msw db

---
+++ src/testing/mocks/seed-db.ts
@@ -0,0 +1,17 @@
+import { testData } from '../test-data';
+
+import { db } from './db';
+
+export const seedDb = () => {
+  const userCount = db.user.count();
+
+  if (userCount > 0) return;
+
+  testData.users.forEach((user) => db.user.create(user));
+
+  testData.stories.forEach((story) =>
+    db.story.create(story)
+  );
+
+  testData.categories.forEach((category) => db.category.create(category));
+};
