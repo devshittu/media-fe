2581d6d6db993868d3f07a1299bc781667fbb6c9
Muhammed Shittu <devshittu@gmail.com>
2023-07-18 23:17:53 +0100

    Added breadcrumb content

---
+++ src/components/blocks/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { ArrowRightIcon, HomeIcon, Icon } from '@/components/illustrations';
+import { Link } from '@/components/labs';
+
+export type BreadcrumbTrail =  {
+    href: string;
+    label: string;
+  }
+type BreadcrumbProps =  {
+  trails?: BreadcrumbTrail[];
+}
+
+const Breadcrumb: React.FC<BreadcrumbProps> = ({ trails }) => {
+
+    const router = useRouter();
+  const { asPath, pathname } = router;
+
+  // Use the provided trail prop or generate the trail links based on the current path
+  const linkTrail = trails || pathname.split('/').filter((path) => path !== '');
+
+  return (
+    <nav aria-label="breadcrumb" className="w-full dark:text-slate-300">
+      <ol className="flex h-8 space-x-2">
+        <li className="flex items-center">
+          <Link href="/" title="Back to homepage" className="hover:underline">
+            <HomeIcon className="w-5 h-5 pr-1 dark:text-gray-300" strokeWidth={3} />
+          </Link>
+        </li>
+
+        {trails?.map((item, index) => (
+          <li key={index} className="flex items-center space-x-2">
+            <Icon icon={<ArrowRightIcon />} strokeWidth={3}  className="w-4 h-4 mt-0.5 dark:text-gray-300"/>
+            <Link
+              href={item.href}
+              className={`flex items-center px-1 capitalize ${
+                asPath === item.href ? 'hover:no-underline cursor-default' : 'hover:underline'
+              }`}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+
+        {/* {linkTrail.map((link, index) => (
+          <li key={index} className="flex items-center space-x-2">
+            <Icon
+              icon={<ArrowRightIcon />}
+              className="w-4 h-4  mt-0.5 fill-current dark:text-gray-600"
+            />
+            <Link href={link.link} className="flex items-center px-1 capitalize hover:underline">
+              {link.label}
+            </Link>
+          </li>
+        ))}
+
+        <li className="flex items-center space-x-2">
+          <Icon
+            icon={<ArrowRightIcon />}
+            className="w-4 h-4 mt-0.5 fill-current dark:text-gray-600"
+          />
+          <Link
+            href="#"
+            className="flex items-center px-1 capitalize hover:underline hover:no-underline cursor-default"
+          >
+            Current
+          </Link>
+        </li> */}
+      </ol>
+    </nav>
+  );
+};
+
+export default Breadcrumb;
