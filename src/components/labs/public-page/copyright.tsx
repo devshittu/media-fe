a5bdf48da417363fa8bb41e5616f522a485f12dc
Muhammed Shittu <devshittu@gmail.com>
2023-07-18 23:25:44 +0100

    Modified copyright component and created footer for public pages

--- src/components/labs/public-page/copyright.tsx
+++ src/components/labs/public-page/copyright.tsx
@@ -1,42 +1,29 @@
-import React from 'react'
+import React from 'react';
 import { Link } from '@/components/labs';
-import { COPYRIGHT_TEXT } from '@/config/constants';
 
-const Copyright = () => {
-  return (
-    
-            <div className="flex flex-col-reverse justify-between py-5 border-t border-slate-300 dark:border-slate-700 lg:flex-row">
-              <p className="text-sm">
-                {COPYRIGHT_TEXT}
-              </p>
-              <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row text-sm">
-                <li>
-                  <Link
-                    href="/faq"
-                    className=" transition-colors duration-300 hover:text-cyan-400"
-                  >
-                    F.A.Q
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/privacy"
-                    className=" transition-colors duration-300 hover:text-cyan-400"
-                  >
-                    Privacy Policy
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/terms"
-                    className=" transition-colors duration-300 hover:text-cyan-400"
-                  >
-                    Terms &amp; Conditions
-                  </Link>
-                </li>
-              </ul>
-            </div>
-  )
+interface CopyrightProps {
+  text: string;
+  links: { href: string; label: string }[];
 }
 
-export default Copyright
+const Copyright: React.FC<CopyrightProps> = ({ text, links }) => {
+  return (
+    <div className="flex flex-col-reverse justify-between py-5 border-t border-slate-300 dark:border-slate-700 lg:flex-row">
+      <p className="text-sm">{text}</p>
+      <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row text-sm">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className="transition-colors duration-300 hover:text-cyan-400"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Copyright;
---
+++ src/components/labs/public-page/footer.tsx
@@ -0,0 +1,61 @@
+
+import { AppLogoIcon, Icon } from '@/components/illustrations';
+import React from 'react';
+import { Link } from '@/components/labs';
+import { Button } from '@/components/button';
+import { APP_NAME, COPYRIGHT_TEXT } from '@/config/constants';
+import Copyright from './copyright';
+
+type Category = {
+  name: string;
+  links: {
+    label: string;
+    url: string;
+  }[];
+};
+
+type FooterProps = {
+  categories: Category[];
+};
+
+const Footer: React.FC<FooterProps> = ({ categories }) => {
+
+    const copyrightText = COPYRIGHT_TEXT;
+  const links = [
+    { href: '/faq', label: 'F.A.Q' },
+    { href: '/privacy', label: 'Privacy Policy' },
+    { href: '/terms', label: 'Terms & Conditions' },
+  ];
+  return (
+    <footer className="bg-slate-50 dark:bg-gray-950">
+      <div className="p-4 py-6 mx-auto max-w-screen-xl md:p-8 lg:p-10">
+        <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
+          {categories.map((category, index) => (
+            <div key={index}>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                {category.name}
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400">
+                {category.links.map((link, index) => (
+                  <li key={index} className="mb-4">
+                    <Link href={link.url} className="hover:underline">
+                      {link.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+
+
+
+      <Copyright text={copyrightText} links={links} />
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
