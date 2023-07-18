d4a29129984c433313d3831341d9aba5a38e2514
Muhammed Shittu <devshittu@gmail.com>
2023-07-18 23:33:22 +0100

    Added RootLayout to the _app.tsx but didnt workout more work there.

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { NavProvider, BodyStyleUpdater } from '@/components/blocks/nav/';
+import RootLayout from '@/layouts/root-layout';
 
 type AppProviderProps = {
   children: ReactNode;
@@ -12,8 +13,10 @@ export const AppProvider = ({ children, theme }: AppProviderProps) => {
     <>
       <ThemeProvider attribute="class" forcedTheme={theme || undefined}>
         <NavProvider>
+          {/* <RootLayout> */}
           <BodyStyleUpdater />
           {children}
+          {/* </RootLayout> */}
         </NavProvider>
       </ThemeProvider>
     </>
