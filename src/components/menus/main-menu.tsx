73fb790c47d56f05bed22d35cdbde5a80f2c9846
Muhammed Shittu <devshittu@gmail.com>
2023-07-19 21:15:49 +0100

    Created and implemented a flashcard..

--- src/components/menus/main-menu.tsx
+++ src/components/menus/main-menu.tsx
@@ -1,23 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from '@/components/labs/typography';
 import ThemeSwitch from '@/components/theme-switch/theme-switch';
 import {
   EditIcon,
   HomeIcon,
   SettingsIcon,
-  Icon,
   FileTextIcon,
   LayoutIcon,
   BookmarkIcon,
   HashIcon,
-  MusicIcon,
-  XIcon,
   UserPlusIcon,
   HelpCircleIcon,
 } from '@/components/illustrations';
 import { MenuItem, MenuList } from './menu-list';
 import TourSequence from '../blocks/tour/tour-sequence';
-import { Button } from '../button';
+import { TourPopperType } from '../blocks/tour/tour-popper';
+import { FlashCard } from '../blocks/flash-card';
 const MainMenu = () => {
   const mainMenuList: MenuItem[] = [
     {
@@ -84,72 +82,46 @@ const MainMenu = () => {
   const openHelp = () => {
     setShowHelp(true);
   };
+
   return (
     <>
       <MenuList menu={mainMenuList} />
       <ThemeSwitch />
-      <div
-        id="dropdown-cta"
-        className="p-4 mt-6 rounded-lg bg-blue-50 dark:bg-blue-900"
-        role="alert"
-      >
-        <div className="flex items-center mb-3">
-          <span className="bg-orange-100 text-orange-800 text-sm font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-orange-200 dark:text-orange-900">
-            Beta
-          </span>
-          <button
-            type="button"
-            className="ml-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-900 rounded-lg focus:ring-2 focus:ring-blue-400 p-1 hover:bg-blue-200 inline-flex h-6 w-6 dark:bg-blue-900 dark:text-blue-400 dark:hover:bg-blue-800"
-            data-dismiss-target="#dropdown-cta"
-            aria-label="Close"
-          >
-            <span className="sr-only">Close</span>
-            <Icon icon={<XIcon />} className="w-4 h-4" />
-          </button>
-        </div>
-        <p className="mb-3 text-sm text-blue-800 dark:text-blue-400">
-          Explore our website with the Help button for a quick tour. Discover
-          seamless navigation in just a few clicks!
-        </p>
-        {/* <Link
-          className="text-sm text-blue-800 underline font-medium hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-300"
-          href="#"
-        >
-          Turn new navigation off
-        </Link> */}
+      <FlashCard
+        title="Beta"
+        closeable={false}
+        text="Explore our website with the Help button for a quick tour. Discover
+          seamless navigation in just a few clicks!"
+        actionText="Show Help"
+        actionIcon={<HelpCircleIcon />}
+        onClickAction={openHelp}
+      />
 
-        <Button
-          onClick={openHelp}
-          icon={<HelpCircleIcon />}
-          className="text-sm text-blue-800 underline font-medium hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-300"
-        >
-          Show help
-        </Button>
-        {showHelp && (
-          <TourSequence
-            sequence={[
-              {
-                forElement: '#page-title',
-                text: 'This is where you will find the page title for the current page.',
-              },
-              {
-                forElement: '#app-search',
-                text: 'Search stories here...',
-              },
-              {
-                forElement: '#trendsForYouTitle',
-                text: 'This is where you will find some trends for you',
-              },
-              {
-                forElement: '.App-nowhere',
-                text: 'This help text will never appear',
-              },
-            ]}
-            open={showHelp}
-            onClose={closeHelp}
-          />
-        )}
-      </div>
+      {showHelp && (
+        <TourSequence
+          sequence={[
+            {
+              forElement: '#page-title',
+              text: 'This is where you will find the page title for the current page.',
+            },
+            {
+              forElement: '#app-search',
+              text: 'Search stories here...',
+            },
+            {
+              forElement: '#trendsForYouTitle',
+              text: 'This is where you will find some trends for you',
+            },
+            {
+              forElement: '.App-nowhere',
+              text: 'This help text will never appear',
+            },
+          ]}
+          open={showHelp}
+          onClose={closeHelp}
+          type={TourPopperType.WARNING}
+        />
+      )}
     </>
   );
 };
