7ce862ef5ff46b0c8bff7d60a86c98e411c2748e
Muhammed Shittu <devshittu@gmail.com>
2023-07-19 21:14:12 +0100

    Formatted.

--- src/components/labs/public-page/hero.tsx
+++ src/components/labs/public-page/hero.tsx
@@ -1,13 +1,19 @@
-import Breadcrumb, { BreadcrumbTrail } from '@/components/blocks/breadcrumb/breadcrumb';
+import Breadcrumb, {
+  BreadcrumbTrail,
+} from '@/components/blocks/breadcrumb/breadcrumb';
 import React from 'react';
 
-type HeroProps =  {
+type HeroProps = {
   title: string;
   subtitle?: string;
   breadcrumbTrail?: BreadcrumbTrail[];
-}
+};
 
-const Hero: React.FC<HeroProps> = ({ title, subtitle, breadcrumbTrail }) => {
+export const Hero: React.FC<HeroProps> = ({
+  title,
+  subtitle,
+  breadcrumbTrail,
+}) => {
   return (
     <section className="bg-slate-50 dark:bg-gray-950 text-slate-900 dark:text-slate-100">
       <div className="container mx-auto flex flex-col gap-4 items-start px-4 py-5 md:py-10 md:px-10 lg:px-32 xl:max-w-3xlx">
@@ -17,7 +23,7 @@ const Hero: React.FC<HeroProps> = ({ title, subtitle, breadcrumbTrail }) => {
         <h1 className="text-4xl font-bold md:leading-normal sm:text-5xl">
           {title}
         </h1>
-        {subtitle && <p className="text-lg text-slate-300">{subtitle}</p>}
+        {subtitle && <p className="text-lg text-slate-600">{subtitle}</p>}
       </div>
     </section>
   );
