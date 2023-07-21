062ec68fce364845819790897d3ca973f9a9b941
Muhammed Shittu <devshittu@gmail.com>
2023-07-21 20:57:17 +0100

    Implemented get category to get from mock.

---
+++ src/features/categories/api/get-category.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { apiClient } from '@/lib/api-client';
+
+import { Category } from '../types';
+
+type GetCategoryOptions = {
+  categoryId: string;
+};
+
+export const getCategory = ({
+  categoryId,
+}: GetCategoryOptions): Promise<Category> => {
+  return apiClient.get(
+    `/categories/${categoryId}`
+  );
+};
+
+export const useCategory = ({
+  categoryId,
+}: GetCategoryOptions) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['categories', categoryId],
+    queryFn: () => getCategory({ categoryId }),
+  });
+
+  return { data, isLoading };
+};
