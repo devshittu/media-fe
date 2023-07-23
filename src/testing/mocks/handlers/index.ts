89bc31465a4d4f47dc5d10e89e4ed4555dfb01e2
Muhammed Shittu <devshittu@gmail.com>
2023-07-23 20:13:47 +0100

    Exported hashtag handlers

--- src/testing/mocks/handlers/index.ts
+++ src/testing/mocks/handlers/index.ts
@@ -2,9 +2,11 @@ import { rest } from 'msw';
 import { API_URL } from '@/config/constants';
 import { storiesHandlers } from './stories';
 import { categoriesHandlers } from './categories';
+import { hashtagsHandlers } from './hashtags';
 export const handlers = [
   ...storiesHandlers,
   ...categoriesHandlers,
+  ...hashtagsHandlers,
   rest.get(`${API_URL}/healthcheck`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ healthy: true }));
   }),
