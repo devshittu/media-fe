dcc7ea8fe89754605e45f26c098592f9dcd686e5
Muhammed Shittu <devshittu@gmail.com>
2023-07-23 19:59:32 +0100

    Renamed trigger button name context-menu-toggle on popover

--- src/components/blocks/popover/popover.tsx
+++ src/components/blocks/popover/popover.tsx
@@ -150,6 +150,7 @@ export const PopoverTrigger = React.forwardRef<
     <button
       ref={ref}
       type="button"
+      name="context-menu-toggle"
       // The user can style the trigger based on the state
       data-state={context.open ? 'open' : 'closed'}
       {...context.getReferenceProps(props)}
