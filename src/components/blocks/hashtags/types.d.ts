0e65eeccd50f917683c73361858446119093a6c1
Muhammed Shittu <devshittu@gmail.com>
2023-07-23 19:56:42 +0100

    Refactored and relocated the HashtagItem

--- src/components/blocks/hashtags/types.d.ts
+++ src/components/blocks/hashtags/types.d.ts
@@ -1,11 +1,7 @@
-export type HashtagItem = {
-  hashtagId: string;
-  name: string;
-};
 export type HashtagItemProps = {
   hashtag: HashtagItem;
   closable?: boolean;
 };
 export type HashtagListProps = {
-  data?: HashtagItem[];
+  dataItems?: HashtagItem[];
 };
