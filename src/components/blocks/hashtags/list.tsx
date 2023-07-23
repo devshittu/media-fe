f010e186166a0197c1739ea5dedca911e11c4a8f
Muhammed Shittu <devshittu@gmail.com>
2023-07-23 19:56:06 +0100

    Added data to hashtag-list with useHashtags

--- src/components/blocks/hashtags/list.tsx
+++ src/components/blocks/hashtags/list.tsx
@@ -1,46 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { HashtagListItem } from './list-item';
-import { HashtagItem, HashtagListProps } from './types';
+import { HashtagListProps } from './types';
+import { HashtagItem } from '@/testing';
+import { useHashtags } from '@/features/hashtags';
+import { HashtagsPlaceholder } from './hashtags-placeholder';
 
-const generateRandomHashtagItems = (): HashtagItem[] => {
-  const hashtagItems: HashtagItem[] = [];
+export const HashtagList = React.memo(
+  ({ dataItems = [] }: HashtagListProps) => {
+    const { data, isLoading } = useHashtags({}); // Use data and isLoading directly from the hook
 
-  for (let i = 0; i < 20; i++) {
-    const hashtagId = Math.random().toString(36).substr(2, 9);
-    const nameLength = Math.floor(Math.random() * 18) + 3; // Random length between 3 and 20
-    const name = Math.random().toString(36).substr(2, nameLength);
-    const hashtagItem: HashtagItem = {
-      hashtagId,
-      name,
-    };
+    return (
+      <>
+        {!data && isLoading ? (
+          <HashtagsPlaceholder />
+        ) : (
+          <div className="flex gap-4 flex-wrap">
+            {data.map((hashtag: HashtagItem) => (
+              <HashtagListItem key={hashtag.id} hashtag={hashtag} />
+            ))}
+          </div>
+        )}
+      </>
+    );
+  },
+);
 
-    hashtagItems.push(hashtagItem);
-  }
-
-  return hashtagItems;
-};
-
-export const HashtagList = ({ data = [] }: HashtagListProps) => {
-  const [hashtagData, setHashtagData] = useState<HashtagItem[]>([]);
-
-  useEffect(() => {
-    if (data?.length === 0) {
-      setHashtagData(generateRandomHashtagItems());
-    }
-  }, [data]);
-
-  // Generate random hashtag items if data is empty
-  if (data.length === 0) {
-    data = hashtagData;
-  }
-
-  return (
-    <>
-      <div className="flex gap-4 flex-wrap">
-        {data.map((hashtag: HashtagItem) => (
-          <HashtagListItem key={hashtag.hashtagId} hashtag={hashtag} />
-        ))}
-      </div>
-    </>
-  );
-};
+HashtagList.displayName = 'HashtagList';
