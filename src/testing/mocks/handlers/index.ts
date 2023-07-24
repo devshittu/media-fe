64de92a7677a2e82d8a6ffeb9798ab149ecbe13f
Muhammed Shittu <devshittu@gmail.com>
2023-07-24 21:08:19 +0100

    Added settingsHandlers

--- src/testing/mocks/handlers/index.ts
+++ src/testing/mocks/handlers/index.ts
@@ -3,10 +3,12 @@ import { API_URL } from '@/config/constants';
 import { storiesHandlers } from './stories';
 import { categoriesHandlers } from './categories';
 import { hashtagsHandlers } from './hashtags';
+import { settingsHandlers } from './settings';
 export const handlers = [
   ...storiesHandlers,
   ...categoriesHandlers,
   ...hashtagsHandlers,
+  ...settingsHandlers,
   rest.get(`${API_URL}/healthcheck`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ healthy: true }));
   }),
