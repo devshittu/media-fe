b3b7ba06c0706e7790ed221abf8465b614c4ac24
Muhammed Shittu <devshittu@gmail.com>
2023-07-25 23:40:58 +0100

    Added and exported the getCategoriesHandler

--- src/testing/mocks/handlers/categories.ts
+++ src/testing/mocks/handlers/categories.ts
@@ -1,8 +1,31 @@
 import { rest } from 'msw';
 
-import { API_URL } from '@/config/constants';
+import { API_URL, PAGINATE_STORIES_LIMIT } from '@/config/constants';
 
 import { db } from '../db';
+
+const getCategoriesHandler = rest.get(
+  `${API_URL}/categories`,
+  (req, res, ctx) => {
+    const categoryId = req.params.categoryId as string;
+
+    const page = 1;
+    const pageSize = PAGINATE_STORIES_LIMIT;
+
+    const start = (page - 1) * pageSize;
+    const end = start ? start + pageSize : -1;
+
+    const categories = db.category.getAll();
+
+    if (!categories) {
+      return res(ctx.status(404), ctx.json({ message: 'Not found!' }));
+    }
+
+    //.slice(start, end)
+
+    return res(ctx.delay(300), ctx.status(200), ctx.json(categories));
+  },
+);
 
 const getCategoryHandler = rest.get(
   `${API_URL}/categories/:categoryId`,
@@ -25,4 +48,4 @@ const getCategoryHandler = rest.get(
   },
 );
 
-export const categoriesHandlers = [getCategoryHandler];
+export const categoriesHandlers = [getCategoryHandler, getCategoriesHandler];
