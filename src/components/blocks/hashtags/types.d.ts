269acc173203517710565564ed424414a77e940d
Muhammed Shittu <devshittu@gmail.com>
2023-07-25 23:39:46 +0100

    Deleted because of refactoring.

--- src/components/blocks/hashtags/types.d.ts
+++
@@ -1,7 +0,0 @@
-export type HashtagItemProps = {
-  hashtag: HashtagItem;
-  closable?: boolean;
-};
-export type HashtagListProps = {
-  dataItems?: HashtagItem[];
-};
--- src/components/blocks/stories/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './story-list-item';
-export * from './story-list-item-loading-placeholder';
-export * from './story-list';
-export * from './types.d';
