e9c275477f7e668326ec724e932cd65b09c8739d
Muhammed Shittu <devshittu@gmail.com>
2023-07-25 23:42:26 +0100

    Renamed for  refactoring.

---
+++ src/features/stories/components/types.ts
@@ -0,0 +1,11 @@
+import { StoryItem } from '@/testing';
+
+export type StoryListItemProps = {
+  story: StoryItem;
+  className?: string;
+};
+export type StoryListProps = {
+  data?: StoryItem[];
+  scrollInfinite?: boolean;
+  isLoading?: boolean;
+};
