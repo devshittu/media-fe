bc156ae191e72f22e3d0ac8aaf418c12a87f4acd
Muhammed Shittu <devshittu@gmail.com>
2023-07-27 23:02:54 +0100

    Defined QUERY_KEYS for the react-query calls

---
+++ src/config/query.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_STALE_TIME = 3_600_000;
+
+export const QUERY_KEYS = {
+  WEB_PUSH_NOTIFICATIONS: 'web-push-notifications',
+  GET_CATEGORIES: 'get-categories',
+};
+
+//Path: src/config/query.ts
