735297574abc5c5708e8b89e30be3cf157b2e61e
Muhammed Shittu <devshittu@gmail.com>
2023-07-28 23:51:33 +0100

    Get stories on query-keys.

--- src/config/query.ts
+++ src/config/query.ts
@@ -3,6 +3,7 @@ export const DEFAULT_STALE_TIME = 3_600_000;
 export const QUERY_KEYS = {
   WEB_PUSH_NOTIFICATIONS: 'web-push-notifications',
   GET_CATEGORIES: 'get-categories',
+  GET_STORIES: 'get-stories',
 };
 
 //Path: src/config/query.ts
