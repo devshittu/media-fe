263ef51fc4fe4c4f0c916659f37fd9514679e5c0
Muhammed Shittu <devshittu@gmail.com>
2023-07-28 23:58:44 +0100

    Get page, limit and hashtag.

--- src/features/stories/api/get-stories.ts
+++ src/features/stories/api/get-stories.ts
@@ -3,10 +3,15 @@ import { useQuery } from '@tanstack/react-query';
 import { apiClient } from '@/lib/api-client';
 
 import { Story } from '../types';
+import { QUERY_KEYS } from '@/config/query';
+const {GET_STORIES} = QUERY_KEYS
 
 type GetStoriesOptions = {
   params?: {
     categoryId?: string | undefined;
+    page?: number | undefined;
+    limit?: number | undefined;
+    hashtag?: string | undefined;
   };
 };
 
@@ -18,7 +23,7 @@ export const getStories = ({ params }: GetStoriesOptions): Promise<Story[]> => {
 
 export const useStories = ({ params }: GetStoriesOptions) => {
   const { data, isFetching, isFetched } = useQuery({
-    queryKey: ['stories', params],
+    queryKey: [GET_STORIES, params],
     queryFn: () => getStories({ params }),
     // enabled: !!params?.categoryId,
     initialData: [],
