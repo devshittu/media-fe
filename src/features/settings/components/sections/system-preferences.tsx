f43926823a76f18fb1731fe0d8ac61e1438a40ea
Muhammed Shittu <devshittu@gmail.com>
2023-07-29 23:58:25 +0100

    Added system preferences settings sections features

---
+++ src/features/settings/components/sections/system-preferences.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { SettingsFieldset } from '../settings-fieldset';
+import { SettingsField } from '../settings-field';
+import ThemeSwitch from '@/components/theme-switch/theme-switch';
+
+export const SystemPreferences = () => {
+  return (
+    <SettingsFieldset
+      id="SystemSettings"
+      title="System Settings"
+      description="See information about your account, download an archive of your data, or learn about your account deactivation options"
+    >
+      <SettingsField
+        id="theme"
+        title="Theme"
+        description="Set your theme according to your preference"
+      >
+        <div className="w-1/3 md:block">
+          <ThemeSwitch showLabel className="items-start" />
+        </div>
+      </SettingsField>
+    </SettingsFieldset>
+  );
+};
