02f6a8541871771448620fc5d25bf7bac7c50f21
Muhammed Shittu <devshittu@gmail.com>
2023-07-29 23:50:09 +0100

    Added get-categories features

---
+++ src/features/categories/api/get-categories.ts
@@ -0,0 +1,44 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { apiClient } from '@/lib/api-client';
+
+import { Category } from '../types';
+import { DEFAULT_STALE_TIME, QUERY_KEYS } from '@/config/query';
+const { GET_CATEGORIES } = QUERY_KEYS;
+type GetCategoriesOptions = {
+  params?: {
+    categoryId?: string | undefined;
+  };
+};
+
+export const getCategories = ({
+  params,
+}: GetCategoriesOptions): Promise<Category[]> => {
+  return apiClient.get(`/categories`, { params });
+};
+
+export const useCategories = ({ params }: GetCategoriesOptions) => {
+  const queryClient = useQueryClient();
+
+  const { data, isFetching, isFetched } = useQuery({
+    queryKey: [GET_CATEGORIES, params],
+    queryFn: () => getCategories({ params }),
+    initialData: () => {
+      // Check if we have anything in cache and return that, otherwise get initial data
+      const cachedData = queryClient.getQueryData<Category[] | undefined>([
+        GET_CATEGORIES,
+      ]);
+      console.log('cachedData', cachedData);
+      if (cachedData) {
+        return cachedData;
+      }
+
+      return [];
+    },
+    cacheTime: 1000 * 10,
+    // staleTime: DEFAULT_STALE_TIME,
+  });
+
+  return { data, isLoading: isFetching && !isFetched };
+};
+
+//Path: src/features/categories/api/get-categories.ts
