bd4c0a4f3486a48c3d10e3069476437563919ea6
Muhammed Shittu <devshittu@gmail.com>
2023-07-30 23:53:04 +0100

    Applied StoriesPageContainer

--- src/pages/stories/index.tsx
+++ src/pages/stories/index.tsx
@@ -1,84 +1,50 @@
-import { SidePanel } from '@/components/blocks/side-panel';
-import { Toast } from '@/components/blocks/toast';
 import { ReactElement, useRef } from 'react';
 import UserLayout from '@/layouts/user-layout';
 import { StoriesPageHeader } from '@/components/blocks/headers';
 import { StoryList } from '@/features/stories/components';
-
-// import { getAllStories } from '@/testing/test-data';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
-import { StoryItem } from '@/testing';
-import { getStories, useStories } from '@/features/stories';
+import { Story, getStories, useStories } from '@/features/stories';
 import { PAGINATE_STORIES_LIMIT } from '@/config/constants';
+import StoriesPageContainer from '@/features/stories/components/stories-page-container/stories-page-container';
 type PublicStoriesPageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
 >;
-const Index = ({ stories }: PublicStoriesPageProps) => {
+const StoriesPage = ({ stories }: PublicStoriesPageProps) => {
   const storiesFromUse = useStories({
     params: {
       page: 1,
-      limit: PAGINATE_STORIES_LIMIT,
+      per_page: PAGINATE_STORIES_LIMIT,
     },
   });
-  const handleToastClose = () => {
-    console.log('Toast closed');
-  };
-  const ShowToast = () => {
-    const notify = new Toast({
-      message: 'Hello, world!',
-      position: 'bottom-center',
-      type: 'success',
-      onClose: () => {
-        // Handle close event
-        handleToastClose();
-      },
-      duration: 5000,
-    });
-
-    notify.open();
-  };
-
   return (
-    <div
-      className={`flex relative min-h-full w-full min-w-0 m-0 items-stretch grow flex-row p-0 justify-between shrink-0 basis-auto `}
-    >
-      <div
-        className={`flex flex-col flex-shrink-0 basis-auto flex-grow relative p-0 min-w-0 min-h-0 m-0 border-x max-w-full lg:max-w-[640px] box-border border-slate-100 dark:border-slate-800`}
-      >
+    <>
         <StoriesPageHeader pageTitle="Home" />
-        <>
-          {/* <StoryList data={stories} scrollInfinite /> */}
           <StoryList
-            data={storiesFromUse.data}
+            data={storiesFromUse.data?.stories}
+            totalPages={storiesFromUse.data?.total_pages}
             isLoading={storiesFromUse.isLoading}
-            // scrollInfinite
+            scrollInfinite
           />
-        </>
-      </div>
-      <div
-        className={`relative hidden lg:flex p-0 z-0 min-w-0 min-h-0 box-border my-0 ml-0 flex-shrink-0 basis-auto flex-col border-0 w-[350px] items-stretch`}
-      >
-        <SidePanel />
-      </div>
-    </div>
+      </>
   );
 };
 
-Index.getLayout = function getLayout(page: ReactElement) {
-  return <UserLayout>{page}</UserLayout>;
+StoriesPage.getLayout = function getLayout(page: ReactElement) {
+  return <UserLayout><StoriesPageContainer>{page}</StoriesPageContainer></UserLayout>;
 };
 
 export const getServerSideProps = async ({
   params,
 }: GetServerSidePropsContext) => {
-  // console.log('getServerSideProps:params:// ', params);
   const categoryId = params?.categoryId as string;
   // const stories = await getAllStories().catch(() => [] as StoryItem[]);
   const stories = await getStories({
     params: {
       categoryId: categoryId,
+      page: 1,
+      per_page: PAGINATE_STORIES_LIMIT,
     },
-  }).catch(() => [] as StoryItem[]);
+  }).catch(() => [] as Story[]);
 
   return {
     props: {
@@ -87,4 +53,6 @@ export const getServerSideProps = async ({
   };
 };
 
-export default Index;
+export default StoriesPage;
+
+//Path: src/pages/stories/index.tsx
