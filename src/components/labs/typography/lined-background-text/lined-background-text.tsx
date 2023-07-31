00af30e58c6248dcad00877b8b05c4d16a92d5e3
Muhammed Shittu <devshittu@gmail.com>
2023-07-31 22:29:57 +0100

    Updated bg-color

--- src/components/labs/typography/lined-background-text/lined-background-text.tsx
+++ src/components/labs/typography/lined-background-text/lined-background-text.tsx
@@ -4,7 +4,7 @@ import { TextProps } from '../shared';
 const LinedBackgroundText = ({ children }: TextProps) => {
   return (
     <p className="my-8 text-center relative before:bg-slate-300/100 dark:before:bg-slate-700/100 before:bottom-0 before:content-[''] before:h-[1px] before:left-0 before:absolute before:right-0 before:w-full before:top-[50%]">
-      <span className="relative px-4 bg-white dark:bg-slate-900">
+      <span className="relative px-4 bg-slate-50 dark:bg-slate-900">
         {children}
       </span>
     </p>
