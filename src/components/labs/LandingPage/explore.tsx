ed31d44a7ad0313eff5ee864e0f19e18356ff4f0
Muhammed Shittu <devshittu@gmail.com>
2023-07-31 22:31:08 +0100

    Created explore component for the home page

---
+++ src/components/labs/LandingPage/explore.tsx
@@ -0,0 +1,35 @@
+import { StoriesPageHeader } from '@/components/blocks/headers';
+import { PAGINATE_STORIES_LIMIT } from '@/config/constants';
+import { useStoriesByHashtag } from '@/features/stories';
+import { StoryList } from '@/features/stories/components';
+import { useRouter } from 'next/router';
+import React from 'react'
+type ExploreProps = {
+    hashtag?: string
+}
+export const Explore = ({hashtag = 'Sustainability'}: ExploreProps) => {
+  const storiesByHashtagFromUse = useStoriesByHashtag({
+    params: {
+      page: 1,
+      per_page: PAGINATE_STORIES_LIMIT,
+      hashtag: hashtag
+    },
+  });
+
+  return (
+        <>
+        <StoriesPageHeader pageTitle={`Explore:#${hashtag}`}  />
+
+        <section className='mb-36 flex justify-center w-full'>
+        <section className='max-w-xl md:max-w-2xl'>
+          <StoryList
+            data={storiesByHashtagFromUse.data?.stories}
+            totalPages={storiesByHashtagFromUse.data?.total_pages}
+            isLoading={storiesByHashtagFromUse.isLoading}
+            // scrollInfinite
+          />
+          </section>
+          </section>
+        </>
+  );
+};
