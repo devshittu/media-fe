8847d75968a5395292bf1ba6ab1ed829dd19e393
Muhammed Shittu <devshittu@gmail.com>
2023-08-04 22:15:25 +0100

    Formatted

--- src/features/stories/components/stories-page-container/stories-page-container.tsx
+++ src/features/stories/components/stories-page-container/stories-page-container.tsx
@@ -7,7 +7,6 @@ type StoriesPageContainerProps = {
 const StoriesPageContainer: React.FC<StoriesPageContainerProps> = ({
   children,
 }) => {
-
   return (
     <div
       className={`flex relative min-h-full w-full min-w-0 m-0 items-stretch grow flex-row p-0 justify-between shrink-0 basis-auto `}
