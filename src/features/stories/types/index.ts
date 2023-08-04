68316ba01b6c1a5999126404158118e4dc3a4b91
Muhammed Shittu <devshittu@gmail.com>
2023-08-04 22:15:50 +0100

    Added user object

--- src/features/stories/types/index.ts
+++ src/features/stories/types/index.ts
@@ -1,4 +1,4 @@
-import { Entity } from '@/types';
+import { Entity, NewsChannel, User } from '@/types';
 
 export type Story = Entity & {
   id: string;
@@ -9,6 +9,7 @@ export type Story = Entity & {
   updated_at: string;
   parent_stories: string[];
   children_stories: string[];
+  user: User;
 };
 export type StoryResponse = {
   stories: Story[];
