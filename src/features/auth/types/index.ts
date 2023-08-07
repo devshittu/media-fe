6789b23c9e98943ac4cc6b731da72c82e0a5404c
Muhammed Shittu <devshittu@gmail.com>
2023-08-07 12:43:32 +0100

    Added to a login feature. types

---
+++ src/features/auth/types/index.ts
@@ -0,0 +1,23 @@
+import { Entity } from '@/types';
+
+export type AuthUser = Entity & {
+  email: string;
+  organizationId: string;
+};
+
+export type LoginData = {
+  email: string;
+  password: string;
+};
+
+export type User = {
+  id: number;
+  username: string;
+  email: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+  last_login: string;
+  roles: string[];
+  avatar_url: string;
+};
