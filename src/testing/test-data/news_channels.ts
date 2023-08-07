b6cabeead780f1d2b035927a5f9eb65552784a09
Muhammed Shittu <devshittu@gmail.com>
2023-08-07 12:37:58 +0100

    Created the news_channels database for seeding.

---
+++ src/testing/test-data/news_channels.ts
@@ -0,0 +1,32 @@
+export const news_channels = [
+  {
+    id: '1',
+    name: 'World News Channel',
+    feed_url: 'https://worldnews.com/feed',
+    logo_url: 'https://worldnews.com/logo.png',
+  },
+  {
+    id: '2',
+    name: 'Politics News Channel',
+    feed_url: 'https://politicsnews.com/feed',
+    logo_url: 'https://politicsnews.com/logo.png',
+  },
+  {
+    id: '3',
+    name: 'Business News Channel',
+    feed_url: 'https://businessnews.com/feed',
+    logo_url: 'https://businessnews.com/logo.png',
+  },
+  {
+    id: '4',
+    name: 'Technology News Channel',
+    feed_url: 'https://technologynews.com/feed',
+    logo_url: 'https://technologynews.com/logo.png',
+  },
+  {
+    id: '5',
+    name: 'Science News Channel',
+    feed_url: 'https://sciencenews.com/feed',
+    logo_url: 'https://sciencenews.com/logo.png',
+  },
+];
