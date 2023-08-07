efc673be176d4bd6c4aae14e891fc6f42e994801
Muhammed Shittu <devshittu@gmail.com>
2023-08-07 12:38:22 +0100

    Created the index.ts export database for seeding.

---
+++ src/testing/test-data/index.ts
@@ -0,0 +1,6 @@
+export { admins } from './admins';
+export { categories } from './categories';
+export { stories } from './stories';
+export { news_channels } from './news_channels';
+export { settings } from './settings';
+export { users } from './users';
