8df32f470df3d5876862da2f422e20e4cb6f5098
Muhammed Shittu <devshittu@gmail.com>
2023-08-07 12:39:22 +0100

    Created the users database for seeding.

---
+++ src/testing/test-data/users.ts
@@ -0,0 +1,28 @@
+export const users = [
+  // {
+  //   id: 'KV4Lv9yUHtNVB42V0ZrFf',
+  //   created_at: 1645628972465,
+  //   email: 'user1@test.com',
+  //   password: 'test@123',
+  //   category_id: 'amYXmIyT9mD9GyO6CCr',
+  // },
+
+  {
+    id: '1',
+    username: 'testuser1',
+    email: 'user1@test.com',
+    password: 'test@123',
+    name: 'User One',
+    created_at: 1679003634000,
+    updated_at: 1679003634000,
+    last_login: 1679003634000,
+    roles: ['editor'],
+    avatar_url: 'https://example.com/user1.png',
+    news_channel: {
+      id: '4',
+      name: 'Technology News Channel',
+      feed_url: 'https://technologynews.com/feed',
+      logo_url: 'https://technologynews.com/logo.png',
+    },
+  },
+];
