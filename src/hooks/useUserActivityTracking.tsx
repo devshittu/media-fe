c66ed12002b6695e87113a38cd1a71d6e5f53c24
Muhammed Shittu <devshittu@gmail.com>
2023-08-10 21:13:36 +0100

    Upgraded to avoid performance issues. It now gather the metrics and print them every 5secons

--- src/hooks/useUserActivityTracking.tsx
+++ src/hooks/useUserActivityTracking.tsx
@@ -1,8 +1,89 @@
+// import { useEffect, useState, useRef, RefObject } from 'react';
+
+// export type UserActivityOptions = {
+//   saveMetrics: (metrics: any) => void;
+//   storyId: string; // assuming storyId is a string, change type if needed
+// };
+
+// export type ActivityMetrics = {
+//   [storyId: string]: {
+//     enterCount: number;
+//     timeInView: number;
+//   };
+// };
+
+// export function useUserActivityTracking(
+//   options: UserActivityOptions,
+// ): RefObject<HTMLDivElement> {
+//   const activityRef = useRef<HTMLDivElement>(null);
+//   const [metrics, setMetrics] = useState<ActivityMetrics>({});
+
+//   useEffect(() => {
+//     let startTime: number | null = null;
+
+//     const handleVisibilityChange = (entries: IntersectionObserverEntry[]) => {
+//       const entry = entries[0];
+
+//       if (entry.isIntersecting) {
+//         // Start the timer if item becomes visible
+//         startTime = Date.now();
+//       } else if (startTime) {
+//         // If item becomes invisible, calculate the time spent
+//         const timeSpent = Date.now() - startTime;
+
+//         setMetrics((prevMetrics) => {
+//           const prevTime = prevMetrics[options.storyId]?.timeInView || 0;
+//           const prevEnterCount = prevMetrics[options.storyId]?.enterCount || 0;
+//           return {
+//             ...prevMetrics,
+//             [options.storyId]: {
+//               timeInView: prevTime + timeSpent,
+//               enterCount: prevEnterCount + 1,
+//             },
+//           };
+//         });
+
+//         startTime = null;
+//       }
+//     };
+
+//     const observer = new IntersectionObserver(handleVisibilityChange, {
+//       threshold: 0.75, // Adjust this value as needed
+//     });
+
+//     const currentRef = activityRef.current; // Store the current ref value in a constant
+
+//     if (currentRef) {
+//       observer.observe(currentRef);
+//     }
+
+//     return () => {
+//       if (currentRef) {
+//         observer.unobserve(currentRef);
+//       }
+//     };
+//   }, [activityRef, options.storyId]);
+
+//   useEffect(() => {
+//     if (metrics[options.storyId]) {
+//       // Call your saveMetrics or any other function with the updated metrics
+//       options.saveMetrics({ [options.storyId]: metrics[options.storyId] });
+//     }
+//   }, [metrics, options]);
+
+//   return activityRef;
+// }
+// //Path: src/hooks/useUserActivityTracking.tsx
+
+
+
+
+
 import { useEffect, useState, useRef, RefObject } from 'react';
 
 export type UserActivityOptions = {
   saveMetrics: (metrics: any) => void;
-  storyId: string; // assuming storyId is a string, change type if needed
+  storyId: string;
 };
 
 export type ActivityMetrics = {
@@ -12,11 +93,23 @@ export type ActivityMetrics = {
   };
 };
 
+// Simple throttle function
+function throttle(func: Function, delay: number) {
+  let lastCall = 0;
+  return function (...args: any[]) {
+    const now = Date.now();
+    if (now - lastCall < delay) return;
+    lastCall = now;
+    return func(...args);
+  };
+}
+
 export function useUserActivityTracking(
   options: UserActivityOptions,
 ): RefObject<HTMLDivElement> {
   const activityRef = useRef<HTMLDivElement>(null);
   const [metrics, setMetrics] = useState<ActivityMetrics>({});
+  const batchedMetrics = useRef<ActivityMetrics>({});
 
   useEffect(() => {
     let startTime: number | null = null;
@@ -25,33 +118,31 @@ export function useUserActivityTracking(
       const entry = entries[0];
 
       if (entry.isIntersecting) {
-        // Start the timer if item becomes visible
         startTime = Date.now();
       } else if (startTime) {
-        // If item becomes invisible, calculate the time spent
         const timeSpent = Date.now() - startTime;
+        const prevTime = batchedMetrics.current[options.storyId]?.timeInView || 0;
+        const prevEnterCount = batchedMetrics.current[options.storyId]?.enterCount || 0;
 
-        setMetrics((prevMetrics) => {
-          const prevTime = prevMetrics[options.storyId]?.timeInView || 0;
-          const prevEnterCount = prevMetrics[options.storyId]?.enterCount || 0;
-          return {
-            ...prevMetrics,
-            [options.storyId]: {
-              timeInView: prevTime + timeSpent,
-              enterCount: prevEnterCount + 1,
-            },
-          };
-        });
+        batchedMetrics.current = {
+          ...batchedMetrics.current,
+          [options.storyId]: {
+            timeInView: prevTime + timeSpent,
+            enterCount: prevEnterCount + 1,
+          },
+        };
 
         startTime = null;
       }
     };
 
-    const observer = new IntersectionObserver(handleVisibilityChange, {
-      threshold: 0.75, // Adjust this value as needed
+    const throttledVisibilityChange = throttle(handleVisibilityChange, 500); // Throttle to once every 500ms
+
+    const observer = new IntersectionObserver(throttledVisibilityChange, {
+      threshold: 0.75,
     });
 
-    const currentRef = activityRef.current; // Store the current ref value in a constant
+    const currentRef = activityRef.current;
 
     if (currentRef) {
       observer.observe(currentRef);
@@ -65,12 +156,26 @@ export function useUserActivityTracking(
   }, [activityRef, options.storyId]);
 
   useEffect(() => {
+    const interval = setInterval(() => {
+      if (Object.keys(batchedMetrics.current).length) {
+        setMetrics((prev) => ({
+          ...prev,
+          ...batchedMetrics.current,
+        }));
+        batchedMetrics.current = {};
+      }
+    }, 5000); // Batch updates every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
     if (metrics[options.storyId]) {
-      // Call your saveMetrics or any other function with the updated metrics
       options.saveMetrics({ [options.storyId]: metrics[options.storyId] });
     }
   }, [metrics, options]);
 
   return activityRef;
 }
+
 //Path: src/hooks/useUserActivityTracking.tsx
