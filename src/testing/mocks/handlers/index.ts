0078ce267899b2bb2b9dfcadf29f01dd59b6f2b0
Muhammed Shittu <devshittu@gmail.com>
2023-08-11 23:54:09 +0100

    Added bookmarksHandlers to all handlers

--- src/testing/mocks/handlers/index.ts
+++ src/testing/mocks/handlers/index.ts
@@ -4,10 +4,12 @@ import { storiesHandlers } from './stories';
 import { categoriesHandlers } from './categories';
 import { hashtagsHandlers } from './hashtags';
 import { settingsHandlers } from './settings';
+import { bookmarksHandlers } from './bookmarks';
 export const handlers = [
   ...storiesHandlers,
   ...categoriesHandlers,
   ...hashtagsHandlers,
+  ...bookmarksHandlers,
   ...settingsHandlers,
   rest.get(`${API_URL}/healthcheck`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ healthy: true }));
