cf83c6aa8a5ea810c339ed96c003e3105c6b9d25
Muhammed Shittu <devshittu@gmail.com>
2023-08-11 23:47:07 +0100

    Exported the stories feature

--- src/features/stories/index.ts
+++ src/features/stories/index.ts
@@ -3,11 +3,8 @@ export * from './api/get-story';
 export * from './api/get-stories';
 export * from './api/get-stories-by-hashtag';
 
-// // components:
-// export * from './components/create-story-form';
-// export * from './components/dashboard-story-info';
-// export * from './components/stories-list';
-// export * from './components/public-story-info';
+// components:
+export * from './components/';
 
 // types:
 export * from './types';
