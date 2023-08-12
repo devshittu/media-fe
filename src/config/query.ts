7432290c63744fde86e490dee48b8f2981ede1aa
Muhammed Shittu <devshittu@gmail.com>
2023-08-12 23:56:18 +0100

    Added GET_BOOKMARKS and GET_HASHTAGS

--- src/config/query.ts
+++ src/config/query.ts
@@ -4,6 +4,8 @@ export const QUERY_KEYS = {
   WEB_PUSH_NOTIFICATIONS: 'web-push-notifications',
   GET_CATEGORIES: 'get-categories',
   GET_STORIES: 'get-stories',
+  GET_BOOKMARKS: 'get-bookmarks',
+  GET_HASHTAGS: 'get-hashtags',
 };
 
 //Path: src/config/query.ts
