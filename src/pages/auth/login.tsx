6585d095a5db14a088a412216df3eed1ab5441c7
Muhammed Shittu <devshittu@gmail.com>
2023-08-16 21:47:58 +0100

    Added success notification

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -3,12 +3,20 @@ import { ReactElement } from 'react';
 
 import { Seo } from '@/components/seo';
 import { LoginForm } from '@/features/auth';
+import { useNotifications } from '@/stores/notifications';
 // import { AuthLayout } from '@/layouts/auth-layout';
 
 const LoginPage = () => {
   const router = useRouter();
+  const { showNotification } = useNotifications();
 
   const onSuccess = () => {
+    showNotification({
+      type: 'success',
+      title: 'Success',
+      duration: 5000,
+      message: 'Job Created!',
+    });
     const redirect = router.query.redirect as string;
     router.replace(redirect || '/stories');
   };
