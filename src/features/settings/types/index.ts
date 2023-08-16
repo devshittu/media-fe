df14ce48fcb833558731eda6b038e6cd5a1b5984
Muhammed Shittu <devshittu@gmail.com>
2023-08-16 21:49:13 +0100

    Splitted out the Setting datastructure.

--- src/features/settings/types/index.ts
+++ src/features/settings/types/index.ts
@@ -1,21 +1,33 @@
 export type Setting = {
   id: string;
   user_id: string;
-  theme: string;
-  language: string;
-  account_details: {
-    display_name: string;
-    email: string;
-  };
-  notifications: {
-    email: {
-      account: number; // email about the user's account
-      marketing: number; // email about the marketing, products and promotions
-      updates: number; // newly on-boarded features/announcements
-    };
-  };
+  system_settings: SystemSettingsData;
+  account_settings: AccountSettingsData;
+  notification_settings: SettingNotification;
   favorite_categories: string[];
   last_updated: number;
   created_at: number;
   updated_at: number;
+};
+export type SettingNotification = {
+  email: EmailNotificationSettingsData;
+  short_message_service?: SMSNotificationSettingsData;
+};
+export type EmailNotificationSettingsData = {
+  account: number; // email about the user's account
+  marketing: number; // email about the marketing, products and promotions
+  updates: number; // newly on-boarded features/announcements
+};
+export type SMSNotificationSettingsData = {
+  account: number; // sms about the user's account
+  marketing: number; // sms about the marketing, products and promotions
+  updates: number; // newly on-boarded features/announcements
+};
+export type AccountSettingsData = {
+  display_name: string;
+  email: string;
+};
+export type SystemSettingsData = {
+  theme: string;
+  language: string;
 };
