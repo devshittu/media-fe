18aef2d0fde65d7c23348c6cfdb5ce5ad808cadb
Muhammed Shittu <devshittu@gmail.com>
2023-08-17 23:51:45 +0100

    Formatted

--- src/features/settings/components/sections/personal-preferences.tsx
+++ src/features/settings/components/sections/personal-preferences.tsx
@@ -1,50 +1,13 @@
 import React from 'react';
-import { SettingsFieldset } from '../settings-fieldset';
-import { SettingsField } from '../settings-field';
+import { SettingsField, SettingsFieldset } from '../blocks/';
 import { Category, useCategories } from '@/features/categories';
 import CustomCheckboxGroup, {
   Option,
 } from '@/components/form/custom-checkbox-group';
 import { Loading } from '@/components/loading';
-import { getObjectsByIds, updateDeep } from '@/utils';
+import { getObjectsByIds } from '@/utils';
 
-//TODO remove Example usage:
-// const originalData = {
-//   id: '1',
-//   user_id: '1',
-//   theme: 'system',
-//   language: 'en',
-//   account_details: {
-//     display_name: 'John Doe',
-//     email: 'john.doe@example.com',
-//   },
-//   notifications: { email: { account: 1, marketing: 1, updates: 0 } },
-//   favorite_categories: [
-//     '1',
-//     '2',
-//     '3',
-//     '4',
-//     '5',
-//     '6',
-//     '7',
-//     '8',
-//     '9',
-//     '10',
-//     '11',
-//   ],
-//   last_updated: 1690123521,
-//   created_at: 1690123521,
-//   updated_at: 1690123521,
-// };
-
-// const updatedData = updateDeep(originalData, {
-//   account_details: {
-//     display_name: 'Jane Smitha',
-//   },
-//   notifications: { email: { marketing: 0 } },
-// });
-
-// console.log('updatedData', updatedData);
+import { useForm, Controller, FieldError } from 'react-hook-form';
 
 export type PersonalPreferencesProps = {
   userSelectedCategoriesId: string[];
@@ -53,14 +16,13 @@ export type PersonalPreferencesProps = {
 export const PersonalPreferences = ({
   userSelectedCategoriesId = [],
 }: PersonalPreferencesProps) => {
-  const dataFromUseCategories = useCategories({});
-  const handleCustomCheckboxGroupChange = (
-    selectedOptions: Option<Category>[],
-  ) => {
-    // Handle the change of selected options here
-    // e.g., update the state or perform other actions
-    console.table(selectedOptions);
+  const { handleSubmit, control, formState } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data.checkboxGroup); // This will print the selected options
   };
+
+  const dataFromUseCategories = useCategories({});
 
   const DisplayComponent = (option: Category) => (
     <>
@@ -78,33 +40,54 @@ export const PersonalPreferences = ({
   if (dataFromUseCategories.isLoading) {
     return <Loading />;
   }
+
   const allCategories = dataFromUseCategories.data as Option<Category>[];
 
   console.log('categories', userSelectedCategoriesId);
+
   const initCategories = getObjectsByIds(
     allCategories,
     userSelectedCategoriesId,
   );
 
   return (
-    <SettingsFieldset
-      id="UserPreferences"
-      title="Personal Preferences "
-      description="Customize your experience! Set your preferred theme, language, and notification preferences in the Settings section."
-    >
-      <SettingsField
-        id="fav_categories"
-        title="Favorite Categories"
-        description="Personalize your news feed! Choose from various topics like World News, Technology, Sports, and more to get updates tailored to your interests. Settings made easy!"
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <SettingsFieldset
+        id="UserPreferences"
+        title="Personal Preferences"
+        description="Customize your experience! Set your preferred theme, language, and notification preferences in the Settings section."
       >
-        <CustomCheckboxGroup<Category, Category>
-          initialSelectedOptions={initCategories}
-          options={allCategories}
-          onChange={handleCustomCheckboxGroupChange}
-          renderDisplayComponent={DisplayComponent}
-        />
-      </SettingsField>
-    </SettingsFieldset>
+        <SettingsField
+          id="fav_categories"
+          title="Favorite Categories"
+          description="Personalize your news feed! Choose from various topics like World News, Technology, Sports, and more to get updates tailored to your interests. Settings made easy!"
+        >
+          <Controller
+            name="checkboxGroup"
+            control={control}
+            defaultValue={initCategories}
+            rules={{
+              validate: (value: Option<any>[]) =>
+                value.length > 0 || 'You must select at least one option.',
+            }}
+            render={({ field: { onChange, value } }) => (
+              <CustomCheckboxGroup<Category, Category>
+                options={allCategories}
+                initialSelectedOptions={value}
+                onChange={onChange}
+                renderDisplayComponent={DisplayComponent}
+                error={
+                  formState.errors.checkboxGroup?.message
+                    ? (formState.errors.checkboxGroup as FieldError)
+                    : null
+                }
+              />
+            )}
+          />
+        </SettingsField>
+      </SettingsFieldset>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 //Path: src/features/settings/components/sections/personal-preferences.tsx
