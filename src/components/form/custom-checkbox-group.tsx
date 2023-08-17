c282d8eed7f5b146d4b8278c6739706a442e3c85
Muhammed Shittu <devshittu@gmail.com>
2023-08-17 23:47:29 +0100

    Added error on CustomCheckboxGroup

--- src/components/form/custom-checkbox-group.tsx
+++ src/components/form/custom-checkbox-group.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import CustomCheckbox from './custom-checkbox';
+import { ChangeHandler, FieldError, UseFormRegister } from 'react-hook-form';
 
 export type Option<T> = T & {
   id: string;
@@ -10,7 +11,9 @@ type CustomCheckboxGroupProps<T, P> = {
   options: Option<T>[];
   initialSelectedOptions?: Option<T>[]; // Make initialSelectedOptions optional
   onChange: (selectedOptions: Option<T>[]) => void;
+  // onChange?: ChangeHandler;
   renderDisplayComponent?: (option: T) => JSX.Element; // Custom render function for display
+  error?: FieldError | null;
 };
 
 function CustomCheckboxGroup<T, P>({
@@ -18,6 +21,7 @@ function CustomCheckboxGroup<T, P>({
   initialSelectedOptions = [],
   onChange,
   renderDisplayComponent,
+  error,
 }: CustomCheckboxGroupProps<T, P>) {
   const [selectedOptions, setSelectedOptions] = useState<Option<T>[]>(
     initialSelectedOptions,
@@ -35,19 +39,23 @@ function CustomCheckboxGroup<T, P>({
   };
 
   return (
-    <ul className="grid w-full gap-2 grid-cols-2 md:gap-6  md:grid-cols-3">
-      {options.map((option) => (
-        <CustomCheckbox
-          key={option.id}
-          option={option}
-          isChecked={selectedOptions.some(
-            (selectedOption) => selectedOption.id === option.id,
-          )}
-          onChange={(isChecked) => handleCheckboxChange(option, isChecked)}
-          renderDisplayComponent={renderDisplayComponent}
-        />
-      ))}
-    </ul>
+    <>
+      <ul className="grid w-full gap-2 grid-cols-2 md:gap-6  md:grid-cols-3">
+        {options.map((option) => (
+          <CustomCheckbox
+            key={option.id}
+            option={option}
+            isChecked={selectedOptions.some(
+              (selectedOption) => selectedOption.id === option.id,
+            )}
+            onChange={(isChecked) => handleCheckboxChange(option, isChecked)}
+            renderDisplayComponent={renderDisplayComponent}
+          />
+        ))}
+      </ul>
+
+      {error && <div className="text-red-500">{error.message}</div>}
+    </>
   );
 }
 
