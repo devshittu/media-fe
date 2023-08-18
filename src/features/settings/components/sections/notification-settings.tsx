d2c9bdea89d3532ade535713b768d04b92547ee9
Muhammed Shittu <devshittu@gmail.com>
2023-08-18 22:49:15 +0100

    Applied SettingsFieldsetFooter

--- src/features/settings/components/sections/notification-settings.tsx
+++ src/features/settings/components/sections/notification-settings.tsx
@@ -1,18 +1,64 @@
-import React from 'react';
-import { SettingsField, SettingsFieldset } from '../blocks';
-import { InputField } from '@/components';
-import { Setting } from '../../types';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import {
+  SettingsField,
+  SettingsFieldset,
+  SettingsFieldsetFooter,
+} from '../blocks';
+import { InputField, ToggleSwitch } from '@/components';
+import { NotificationSettingsData, Setting } from '../../types';
+import { useForm, Controller } from 'react-hook-form';
 import { Button } from '@/components/button';
+import { SettingsSectionProps } from '../types';
+import { useUpdateUserSettings } from '../../api/update-user-settings';
+import {
+  NotificationPosition,
+  NotificationType,
+  useNotifications,
+} from '@/stores/notifications';
+import { updateDeep } from '@/utils';
 
-export const NotificationSettings = () => {
-  // const login = useLogin({ onSuccess });
-  let login = { submit: (d: any) => login };
-  const { register, handleSubmit, formState } = useForm<Setting>();
-  const onSubmit = (data: Setting) => {
-    console.log(data);
-    login.submit(data);
+export const NotificationSettings = ({
+  initialSettingValues,
+}: // onSuccess,
+SettingsSectionProps) => {
+  const { showNotification } = useNotifications();
+  const defaultSettings: NotificationSettingsData = {
+    account: 1,
+    marketing: 1,
+    updates: 1,
   };
+
+  const [localSettings, setLocalSettings] = useState<NotificationSettingsData>(
+    initialSettingValues?.notification_settings.email || defaultSettings,
+  );
+
+  const onSuccess = () => {
+    showNotification(
+      {
+        type: NotificationType.SUCCESS,
+        title: 'Success',
+        duration: 5000, // duration
+        message: 'Settings updated!',
+      },
+      { position: NotificationPosition.BOTTOM_RIGHT },
+    );
+  };
+
+  const updateSettings = useUpdateUserSettings({ onSuccess });
+  const { register, handleSubmit, formState, control, watch } =
+    useForm<NotificationSettingsData>({
+      defaultValues: localSettings,
+    });
+  const onSubmit = (data: NotificationSettingsData) => {
+    console.log('data:', data);
+    const updatedData = updateDeep(initialSettingValues, {
+      notification_settings: { email: data },
+    });
+    updateSettings.submit(updatedData);
+  };
+  const isAccountChecked = watch('account');
+  const isMarketingChecked = watch('marketing');
+  const isProductChecked = watch('updates');
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
@@ -22,42 +68,92 @@ export const NotificationSettings = () => {
           description="Select the kinds of notifications you get about your activities, interests, and recommendations."
         >
           <SettingsField
-            id="myField"
-            fieldType="checkbox"
-            title="Who haven’t confirmed their phone number"
-            description="See information about your account, download an archive of your data, or learn about your account deactivation options"
+            id="accountField"
+            // fieldType="checkbox"
+            title="Account notifications"
+            description="Get notifications about your activities, interests, and recommendations"
           >
-            <InputField
-              label="Email"
-              type="email"
-              {...register('email', { required: 'Required' })}
-              error={formState.errors['email']}
+            <Controller
+              name="account"
+              control={control}
+              defaultValue={defaultSettings.account}
+              render={({ field }) => (
+                <ToggleSwitch
+                  id="accountToggle"
+                  leftLabel="Off"
+                  rightLabel="On"
+                  checked={field.value}
+                  {...field}
+                  onChange={(value) => field.onChange(value)} // This will pass 1 or 0 to the form
+                />
+              )}
             />
+
+            <p>Toggle is {isAccountChecked ? 'ON' : 'OFF'}</p>
           </SettingsField>
           <SettingsField
-            id="myField2"
-            fieldType="text"
-            fieldName="info"
-            fieldPlaceholder="Enter your email"
-            title="Account Email"
-            description="See information about your account, download an archive of your data, or learn about your account deactivation options"
-          />
+            id="marketingField"
+            // fieldType="checkbox"
+            title="Marketting notification"
+            description="Get a notification about marketting information"
+          >
+            <Controller
+              name="marketing"
+              control={control}
+              defaultValue={defaultSettings.marketing}
+              render={({ field }) => (
+                <ToggleSwitch
+                  id="marketingToggle"
+                  leftLabel="Off"
+                  rightLabel="On"
+                  checked={field.value}
+                  {...field}
+                  onChange={(value) => field.onChange(value)} // This will pass 1 or 0 to the form
+                />
+              )}
+            />
+
+            <p>Toggle is {isMarketingChecked ? 'ON' : 'OFF'}</p>
+          </SettingsField>
           <SettingsField
-            id="myField3"
-            fieldType="textarea"
-            fieldPlaceholder="Enter your long text here..."
-            title="Who haven’t confirmed their phone number"
-            description="See information about your account, download an archive of your data, or learn about your account deactivation options"
-          ></SettingsField>
+            id="updatesField"
+            // fieldType="checkbox"
+            title="Updates notifications"
+            description="Get updates about our products and services..."
+          >
+            <Controller
+              name="updates"
+              control={control}
+              defaultValue={defaultSettings.updates}
+              render={({ field }) => (
+                <ToggleSwitch
+                  id="updatesToggle"
+                  leftLabel="Off"
+                  rightLabel="On"
+                  checked={field.value}
+                  {...field}
+                  onChange={(value) => field.onChange(value)} // This will pass 1 or 0 to the form
+                />
+              )}
+            />
+
+            <p>Toggle is {isProductChecked ? 'ON' : 'OFF'}</p>
+          </SettingsField>
+          <SettingsFieldsetFooter>
+            <Button
+              loading={!!updateSettings.isLoading}
+              disabled={updateSettings.isLoading}
+              nativeType="submit"
+              size="large"
+              type="primary"
+            >
+              Update settings
+            </Button>
+          </SettingsFieldsetFooter>
         </SettingsFieldset>
-        <Button
-          loading={!!login.isLoading}
-          disabled={login.isLoading}
-          nativeType="submit"
-        >
-          Log in
-        </Button>
       </form>
     </>
   );
 };
+
+//Path: src/features/settings/components/sections/notification-settings.tsx
