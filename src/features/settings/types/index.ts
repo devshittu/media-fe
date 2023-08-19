3b96d2ebe4fdf7836ff406f7621990fa85b6f211
Muhammed Shittu <devshittu@gmail.com>
2023-08-19 23:56:03 +0100

    Updated personal_settings

--- src/features/settings/types/index.ts
+++ src/features/settings/types/index.ts
@@ -1,28 +1,30 @@
+import { Category } from '@/features/categories';
+
 export type Setting = {
   id: string;
   user_id: string;
   system_settings: SystemSettingsData;
   account_settings: AccountSettingsData;
   notification_settings: SettingNotification;
-  favorite_categories: string[];
+  personal_settings: PersonalSettingsData;
   last_updated: number;
   created_at: number;
   updated_at: number;
 };
 export type SettingNotification = {
-  email: EmailNotificationSettingsData;
-  short_message_service?: SMSNotificationSettingsData;
+  email: NotificationSettingsData;
+  // short_message_service?: SMSNotificationSettingsData;
 };
-export type EmailNotificationSettingsData = {
+export type NotificationSettingsData = {
   account: number; // email about the user's account
   marketing: number; // email about the marketing, products and promotions
   updates: number; // newly on-boarded features/announcements
 };
-export type SMSNotificationSettingsData = {
-  account: number; // sms about the user's account
-  marketing: number; // sms about the marketing, products and promotions
-  updates: number; // newly on-boarded features/announcements
-};
+// export type SMSNotificationSettingsData = {
+//   account: number; // sms about the user's account
+//   marketing: number; // sms about the marketing, products and promotions
+//   updates: number; // newly on-boarded features/announcements
+// };
 export type AccountSettingsData = {
   display_name: string;
   email: string;
@@ -30,4 +32,8 @@ export type AccountSettingsData = {
 export type SystemSettingsData = {
   theme: string;
   language: string;
+};
+
+export type PersonalSettingsData = {
+  favorite_categories: (string | Category)[];
 };
