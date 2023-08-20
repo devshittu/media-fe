21e73e6eed21971198b1e7c1cc41f81e3e70a653
Muhammed Shittu <devshittu@gmail.com>
2023-08-20 23:50:38 +0100

    Added FieldError onChange value

--- src/components/theme-switch/theme-switch.tsx
+++ src/components/theme-switch/theme-switch.tsx
@@ -8,12 +8,22 @@ import {
   SunIcon,
 } from '@/components/illustrations';
 import { AnalogSwitch, AnalogSwitchItem } from '../blocks';
+import { FieldError } from 'react-hook-form';
 
 type ThemeSwitchProps = {
+  onChange?: (value: string) => void; // This is the type for onChange
+  value?: string;
+  error?: FieldError;
   showLabel?: boolean;
   className?: string;
 };
-const ThemeSwitch = ({ className, showLabel = false }: ThemeSwitchProps) => {
+const ThemeSwitch = ({
+  onChange, // Add this
+  value, // Add this
+  error,
+  className,
+  showLabel = false,
+}: ThemeSwitchProps) => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme, systemTheme, forcedTheme } = useTheme();
 
@@ -26,33 +36,45 @@ const ThemeSwitch = ({ className, showLabel = false }: ThemeSwitchProps) => {
   const currentTheme = theme === systemTheme ? systemTheme : theme;
   const disabled = !!forcedTheme;
 
+  const handleThemeChange = (newTheme: string) => {
+    setTheme(newTheme);
+    if (onChange) {
+      onChange(newTheme); // Notify react-hook-form of the change
+    }
+  };
   return (
-    <AnalogSwitch className={className}>
-      <AnalogSwitchItem
-        title="Light"
-        isSelected={currentTheme === THEME_LIGHT}
-        onClick={() => setTheme(THEME_LIGHT)}
-        icon={<SunIcon />}
-        disabled={disabled}
-        ariaLabel="Light Theme"
-      />
-      <AnalogSwitchItem
-        title="System"
-        isSelected={currentTheme === THEME_SYSTEM}
-        onClick={() => setTheme(THEME_SYSTEM)}
-        icon={<MonitorIcon />}
-        disabled={disabled}
-        ariaLabel="System Theme"
-      />
-      <AnalogSwitchItem
-        title="Dark"
-        isSelected={currentTheme === THEME_DARK}
-        onClick={() => setTheme(THEME_DARK)}
-        icon={<MoonIcon />}
-        disabled={disabled}
-        ariaLabel="Dark Theme"
-      />
-    </AnalogSwitch>
+    <>
+      <AnalogSwitch className={className}>
+        <AnalogSwitchItem
+          title="Light"
+          isSelected={currentTheme === THEME_LIGHT}
+          // onClick={() => setTheme(THEME_LIGHT)}
+          onClick={() => handleThemeChange(THEME_LIGHT)}
+          icon={<SunIcon />}
+          disabled={disabled}
+          ariaLabel="Light Theme"
+        />
+        <AnalogSwitchItem
+          title="System"
+          isSelected={currentTheme === THEME_SYSTEM}
+          // onClick={() => setTheme(THEME_SYSTEM)}
+          onClick={() => handleThemeChange(THEME_SYSTEM)}
+          icon={<MonitorIcon />}
+          disabled={disabled}
+          ariaLabel="System Theme"
+        />
+        <AnalogSwitchItem
+          title="Dark"
+          isSelected={currentTheme === THEME_DARK}
+          // onClick={() => setTheme(THEME_DARK)}
+          onClick={() => handleThemeChange(THEME_DARK)}
+          icon={<MoonIcon />}
+          disabled={disabled}
+          ariaLabel="Dark Theme"
+        />
+      </AnalogSwitch>
+      {error && <p className="mt-2 text-sm text-red-500">{error.message}</p>}
+    </>
   );
 };
 export default ThemeSwitch;
