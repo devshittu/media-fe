af1c68cd74c3aec2b4ac4c8731d2a447a9735077
Muhammed Shittu <devshittu@gmail.com>
2023-08-22 23:22:57 +0100

    Exported and refactored LoadingButtonTextList

--- src/components/loading/index.ts
+++ src/components/loading/index.ts
@@ -3,4 +3,5 @@ export * from './loading-avatar';
 export * from './loading-paragraph';
 export * from './loading-photo';
 export * from './loading-heading';
-export * from './loading-button';
+export * from './loading-button-text';
+export * from './loading-button-text-list';
