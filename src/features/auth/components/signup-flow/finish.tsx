db64e230c2eb97f5851b45173910dea2b35b9c8c
Muhammed Shittu <devshittu@gmail.com>
2023-08-22 22:43:37 +0100

    signup-flow finish

---
+++ src/features/auth/components/signup-flow/finish.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Finish = () => {
+  return (
+    <div>
+      Thanks and show loader and tell them you will be taking them to the
+      homepage. And and a quick tour will show them how to navigate the site.
+      Step 7: Load to the home pageShow them around using the tour feature in
+      the app
+    </div>
+  );
+};
