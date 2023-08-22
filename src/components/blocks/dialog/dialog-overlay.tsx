441e865ebfeefef6ffec0ce63aa4e643d60e0632
Muhammed Shittu <devshittu@gmail.com>
2023-08-22 21:07:05 +0100

    Added dialog overlay

---
+++ src/components/blocks/dialog/dialog-overlay.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type DialogOverlayProps = React.HTMLProps<HTMLDivElement> & {};
+
+export const DialogOverlay = React.forwardRef<
+  HTMLDivElement,
+  DialogOverlayProps
+>((props, ref) => (
+  <div
+    className="fixed top-0 right-0 bottom-0 left-0 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80  pointer-events-auto z-[-1]"
+    data-app-dialog-backdrop="true"
+    ref={ref}
+    {...props}
+  ></div>
+));
+DialogOverlay.displayName = 'Dialog Overlay';
