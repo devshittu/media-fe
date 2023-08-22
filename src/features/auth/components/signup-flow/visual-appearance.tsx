80adf36c33868e27f013b8f56524314901b9dafe
Muhammed Shittu <devshittu@gmail.com>
2023-08-22 22:43:22 +0100

    signup-flow personalise visual appearance

---
+++ src/features/auth/components/signup-flow/visual-appearance.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const VisualAppearance = () => {
+  return (
+    <div>
+      Step 6: Set favorite theme color (light, dark, or system preferences)
+    </div>
+  );
+};
