9b0286b989b3cc247a0db714c6151a66d74bc69d
Muhammed Shittu <devshittu@gmail.com>
2023-08-22 21:10:21 +0100

    Started the popup based on floating-ui

--- src/components/blocks/popup/index.ts
+++ src/components/blocks/popup/index.ts
@@ -1,1 +1,6 @@
 export * from './popup-components';
+export * from './popup-content';
+export * from './popup-trigger';
+export * from './popup';
+export * from './popper';
+export * from './hooks/';
