b81e7d887996342f1ed9fdc4d59294abfb9219bc
Muhammed Shittu <devshittu@gmail.com>
2023-08-22 21:07:31 +0100

    Added dialog wrapper

---
+++ src/components/blocks/dialog/dialog.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { usePopupContext } from '../popup';
+import { useId } from '@floating-ui/react';
+
+type DialogProps = {
+  title?: string;
+  subtitle?: string;
+  onClose?: () => void;
+  rounded?: boolean;
+  children?: React.ReactNode;
+};
+
+export const Dialog: React.FC<DialogProps> = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLProps<HTMLDivElement>
+>((props, ref) => {
+  const { setLabelId } = usePopupContext();
+  const id = useId();
+
+  React.useLayoutEffect(() => {
+    setLabelId(id);
+    return () => setLabelId(undefined);
+  }, [id, setLabelId]);
+
+  return (
+    <div
+      {...props}
+      className="fixed top-0 right-0 bottom-0 left-0 overflow-auto z-50 flex flex-col items-center content-center pt-[15px] px-[15px] h-[calc(100%-15px)] md:p-4 lg:pt-[10vh]"
+      data-app-dialog-overlay="true"
+      ref={ref}
+      id={id}
+    >
+      {props.children}
+    </div>
+  );
+});
+Dialog.displayName = 'Dialog';
