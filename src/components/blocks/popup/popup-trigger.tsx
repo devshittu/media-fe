047ecaf8797a9b7c04ba0a30018e96085eceabbc
Muhammed Shittu <devshittu@gmail.com>
2023-08-22 21:13:24 +0100

    Added popup trigger

---
+++ src/components/blocks/popup/popup-trigger.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { usePopupContext } from './hooks/usePopupContext';
+import { useMergeRefs } from '@floating-ui/react';
+interface PopupTriggerProps {
+  children: React.ReactNode;
+  asChild?: boolean;
+}
+
+export const PopupTrigger = React.forwardRef<
+  HTMLElement,
+  React.HTMLProps<HTMLElement> & PopupTriggerProps
+>((props, propRef) => {
+  const { children, asChild = false, ...rest } = props;
+  const context = usePopupContext();
+  const childrenRef = (children as any).ref;
+  const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
+
+  if (asChild && React.isValidElement(children)) {
+    return React.cloneElement(
+      children,
+      context.getReferenceProps({
+        ref,
+        ...rest,
+        ...children.props,
+        'data-state': context.open ? 'open' : 'closed',
+      }),
+    );
+  }
+
+  return (
+    <button
+      ref={ref}
+      data-state={context.open ? 'open' : 'closed'}
+      {...context.getReferenceProps(rest)}
+    >
+      {children}
+    </button>
+  );
+});
+PopupTrigger.displayName = 'PopupTrigger';
