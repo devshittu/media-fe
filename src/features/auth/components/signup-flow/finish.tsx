7d815f69ff78d7eaf70f025da95db2d2de0d8907
Muhammed Shittu <devshittu@gmail.com>
2023-08-24 00:29:44 +0100

    Added path and note

--- src/features/auth/components/signup-flow/finish.tsx
+++ src/features/auth/components/signup-flow/finish.tsx
@@ -9,10 +9,8 @@ export const Finish = ({ onValidationStatusChange }: StepProps) => {
 
   return (
     <div>
-      Thanks and show loader and tell them you will be taking them to the
-      homepage. And and a quick tour will show them how to navigate the site.
-      Step 7: Load to the home pageShow them around using the tour feature in
-      the app
+      {`Thanks for joining us! 🎉 You're headed to your homepage, but don't fret—a quick tour will pop up to guide your adventure through the news world. Happy exploring!`}
+      <h1 className='font-serif text-6xl'>Happy Exploring!</h1>
     </div>
   );
 };
