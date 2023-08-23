d1c77889c572087324e5a186399df48ed6a357df
Muhammed Shittu <devshittu@gmail.com>
2023-08-24 00:22:55 +0100

    Added path and retrieved the categories from category context.

--- src/features/auth/components/signup-flow/personalize-categories.tsx
+++ src/features/auth/components/signup-flow/personalize-categories.tsx
@@ -1,41 +1,57 @@
+import { useStepValidation } from '@/components/blocks/wizard/hooks';
+import { StepProps } from '@/components/blocks/wizard/types';
 import CustomCheckboxGroup, {
   Option,
 } from '@/components/form/custom-checkbox-group';
 import { Icon, XIcon } from '@/components/illustrations';
 import { Category, useCategories } from '@/features/categories';
 import { CategoryList } from '@/features/categories/components';
+import { useCategoryContext } from '@/features/categories/hooks';
 import Link from 'next/link';
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 
-export const PersonalizeCategories = () => {
+export const PersonalizeCategories = ({
+  onValidationStatusChange,
+}: StepProps) => {
+  // Define the validation function
+  // const validate = () => true;
+
+  // Call useStepValidation unconditionally
+  // const isValid = useStepValidation(validate, [], onValidationStatusChange);
+
+  // Call useCategories unconditionally
+  // const { data: responseData, isLoading } = useCategories({});
+
+  const { categories } = useCategoryContext();
+  // Check if the data is still loading
+  // if (isLoading || !responseData) {
+  //   return <div>Loading...</div>; // You can replace this with a proper loading component
+  // }
+
   const handleCustomCheckboxGroupChange = (
     selectedOptions: Option<Category>[],
   ) => {
     // Handle the change of selected options here
-    // e.g., update the state or perform other actions
     console.table(selectedOptions);
   };
 
-
-const categoriesData = useCategories({});
-  const allCategories = (categoriesData.data.categories as Category[]).map(
-    (category) => ({
-      ...category,
-      label: category.title, // Map the title to the label property
-    })
-  ) as Option<Category>[];
+  const allCategories = (categories as Category[]).map((category) => ({
+    ...category,
+    label: category.title, // Map the title to the label property
+  })) as Option<Category>[];
 
 
-console.table(allCategories);
   return (
     <div>
       <CustomCheckboxGroup<Category, Category>
         options={allCategories}
         initialSelectedOptions={[]} // Use the converted array here
         onChange={handleCustomCheckboxGroupChange}
-        renderAs='button'
-        className='flex gap-4 flex-wrap'
+        renderAs="button"
+        className="flex gap-4 flex-wrap"
       />
     </div>
   );
 };
+
+//Path src/features/auth/components/signup-flow/personalize-categories.tsx
