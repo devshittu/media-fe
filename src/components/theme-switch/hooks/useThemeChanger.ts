b2cba811f624172c550c07a0b2a6b3d1c9d1b5aa
Muhammed Shittu <devshittu@gmail.com>
2023-08-24 11:29:02 +0100

    Added useThemeChanger hook

---
+++ src/components/theme-switch/hooks/useThemeChanger.ts
@@ -0,0 +1,32 @@
+import { useTheme } from 'next-themes';
+import { useState, useEffect } from 'react';
+import { Theme } from '../types';
+
+type UseThemeChangerProps = {
+  onChange?: (value: Theme) => void;
+  onServerSync?: (value: Theme) => Promise<void>;
+};
+
+export const useThemeChanger = ({ onChange, onServerSync }: UseThemeChangerProps) => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, systemTheme, forcedTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const handleThemeChange = async (newTheme: Theme) => {
+    if (onServerSync) {
+      await onServerSync(newTheme); // Call the server sync function if provided
+    }
+    setTheme(newTheme);
+    if (onChange) {
+      onChange(newTheme);
+    }
+  };
+
+  const currentTheme = theme === systemTheme ? systemTheme : theme;
+  const disabled = !!forcedTheme;
+
+  return { currentTheme, handleThemeChange, mounted, disabled };
+};
