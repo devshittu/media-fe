ee582d7d2785abe762acd89ae48a0185d399480e
Muhammed Shittu <devshittu@gmail.com>
2023-08-25 20:47:59 +0100

    Added Tour, ConfettiEffect, and SplashLoader for global accessibility.

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { NavProvider, BodyStyleUpdater } from '@/components/blocks/nav/';
 import RootLayout from '@/layouts/root-layout';
@@ -9,6 +9,10 @@ import { IS_DEVELOPMENT } from '@/config/constants';
 import { CategoriesProvider } from '@/features/categories/hooks';
 import { Notifications } from '@/components/notifications';
 import { GlobalPopup } from '@/components/blocks/popup';
+import { Tour } from '@/components/blocks/tour';
+import { ConfettiEffect } from '@/components/confetti';
+import SplashLoader from '@/components/blocks/splash-loader/splash-loader';
+import { useInitialSplashLoader } from '@/components/blocks/splash-loader/hooks/useInitialSplashLoader';
 
 type AppProviderProps = {
   children: ReactNode;
@@ -16,6 +20,7 @@ type AppProviderProps = {
 };
 
 export const AppProvider = ({ children, theme }: AppProviderProps) => {
+  useInitialSplashLoader();
   return (
     <>
       <ThemeProvider attribute="class" forcedTheme={theme || undefined}>
@@ -29,11 +34,19 @@ export const AppProvider = ({ children, theme }: AppProviderProps) => {
             <CategoriesProvider>
               {children}
               <GlobalPopup />
+
+              <Tour />
             </CategoriesProvider>
           </QueryClientProvider>
+
+          {/* The confetti effect and Splash loader effect to make it available through out the app */}
+          <ConfettiEffect />
+          <SplashLoader />
           {/* </RootLayout> */}
         </NavProvider>
       </ThemeProvider>
     </>
   );
 };
+
+// Path: src/providers/app.tsx
