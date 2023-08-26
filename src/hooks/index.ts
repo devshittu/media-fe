76af89a1f3fff0c66a290926195de445d81c3864
Muhammed Shittu <devshittu@gmail.com>
2023-08-26 20:20:27 +0100

    Exported useInitializeStore

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -6,3 +6,4 @@ export { default as useScrollSpeed } from './useScrollSpeed';
 export * from './useScrollSync';
 export * from './useListGrouping';
 export * from './useListSorting';
+export * from './useInitializeStore';
