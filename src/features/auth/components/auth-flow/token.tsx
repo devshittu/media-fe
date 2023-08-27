04dc30165539ebe97fa40b16462996bc80ae8aaa
Muhammed Shittu <devshittu@gmail.com>
2023-08-27 19:22:59 +0100

    Refactored to use useWizardStepValidation and added info hint

--- src/features/auth/components/auth-flow/token.tsx
+++ src/features/auth/components/auth-flow/token.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useStepValidation } from '@/components/blocks/wizard/hooks';
+import { useWizardStepValidation } from '@/components/blocks/wizard/hooks';
 import { StepProps } from '@/components/blocks/wizard/types';
 // import PinInput, { UserAuthStatus } from '@/components/form/token-pin-digit';
 import PinInput from '@/components/form/pin-input/pin-input';
@@ -7,15 +7,17 @@ import { AuthStatus } from '@/components/form/pin-input/types';
 import { Tag } from '@/components/blocks/tag';
 import { Icon, InfoIcon } from '@/components/illustrations';
 import { pluralize } from '@/utils';
+import { Badge, BadgeSize, BadgeType } from '@/components/blocks/badge';
+import { useToggle } from '@/hooks';
+import { useTour } from '@/stores/tour';
+import { TourPopperType } from '@/components/blocks/tour/tour-popper';
+
+const trialCount = 3;
+const warningThreshold = 1;
 
 export const Token = ({ onValidationStatusChange }: StepProps) => {
-  const trialCount = 3;
   const [remainingAttempts, setRemainingAttempts] = useState(trialCount); // Initial attempts
-  const warningThreshold = 1;
-  // const validate = () => true;
-
-  // const isValid = useStepValidation(validate, [], onValidationStatusChange);
-
+  useWizardStepValidation();
   const verifyPin = async (pin: string): Promise<boolean> => {
     // Simulate a server call to verify the pin
     console.log('Verifying PIN...');
@@ -40,7 +42,8 @@ export const Token = ({ onValidationStatusChange }: StepProps) => {
   };
 
   return (
-    <div className="flex flex-col text-slate-800 dark:text-slate-400 space-y-4 md:space-y-8">
+    <>
+    <div className="flex flex-col text-slate-800 dark:text-slate-400 space-y-4 md:space-y-8 flex-grow">
       <div className=" justify-center">
         <PinInput
           id="auth-token"
@@ -66,25 +69,46 @@ export const Token = ({ onValidationStatusChange }: StepProps) => {
         </h1>
       </div>
       <div className="text-xs md:text-sm">
-        <h5
-          id="drawer-top-label"
-          className="inline-flex items-center mb-4 text-base font-bold "
-        >
-          <InfoIcon className="w-4 h-4 mr-2.5 stroke-[3]" />
-          Hint
-        </h5>
-        <p>
-          A PIN or token has been sent to your email address. Please check your
-          inbox or spam folder. It should arrive within the next 15 minutes.
-          Keep this page open until you receive it.
-        </p>
-        <p>
-          <strong>Note:</strong> After three unsuccessful attempts, you will be
-          prevented from requesting another token for 30 minutes.
-        </p>
+        <InfoWithHint/>
       </div>
     </div>
+    </>
   );
 };
+
+
+const InfoWithHint = () => {
+  const { isVisible, toggleVisibility } = useToggle();
+  const handleClick = () => {
+    toggleVisibility();
+  };
+
+  return (
+    <>
+      <Badge
+      id="show-hint-token"
+        type={BadgeType.INFO}
+        size={BadgeSize.LARGE}
+        onClick={handleClick}
+        rounded
+        icon={<InfoIcon className="w-4 h-4 stroke-[3]" />}
+      >
+        <h2 className="font-bold text-base">Hint</h2>
+      </Badge>
+      <div className={`hint ${isVisible ? 'block' : 'hidden'} bg-sky-200 dark:bg-sky-800 p-2 rounded-sm mt-3`}>
+        <p>
+          A PIN or token has been sent to your email address. Please check
+          your inbox or spam folder. It should arrive within the next 15
+          minutes. Keep this page open until you receive it.
+        </p>
+        <p>
+          <strong>Note:</strong> After three unsuccessful attempts, you will
+          be prevented from requesting another token for 30 minutes.
+        </p>
+      </div>
+    </>
+  );
+};
+
 
 //Path: src/features/auth/components/auth-flow/token.tsx
