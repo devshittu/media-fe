cea0c33e01de3ae07944e3dc19c0edfebdc5e0d7
Muhammed Shittu <devshittu@gmail.com>
2023-08-27 19:22:01 +0100

    Refactored to use useWizardStepValidation

--- src/features/auth/components/signup-flow/personalize-categories.tsx
+++ src/features/auth/components/signup-flow/personalize-categories.tsx
@@ -1,56 +1,98 @@
-import { useStepValidation } from '@/components/blocks/wizard/hooks';
-import { StepProps } from '@/components/blocks/wizard/types';
+import React, { useEffect } from 'react';
+import { useForm, Controller, FieldError } from 'react-hook-form';
 import CustomCheckboxGroup, {
   Option,
 } from '@/components/form/custom-checkbox-group';
-import { Icon, XIcon } from '@/components/illustrations';
-import { Category, useCategories } from '@/features/categories';
-import { CategoryList } from '@/features/categories/components';
+import { Category } from '@/features/categories';
 import { useCategoryContext } from '@/features/categories/hooks';
-import Link from 'next/link';
-import React, { useEffect, useMemo } from 'react';
+import { Button } from '@/components/button';
+import { useWizardStepValidation } from '@/components/blocks/wizard/hooks';
+import { useAccountSettingsStore } from '@/stores/app-settings';
+import { getObjectsByIds } from '@/utils';
 
-export const PersonalizeCategories = ({
-  onValidationStatusChange,
-}: StepProps) => {
-  // Define the validation function
-  // const validate = () => true;
-
-  // Call useStepValidation unconditionally
-  // const isValid = useStepValidation(validate, [], onValidationStatusChange);
-
-  // Call useCategories unconditionally
-  // const { data: responseData, isLoading } = useCategories({});
-
+export const PersonalizeCategories = () => {
+  const { modifiedSettings, updateModifiedSettings } =
+    useAccountSettingsStore();
+  const {
+    handleSubmit,
+    control,
+    watch,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      user_categories:
+        modifiedSettings.personal_settings.favorite_categories || [],
+    },
+    mode: 'all',
+  });
   const { categories } = useCategoryContext();
-  // Check if the data is still loading
-  // if (isLoading || !responseData) {
-  //   return <div>Loading...</div>; // You can replace this with a proper loading component
-  // }
+  const selectedOptions = watch('user_categories', []);
 
-  const handleCustomCheckboxGroupChange = (
-    selectedOptions: Option<Category>[],
-  ) => {
-    // Handle the change of selected options here
-    console.table(selectedOptions);
+  useWizardStepValidation(() => selectedOptions.length >= 3); // TODO: take out the magic number 3
+
+  const onSubmit = async (data: any) => {
+    const selectedCategoryIds = data.user_categories.map((item: any) =>
+      typeof item === 'string' ? item : item.id,
+    );
+
+    updateModifiedSettings(
+      ['personal_settings', 'favorite_categories'],
+      selectedCategoryIds,
+    );
+
+    console.log('Update modifiedSettings:');
+    console.log(modifiedSettings.personal_settings.favorite_categories);
   };
 
   const allCategories = (categories as Category[]).map((category) => ({
     ...category,
-    label: category.title, // Map the title to the label property
+    label: category.title,
   })) as Option<Category>[];
 
   return (
-    <div>
-      <CustomCheckboxGroup<Category, Category>
-        options={allCategories}
-        initialSelectedOptions={[]} // Use the converted array here
-        onChange={handleCustomCheckboxGroupChange}
-        renderAs="button"
-        className="flex gap-4 flex-wrap"
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="user_categories"
+        control={control}
+        rules={{
+          validate: (value: Option<any>[]) =>
+            (value?.length ?? 0) > 0 ||
+            'At least one category must be selected',
+        }}
+        render={({ field: { onChange, value } }) => {
+          const categoryIds = value.map((item) =>
+            typeof item === 'string' ? item : item.id,
+          );
+          const selectedCategories = getObjectsByIds(
+            allCategories,
+            categoryIds,
+          );
+          return (
+            <CustomCheckboxGroup<Category, Category>
+              options={allCategories}
+              initialSelectedOptions={selectedCategories}
+              onChange={onChange}
+              renderAs="button"
+              className="flex gap-4 flex-wrap"
+              error={
+                errors.user_categories?.message
+                  ? (errors.user_categories as FieldError)
+                  : null
+              }
+            />
+          );
+        }}
       />
-    </div>
+      <Button
+        // loading={!!updateSettings.isLoading}
+        // disabled={updateSettings.isLoading}
+        nativeType="submit"
+        size="large"
+        type="primary"
+      >
+        Update settings
+      </Button>
+    </form>
   );
 };
-
 //Path src/features/auth/components/signup-flow/personalize-categories.tsx
