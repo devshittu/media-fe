322ed42ef9d10440fccb5d7447e514ef10d4ce0c
Muhammed Shittu <devshittu@gmail.com>
2023-08-28 19:36:01 +0100

    Used getLayout prop

--- media-fe/src/pages/stories/[storyId].tsx
+++ media-fe/src/pages/stories/[storyId].tsx
@@ -1,80 +1,87 @@
-import { ReactElement, use, useEffect, useState } from 'react';
+import React, { ReactElement, useMemo } from 'react';
 import UserLayout from '@/layouts/user-layout';
 import { StoriesPageHeader } from '@/components/blocks/headers';
-import {
-  StoryList,
-  StoryListItem,
-  StoryListItemLoadingPlaceholder,
-} from '@/features/stories/components';
-import {
-  getChildrenStories,
-  getParentStories,
-  getRelatedStories,
-  getStory,
-  useChildrenStories,
-  useParentStories,
-  useRelatedStories,
-  useStory,
-} from '@/testing/test-data';
+import { StoryList, StoryListLoadingPlaceholder } from '@/features/stories/components';
+import { getStorylines, useStorylines } from '@/features/stories/api/get-storylines';
+import { PAGINATE_STORIES_LIMIT } from '@/config/constants';
+import { Story } from '@/features/stories';
+import { NotFound } from '@/components/not-found';
+import { StoriesPageFrame } from '@/components/frames';
+import { PaneConfig } from '@/components/blocks/side-panel/types';
+import { AccountList } from '@/features/users/components';
 import { TableOfContents } from '@/components/blocks/table-of-contents/';
 import { useRouter } from 'next/router';
-import { StoryItem } from '@/testing';
-import { Loading } from '@/components/loading';
-import { NotFound } from '@/components/not-found';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import { TimelineScrollbar } from '@/components/blocks/timeline-scroller';
-import { HeadingsLoadingPlaceholder } from '@/components/blocks/table-of-contents/';
-import { Story } from '@/features/stories';
 
-const Index = () => {
-  const [stories, setStories] = useState<Story[]>([]);
+type PublicStoryPageProps = InferGetServerSidePropsType<
+  typeof getServerSideProps
+>;
 
+const StorylinePage = ({stories}: PublicStoryPageProps) => {
   const router = useRouter();
   const storyId = router.query.storyId as string;
-  // const story = useStory(storyId);
-  const { data, isLoading } = useRelatedStories(storyId);
+  const { data: responseData, isLoading } = useStorylines({
+    storyId,
+    params: {
+      page: 1,
+      per_page: PAGINATE_STORIES_LIMIT,
+    },
+  });
 
-  // console.log('Stories', data, 'isLoading', isLoading);
+  const stableStories = useMemo(() => responseData?.stories, [responseData?.stories]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const relatedStories = (await getRelatedStories(storyId)) as Story[];
-      if (relatedStories) {
-        setStories(relatedStories);
-      }
-    };
-    fetchData();
-  }, [storyId]);
   return (
-    <div
-      className={`flex relative min-h-full w-full min-w-0 m-0 items-stretch grow flex-row p-0 justify-between shrink-0 basis-auto `}
-    >
-      <div
-        className={`flex flex-col flex-shrink-0 basis-auto flex-grow relative p-0 min-w-0 min-h-0 m-0 border-x max-w-full lg:max-w-[640px] box-border border-slate-100 dark:border-slate-800`}
-      >
-        <StoriesPageHeader pageTitle="Timeline" />
-        <section>
-          {isLoading ? (
-            <StoryListItemLoadingPlaceholder />
-          ) : stories.length > 0 && !isLoading ? (
-            <StoryList data={stories} />
-          ) : (
-            <NotFound />
-          )}
-        </section>
-      </div>
-      <div
-        className={`relative hidden lg:flex p-0 z-0 min-w-0 min-h-0 box-border my-0 ml-0 flex-shrink-0 basis-auto flex-col border-0 w-[350px] items-stretch`}
-      >
-        <TableOfContents data={stories} />
-      </div>
-    </div>
+    <>
+      <StoriesPageHeader pageTitle="Timeline" />
+      {isLoading && <StoryListLoadingPlaceholder />}
+      {stableStories?.length === 0 && <NotFound />}
+      {stableStories?.length > 0 && (
+        <StoryList
+          data={stableStories}
+          totalPages={responseData?.total_pages}
+          scrollInfinite
+        />
+      )}
+    </>
+    // ,{ stableStories }
+  );
+};
+StorylinePage.getLayout = function getLayout(page: ReactElement, layoutProps?: Record<string, any>) {
+  const sidePanelSections: PaneConfig[] = [
+    {
+      id: 'relatedStories',
+      title: 'Related Stories',
+      // component: <TableOfContents data={layoutProps?.stories || []} />,
+      component: <TimelineScrollbar data={layoutProps?.stories || []} />,
+      showLink: false,
+    },
+    {
+      id: 'channelSubscriptions',
+      title: 'Suggestions',
+      component: <AccountList />,
+    },
+  ];
+
+  return (
+    <UserLayout>
+      <StoriesPageFrame sidePanelSections={sidePanelSections}>
+        {page}
+      </StoriesPageFrame>
+    </UserLayout>
   );
 };
 
-Index.getLayout = function getLayout(page: ReactElement) {
-  return <UserLayout>{page}</UserLayout>;
-};
+export  const getServerSideProps = async(context: GetServerSidePropsContext) => {
+  const storyId = context.params?.storyId as string;
+  const stories = await getStorylines({storyId});  
+  return {
+    props: { stories },
+  };
+}
+export default StorylinePage;
 
-export default Index;
+
+
 
 // Path: pages/stories/[storyId]
