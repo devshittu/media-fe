93c4aa614d43effa5e9bf3b93ac6b964248af268
Muhammed Shittu <devshittu@gmail.com>
2023-08-28 19:30:50 +0100

    Formatted export type

--- media-fe/src/components/blocks/table-of-contents/index.ts
+++ media-fe/src/components/blocks/table-of-contents/index.ts
@@ -1,5 +1,8 @@
-export { default as TableOfContents } from './table-of-contents';
+export * from './table-of-contents';
 export * from './headings-loading-placeholder';
-export { default as Heading } from './heading';
-export { default as Headings } from './headings';
+export * from './heading';
+export * from './headings';
 export * from './hooks';
+
+
+// Path: media-fe/src/components/blocks/table-of-contents/index.ts
