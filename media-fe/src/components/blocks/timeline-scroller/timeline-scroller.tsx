1743a1583b01a9a6419bf7206767980c7936b6ff
Muhammed Shittu <devshittu@gmail.com>
2023-08-28 19:31:54 +0100

    Started with the timeline scroller

--- media-fe/src/components/blocks/timeline-scroller/timeline-scroller.tsx
+++ media-fe/src/components/blocks/timeline-scroller/timeline-scroller.tsx
@@ -1,10 +1,38 @@
-import { useHeadingsData } from '../table-of-contents/hooks/';
-import React, { useState } from 'react';
-import Headings from '../table-of-contents/headings';
+import { useIntersectionObserver } from '../table-of-contents/hooks/';
+import React, { useEffect, useState } from 'react';
+import {Headings} from '../table-of-contents/headings';
+import { Heading, HeadingsLoadingPlaceholder, TableOfContentsProps } from '../table-of-contents';
+import { StoryResponse } from '@/features/stories';
 
-const TimelineScrollbar = () => {
+export type TimelineScrollbarProps = {
+  data?: StoryResponse;
+};
+
+const TimelineScrollbar = ({data}: TimelineScrollbarProps) => {
   const [activeId, setActiveId] = useState<string>();
-  const { nestedHeadings } = useHeadingsData();
+  // const { nestedHeadings } = useHeadingsData();
+  const [nestedHeadings, setNestedHeadings] = useState<Heading[]>([]);
+
+
+  useEffect(() => {
+    if (data && data.stories && data.stories.length > 0) {
+      
+      // Transform your data into nestedHeadings here
+      const newNestedHeadings = data?.stories.map((story) => {
+        return {
+          id: story.slug,  // Replace with actual id field
+          title: `${story.id} ${story.title}`,  // Replace with actual title field
+          items: []  // Populate this array based on your data structure
+        };
+      });
+      setNestedHeadings(newNestedHeadings);
+    }
+  }, [data]);
+  useIntersectionObserver(setActiveId);
+  if (!data) {
+    return <HeadingsLoadingPlaceholder />; // or show a loading indicator
+  }
+
   return (
     <nav
       aria-label="Timeline Scrollbar"
