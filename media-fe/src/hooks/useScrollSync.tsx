847237dddadd938b507bcced7eda9163bdad73cf
Muhammed Shittu <devshittu@gmail.com>
2023-08-29 21:58:34 +0100

    Formatted

--- media-fe/src/hooks/useScrollSync.tsx
+++ media-fe/src/hooks/useScrollSync.tsx
@@ -3,30 +3,33 @@ import { useScrollSpeed } from '@/hooks';
 import { rangeLimit } from '@/utils/helpers';
 import throttle from 'lodash.throttle';
 
-
 type useScrollSyncType = {
-  contentHeight?: number,
-  minTop?: number,
-}
+  contentHeight?: number;
+  minTop?: number;
+};
 
-export const useScrollSync = ({contentHeight, minTop = 53, }: useScrollSyncType) => {
+export const useScrollSync = ({
+  contentHeight,
+  minTop = 53,
+}: useScrollSyncType) => {
   const [topPosition, setTopPosition] = useState(0);
   const scrollSpeed = useScrollSpeed({ delay: 40 }) || 0;
   const animationFrame = useRef<number | null>(null);
 
   const handleScroll = throttle(() => {
-      // Cancel the previous frame
-      if (animationFrame.current) {
-        cancelAnimationFrame(animationFrame.current);
-      }
+    // Cancel the previous frame
+    if (animationFrame.current) {
+      cancelAnimationFrame(animationFrame.current);
+    }
 
-      // Schedule the next frame
-      animationFrame.current = requestAnimationFrame(() => {
-        setTopPosition((prevTopPosition) => {
-          const newTop = rangeLimit(prevTopPosition - scrollSpeed, -minTop, 0);
-          return newTop;
-        });
-      });})
+    // Schedule the next frame
+    animationFrame.current = requestAnimationFrame(() => {
+      setTopPosition((prevTopPosition) => {
+        const newTop = rangeLimit(prevTopPosition - scrollSpeed, -minTop, 0);
+        return newTop;
+      });
+    });
+  });
   useEffect(() => {
     window.addEventListener('scroll', handleScroll, { passive: true });
 
