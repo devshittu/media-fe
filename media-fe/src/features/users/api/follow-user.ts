e3cc6c324c28317f9a92f70809d9bb26d6b75b7c
Muhammed Shittu <devshittu@gmail.com>
2023-08-31 23:37:17 +0100

    Added a follow user functionality (refactored)

--- media-fe/src/features/users/api/follow-user.ts
+++ media-fe/src/features/users/api/follow-user.ts
@@ -1,50 +1,25 @@
 import { useMutation } from '@tanstack/react-query';
 import { apiClient } from '@/lib/api-client';
 import { QUERY_KEYS } from '@/config/query';
-import { ApiResponse } from '@/types';
+
 const { FOLLOW_USER } = QUERY_KEYS;
 
-type FollowUserData = {
-  data: {
-    user_id: string | undefined;
-  };
-};
-
-export const followUser = ({ data }: FollowUserData): Promise<ApiResponse> => {
+export const followUser = (data: {
+  user_id: string;
+}): Promise<{ status: boolean }> => {
   return apiClient.post('/users/follow', { data });
 };
 
-type UseFollowUserOptions = {
-  data: {
-    user_id: string | undefined;
-  };
-  onSuccess?: ((data: any) => boolean) | ((data: any) => void);
-
-  onError?: (message: any) => void;
-};
-
-export const useFollowUser = ({
-  data,
-  onSuccess,
-  onError,
-}: UseFollowUserOptions) => {
-  const { mutate: submit, isLoading } = useMutation({
-    mutationKey: [FOLLOW_USER, data],
+export const useFollowUser = () => {
+  const { mutate: submit, isLoading: isFollowLoading } = useMutation({
+    mutationKey: [FOLLOW_USER],
     mutationFn: followUser,
-    onSuccess: (data) => {
-      // Invalidate and refetch something when a post is unbookmarked
-      //   queryClient.invalidateQueries('someQueryKey');
-      console.log('useFollowUser:// ',data);
-      onSuccess?.(data);
-      return data.status;
-    },
-    onError: (data) => {
-      onError?.(data);
-    },
   });
 
   return {
     submit,
-    isLoading,
+    isFollowLoading,
   };
 };
+
+// Path: media-fe/src/features/users/api/follow-user.ts
