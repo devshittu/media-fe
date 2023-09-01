abe06613563ca6de70ab05acbd2aaf6db9f9de50
Muhammed Shittu <devshittu@gmail.com>
2023-09-01 23:32:03 +0100

    Improving the storylist ux

--- media-fe/src/stores/feeds/feeds.ts
+++ media-fe/src/stores/feeds/feeds.ts
@@ -1,10 +1,11 @@
-// src/stores/feeds/feeds.ts
 import { createStore } from 'zustand';
 
 type FeedsState<T> = {
   allItems: T[];
   scrollPosition: number;
-  setAllItems: (stories: T[]) => void;
+  currentPage: number;
+  setAllItems: (items: T[] | ((prevStories: T[]) => T[])) => void;
+  setCurrentPage: (page: number) => void;
   setScrollPosition: (position: number) => void;
 };
 
@@ -12,7 +13,17 @@ export const createFeedsStore = <T extends Record<string, any>>() => {
   return createStore<FeedsState<T>>((set) => ({
     allItems: [],
     scrollPosition: 0,
-    setAllItems: (stories) => set({ allItems: stories }),
+    currentPage: 1,
+    setAllItems: (items) =>
+      set((state) => {
+        if (typeof items === 'function') {
+          return { allItems: items(state.allItems) };
+        }
+        return { allItems: items };
+      }),
+    setCurrentPage: (page) => set({ currentPage: page }),
     setScrollPosition: (position) => set({ scrollPosition: position }),
   }));
 };
+
+// Path: media-fe/src/stores/feeds/feeds.ts
