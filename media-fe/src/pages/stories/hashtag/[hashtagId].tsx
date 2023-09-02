8f8a3e2b592ea0ab6f8dc2fcedb250b17ba16ed7
Muhammed Shittu <devshittu@gmail.com>
2023-09-03 00:24:37 +0100

    Added hashtag

--- media-fe/src/pages/stories/hashtag/[hashtagId].tsx
+++ media-fe/src/pages/stories/hashtag/[hashtagId].tsx
@@ -1,53 +1,33 @@
 import { ReactElement, useMemo } from 'react';
 import UserLayout from '@/layouts/user-layout';
 import { StoriesPageHeader } from '@/components/blocks/headers';
-import {
-  StoryList,
-  StoryListLoadingPlaceholder,
-} from '@/features/stories/components';
-
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
-import { Story, getStories, useStories } from '@/features/stories';
+import {
+  StoriesQueryParams,
+  Story,
+} from '@/features/stories';
 import { PAGINATE_STORIES_LIMIT } from '@/config/constants';
 import {
   getStoriesByHashtag,
-  useStoriesByHashtag,
 } from '@/features/stories/api/get-stories-by-hashtag';
-import { useRouter } from 'next/router';
 import { StoriesPageFrame } from '@/components/frames';
+import { cleanObject } from '@/utils';
+import { NotFound } from '@/components/not-found';
+import { HashtaggedStoryList } from '@/features/stories/components/blocks/hashtagged-story-list';
 type PublicStoriesPageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
 >;
-const StoriesByHashtagPage = ({ stories }: PublicStoriesPageProps) => {
-  const router = useRouter();
-  const hashtagId = router.query.hashtagId as string;
-  const { data: responseData, isLoading } = useStoriesByHashtag({
-    params: {
-      page: 1,
-      per_page: PAGINATE_STORIES_LIMIT,
-      hashtag: hashtagId,
-    },
-  }); // Use data and isLoading directly from the hook
-
-  const stableHashtags = useMemo(
-    () => responseData?.stories,
-    [responseData?.stories],
-  );
-
+const StoriesByHashtagPage = ({
+  stories,
+  queryParams,
+}: PublicStoriesPageProps) => {
   return (
     <>
-      <StoriesPageHeader pageTitle="Home" />
-      {isLoading && (
-        <>
-          <StoryListLoadingPlaceholder />
-        </>
-      )}
-      {stableHashtags?.length > 0 && (
-        <StoryList
-          data={stableHashtags}
-          totalPages={responseData?.total_pages}
-          // scrollInfinite
-        />
+      <StoriesPageHeader pageTitle={`#${queryParams.hashtag}`} />
+      {/* TODO: no stories to display */}
+      {!stories.stories && <NotFound />}
+      {stories.stories?.length > 0 && (
+        <HashtaggedStoryList data={stories} queryParams={queryParams} />
       )}{' '}
     </>
   );
@@ -65,20 +45,33 @@ export const getServerSideProps = async ({
   params,
 }: GetServerSidePropsContext) => {
   const hashtagId = params?.hashtagId as string;
-  // const stories = await getAllStories().catch(() => [] as StoryItem[]);
-  const stories = await getStoriesByHashtag({
-    params: {
-      hashtag: hashtagId,
-      page: 1,
-      per_page: PAGINATE_STORIES_LIMIT,
-    },
-  }).catch(() => [] as Story[]);
 
-  return {
-    props: {
-      stories,
-    },
-  };
+  const queryParams: StoriesQueryParams = cleanObject({
+    hashtag: hashtagId,
+    page: 1,
+    per_page: PAGINATE_STORIES_LIMIT,
+  });
+  try {
+    const stories = await getStoriesByHashtag({ params: queryParams });
+    return {
+      props: {
+        stories,
+        queryParams, // Pass queryParams as a prop
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        stories: {
+          stories: [] as Story[],
+          page: 1,
+          total_pages: 0,
+          total: 0,
+        },
+        queryParams, // Pass queryParams as a prop
+      },
+    };
+  }
 };
 
 export default StoriesByHashtagPage;
