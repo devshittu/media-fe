6ce2b53992d3724d4d8465c333875a934a850738
Muhammed Shittu <devshittu@gmail.com>
2023-09-02 23:50:23 +0100

    Formatted

--- media-fe/src/components/blocks/nav/body-el-updater.tsx
+++ media-fe/src/components/blocks/nav/body-el-updater.tsx
@@ -2,7 +2,6 @@ import { useContext } from 'react';
 import { NavContext } from './providers/nav-provider';
 import { useBodyStyle } from './hooks/useBodyEl';
 
-
 export const BodyStyleUpdater: React.FC = () => {
   const { isNavOpen, lockScroll } = useContext(NavContext);
   useBodyStyle(isNavOpen, lockScroll);
