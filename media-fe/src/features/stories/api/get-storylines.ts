870c2f0ebc848df8820565720f885e649aea4e0e
Muhammed Shittu <devshittu@gmail.com>
2023-09-03 00:16:36 +0100

    Added useInfiniteStorylines

--- media-fe/src/features/stories/api/get-storylines.ts
+++ media-fe/src/features/stories/api/get-storylines.ts
@@ -1,34 +1,30 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
 import { apiClient } from '@/lib/api-client';
 
-import { StoryResponse } from '../types';
+import { StoriesQueryParams, StoryResponse } from '../types';
 import { QUERY_KEYS } from '@/config/query';
 const { GET_STORYLINES } = QUERY_KEYS;
 
 type GetStorylineOptions = {
-  storyId: string;
-  params?: {
-    // category_id?: string | undefined;
-    page?: number | undefined;
-    per_page?: number | undefined;
-    // hashtag?: string | undefined;
-  };
+  storyFor: string;
+  params?: StoriesQueryParams;
+  initialData?: any;
 };
 
 export const getStorylines = ({
-  storyId,
+  storyFor,
   params,
 }: GetStorylineOptions): Promise<StoryResponse> => {
-  return apiClient.get(`/stories/${storyId}`, {
+  return apiClient.get(`/stories/${storyFor}`, {
     params,
   });
 };
 
-export const useStorylines = ({ storyId, params }: GetStorylineOptions) => {
+export const useStorylines = ({ storyFor, params }: GetStorylineOptions) => {
   const { data, isFetching, isFetched } = useQuery({
-    queryKey: [GET_STORYLINES, storyId],
-    queryFn: () => getStorylines({ storyId, params }),
+    queryKey: [GET_STORYLINES, storyFor],
+    queryFn: () => getStorylines({ storyFor, params }),
     // enabled: !!params?.category_id,
     initialData: {} as StoryResponse,
   });
@@ -39,4 +35,41 @@ export const useStorylines = ({ storyId, params }: GetStorylineOptions) => {
   };
 };
 
+export const useInfiniteStorylines = ({
+  storyFor,
+  params,
+  initialData,
+}: GetStorylineOptions) => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery(
+      ['stories', storyFor],
+      async ({ pageParam = 2 }) => {
+        const response = await getStorylines({
+          storyFor,
+          params: { ...params, page: pageParam },
+        });
+        return response;
+      },
+      {
+        getNextPageParam: (lastPage) => {
+          return lastPage.page < lastPage.total_pages
+            ? lastPage.page + 1
+            : undefined;
+        },
+
+        initialData: { pages: [initialData], pageParams: [1] },
+        //TODO: Keep data fresh for 5 minutes
+        staleTime: 1000 * 60 * 5,
+        // Keep data in cache for 10 minutes
+        cacheTime: 1000 * 60 * 10,
+      },
+    );
+
+  return {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  };
+};
 //Path: src/features/stories/api/get-stories.ts
