ef8ba5ffc3f2b758843a0dab829a113bc80a946b
Muhammed Shittu <devshittu@gmail.com>
2023-09-03 00:32:39 +0100

    Updated StorylinePage

--- media-fe/src/pages/stories/[storyId].tsx
+++ media-fe/src/pages/stories/[storyId].tsx
@@ -1,59 +1,66 @@
-import React, { ReactElement, useMemo } from 'react';
+import React, {
+  ReactElement,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import UserLayout from '@/layouts/user-layout';
 import { StoriesPageHeader } from '@/components/blocks/headers';
-import {
-  StoryList,
-  StoryListLoadingPlaceholder,
-} from '@/features/stories/components';
-import {
-  getStorylines,
-  useStorylines,
-} from '@/features/stories/api/get-storylines';
+
+import { getStorylines } from '@/features/stories/api/get-storylines';
 import { PAGINATE_STORIES_LIMIT } from '@/config/constants';
-import { Story } from '@/features/stories';
 import { NotFound } from '@/components/not-found';
 import { StoriesPageFrame } from '@/components/frames';
 import { PaneConfig } from '@/components/blocks/side-panel/types';
 import { UserSuggestionList } from '@/features/users/components';
-import { TableOfContents } from '@/components/blocks/table-of-contents/';
-import { useRouter } from 'next/router';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import { TimelineScrollbar } from '@/components/blocks/timeline-scroller';
+import { StoriesQueryParams, Story } from '@/features/stories';
+import { cleanObject } from '@/utils';
+import { StorylineStoryList } from '@/features/stories/components/blocks/storyline-story-list';
 
 type PublicStoryPageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
 >;
 
-const StorylinePage = ({ stories }: PublicStoryPageProps) => {
-  const router = useRouter();
-  const storyId = router.query.storyId as string;
-  const { data: responseData, isLoading } = useStorylines({
-    storyId,
-    params: {
-      page: 1,
-      per_page: PAGINATE_STORIES_LIMIT,
-    },
-  });
+const StorylinePage = ({
+  stories,
+  storyFor,
+  queryParams,
+}: PublicStoryPageProps) => {
+  const [currentStoryId, setCurrentStoryId] = useState<string | null>(null);
+  useEffect(() => {
+    console.log('about setting setCurrentStoryId:// ', storyFor);
+    setCurrentStoryId(storyFor);
+  }, [storyFor]);
 
-  const stableStories = useMemo(
-    () => responseData?.stories,
-    [responseData?.stories],
-  );
+  useEffect(() => {
+    if (currentStoryId && stories?.stories?.length > 0) {
+      const element = document.getElementById(`scroll-to-${currentStoryId}`);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        window.scrollTo({
+          top: rect.top + window.scrollY,
+          behavior: 'auto',
+        });
+      }
+    }
+  }, [currentStoryId, stories.stories]);
 
   return (
     <>
       <StoriesPageHeader pageTitle="Timeline" />
-      {isLoading && <StoryListLoadingPlaceholder />}
-      {stableStories?.length === 0 && <NotFound />}
-      {stableStories?.length > 0 && (
-        <StoryList
-          data={stableStories}
-          totalPages={responseData?.total_pages}
-          scrollInfinite
+      {!stories.stories && <NotFound />}
+      {stories?.stories?.length > 0 && (
+        <StorylineStoryList
+          storyFor={storyFor}
+          data={stories}
+          queryParams={queryParams}
+         
         />
       )}
     </>
-    // ,{ stableStories }
   );
 };
 StorylinePage.getLayout = function getLayout(
@@ -84,14 +91,45 @@ StorylinePage.getLayout = function getLayout(
   );
 };
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext,
-) => {
-  const storyId = context.params?.storyId as string;
-  const stories = await getStorylines({ storyId });
-  return {
-    props: { stories },
-  };
+export const getServerSideProps = async ({
+  params,
+}: GetServerSidePropsContext) => {
+  const queryParams: StoriesQueryParams = cleanObject({
+    // category_id: params?.category_id as string,
+    page: 1,
+    per_page: PAGINATE_STORIES_LIMIT,
+    // story_id: params?.storyId as string
+  });
+  const storyFor = params?.storyId as string;
+  console.log('StoryFor://', storyFor);
+  try {
+    const stories = await getStorylines({
+      storyFor,
+      params: queryParams,
+    });
+
+    // console.log('StoryID://', storyId,'storyline://',stories);
+    return {
+      props: {
+        stories,
+        storyFor,
+        queryParams,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        stories: {
+          stories: [] as Story[],
+          page: 1,
+          total_pages: 0,
+          total: 0,
+        },
+        storyFor,
+        queryParams,
+      },
+    };
+  }
 };
 export default StorylinePage;
 
