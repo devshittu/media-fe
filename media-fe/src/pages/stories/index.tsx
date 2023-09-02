82a1e3932c6d6ebaa253ffb3396932dee900c449
Muhammed Shittu <devshittu@gmail.com>
2023-09-03 00:27:08 +0100

    Updated StoriesPage

--- media-fe/src/pages/stories/index.tsx
+++ media-fe/src/pages/stories/index.tsx
@@ -1,43 +1,25 @@
 import { ReactElement, useMemo, useRef } from 'react';
 import UserLayout from '@/layouts/user-layout';
 import { StoriesPageHeader } from '@/components/blocks/headers';
-import {
-  StoryList,
-  StoryListLoadingPlaceholder,
-} from '@/features/stories/components';
+import { StoryList } from '@/features/stories/components';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
-import { Story, getStories, useStories } from '@/features/stories';
+import { StoriesQueryParams, Story, getStories } from '@/features/stories';
 import { PAGINATE_STORIES_LIMIT } from '@/config/constants';
 import { StoriesPageFrame } from '@/components/frames';
+import { NotFound } from '@/components/not-found';
+import { cleanObject } from '@/utils';
 type PublicStoriesPageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
 >;
-const StoriesPage = ({ stories }: PublicStoriesPageProps) => {
-  const { data: responseData, isLoading } = useStories({
-    params: {
-      page: 1,
-      per_page: PAGINATE_STORIES_LIMIT,
-    },
-  });
-  const stableStories = useMemo(
-    () => responseData?.stories,
-    [responseData?.stories],
-  );
-
+const StoriesPage = ({ stories, queryParams }: PublicStoriesPageProps) => {
   return (
     <>
       <StoriesPageHeader pageTitle="Home" showTab parallax />
-      {isLoading && (
-        <>
-          <StoryListLoadingPlaceholder />
-        </>
-      )}
-      {stableStories?.length > 0 && (
-        <StoryList
-          data={stableStories}
-          totalPages={responseData?.total_pages}
-          scrollInfinite
-        />
+
+      {/* TODO: no stories to display */}
+      {!stories.stories && <NotFound />}
+      {stories.stories?.length > 0 && (
+        <StoryList data={stories} queryParams={queryParams} />
       )}
     </>
   );
@@ -54,20 +36,31 @@ StoriesPage.getLayout = function getLayout(page: ReactElement) {
 export const getServerSideProps = async ({
   params,
 }: GetServerSidePropsContext) => {
-  const category_id = params?.category_id as string;
-  const stories = await getStories({
-    params: {
-      category_id: category_id,
-      page: 1,
-      per_page: PAGINATE_STORIES_LIMIT,
-    },
-  }).catch(() => [] as Story[]);
-
-  return {
-    props: {
-      stories,
-    },
-  };
+  const queryParams: StoriesQueryParams = cleanObject({
+    page: 1,
+    per_page: PAGINATE_STORIES_LIMIT,
+  });
+  try {
+    const stories = await getStories({ params: queryParams });
+    return {
+      props: {
+        stories,
+        queryParams, // Pass queryParams as a prop
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        stories: {
+          stories: [] as Story[],
+          page: 1,
+          total_pages: 0,
+          total: 0,
+        },
+        queryParams, // Pass queryParams as a prop
+      },
+    };
+  }
 };
 
 export default StoriesPage;
