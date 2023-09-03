3304dcad680bfa92a9aace3050936d90c091f871
Muhammed Shittu <devshittu@gmail.com>
2023-09-03 23:26:21 +0100

    experimenting the cachedData

--- src/features/categories/api/get-categories.ts
+++ src/features/categories/api/get-categories.ts
@@ -3,6 +3,7 @@ import { apiClient } from '@/lib/api-client';
 
 import { Category, CategoryResponse } from '../types';
 import { DEFAULT_STALE_TIME, QUERY_KEYS } from '@/config/query';
+import { useEffect } from 'react';
 const { GET_CATEGORIES } = QUERY_KEYS;
 type GetCategoriesOptions = {
   params?: {
@@ -20,17 +21,19 @@ export const useCategories = ({ params }: GetCategoriesOptions) => {
   const queryClient = useQueryClient();
 
   const { data, isFetching, isFetched } = useQuery({
-    queryKey: [GET_CATEGORIES, params],
+    queryKey: [GET_CATEGORIES, params?.category_id],
     queryFn: () => getCategories({ params }),
     initialData: () => {
       // Check if we have anything in cache and return that, otherwise get initial data
       const cachedData = queryClient.getQueryData<CategoryResponse | undefined>(
-        [GET_CATEGORIES],
+        [GET_CATEGORIES, params?.category_id],
       );
 
       if (cachedData) {
         return cachedData as CategoryResponse;
       }
+
+      console.log('cachedData for categories://', cachedData);
 
       return {
         categories: [] as Category[],
@@ -39,10 +42,17 @@ export const useCategories = ({ params }: GetCategoriesOptions) => {
         total: 0,
       } as CategoryResponse;
     },
+    //TODO: Keep data fresh for 5 minutes
+    staleTime: 1000 * 60 * 5,
+    // Keep data in cache for 10 minutes
     cacheTime: 1000 * 10,
     // staleTime: DEFAULT_STALE_TIME,
   });
-
+  // useEffect(() => {
+  //   if (!isFetching && !isFetched) {
+  //     queryClient.invalidateQueries([GET_CATEGORIES, params?.category_id]);
+  //   }
+  // }, [queryClient, params, isFetching, isFetched]);
   return { data, isLoading: isFetching && !isFetched };
 };
 
