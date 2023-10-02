e22c3edf7a4432a7a1b13a4ea01e3fc143639be8
Muhammed Shittu <devshittu@gmail.com>
2023-10-02 14:04:47 +0100

    Added api constants

---
+++ src/config/api-constants.ts
@@ -0,0 +1,65 @@
+export const URI_AUTH_REGISTER = '/auth/register/';
+export const URI_AUTH_COMPLETE_SETUP = '/auth/complete_setup/';
+export const URI_AUTH_ME = '/auth/me/';
+export const URI_AUTH_ME_SETTINGS = '/auth/me/settings/';
+export const URI_AUTH_LOGOUT = '/auth/logout/';
+export const URI_AUTH_TOKEN = '/auth/token/';
+export const URI_AUTH_TOKEN_REFRESH = '/auth/token/refresh/';
+export const URI_AUTH_TOKEN_VERIFY = '/auth/token/verify/';
+export const URI_AUTH_PASSWORD_RESET = '/auth/password-reset/';
+export const URI_AUTH_PASSWORD_RESET_CONFIRM = '/auth/password-reset-confirm/';
+export const URI_AUTH_VERIFY_OTP = '/auth/verify-otp/';
+export const URI_AUTH_VERIFY_OTP_AUTH = '/auth/verify-otp-auth/';
+export const URI_AUTH_RESEND_OTP = '/auth/resend-otp/';
+export const URI_AUTH_VERIFY_ACCOUNT_BY_TOKEN =
+  '/auth/verify-account/{{token}}/';
+export const URI_AUTH_RESEND_VERIFICATION_LINK =
+  '/auth/resend-verification-link/';
+export const URI_CATEGORIES = '/categories/';
+export const URI_CATEGORIES_BY_SLUG = '/categories/{{slug}}/';
+export const URI_STORIES = '/stories/';
+export const URI_STORIES_CATEGORY_BY_CATEGORY_SLUG =
+  '/stories/category/{{category_slug}}/';
+export const URI_USER_FEED = '/user-feed/';
+export const URI_STORIES_HASHTAG_BY_HASHTAG_NAME =
+  '/stories/hashtag/{{hashtag_name}}/';
+export const URI_STORIES_TRENDING = '/stories/trending/';
+export const URI_STORYLINES_BY_STORYLINE_ID_HASHTAGS =
+  '/storylines/{{storyline_id}}/hashtags/';
+export const URI_STORIES_BY_STORY_SLUG = '/stories/{{story_slug}}/';
+export const URI_STORIES_BY_STORY_ID_LIKE = '/stories/{{story_id}}/like/';
+export const URI_STORIES_BY_STORY_ID_DISLIKE = '/stories/{{story_id}}/dislike/';
+export const URI_STORIES_BY_STORY_ID_UNLIKE = '/stories/{{story_id}}/unlike/';
+export const URI_STORIES_BY_STORY_ID_UNDISLIKE =
+  '/stories/{{story_id}}/undislike/';
+export const URI_STORIES_BY_SLUG_STORYLINES = '/stories/{{slug}}/storylines/';
+export const URI_STORYLINES = '/storylines/';
+export const URI_STORYLINES_BY_STORYLINE_ID = '/storylines/{{storyline_id}}/';
+export const URI_STORYLINES_BY_STORYLINE_ID_STORIES =
+  '/storylines/{{storyline_id}}/stories/';
+export const URI_BOOKMARKS = '/bookmarks/';
+export const URI_BOOKMARKS_BY_PK = '/bookmarks/{{pk}}/';
+export const URI_USERS = '/users/';
+export const URI_USERS_BY_PK = '/users/{{pk}}/';
+export const URI_USERS_UNFOLLOWED = '/users/unfollowed/';
+export const URI_SETTINGS = '/settings/';
+export const URI_UPDATE_FEED_POSITION = '/update-feed-position/';
+export const URI_FOLLOW_BY_USER_ID = '/follow/{{user_id}}/';
+export const URI_FOLLOW_BY_USERNAME = '/follow/{{username}}/';
+export const URI_UNFOLLOW_BY_USER_ID = '/unfollow/{{user_id}}/';
+export const URI_UNFOLLOW_BY_USERNAME = '/unfollow/{{username}}/';
+export const URI_BY_USER_ID_FOLLOWERS = '/{{user_id}}/followers/';
+export const URI_ANALYTICS_INTERACTIONS = '/analytics/interactions/';
+export const URI_ANALYTICS_INTERACTIONS_BY_PK =
+  '/analytics/interactions/{{pk}}/';
+export const URI_ANALYTICS_INTERACTIONS_BATCH =
+  '/analytics/interactions/batch/';
+export const URI_ANALYTICS_SESSIONS = '/analytics/sessions/';
+export const URI_ANALYTICS_SESSIONS_BY_PK = '/analytics/sessions/{{pk}}/';
+export const URI_ANALYTICS_NOT_INTERESTED = '/analytics/not-interested/';
+export const URI_ANALYTICS_NOT_INTERESTED_BY_PK =
+  '/analytics/not-interested/{{pk}}/';
+export const URI_ANALYTICS_ACCESSIBILITY_TOOLS =
+  '/analytics/accessibility-tools/';
+export const URI_ANALYTICS_ACCESSIBILITY_TOOLS_BY_PK =
+  '/analytics/accessibility-tools/{{pk}}/';
