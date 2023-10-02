58efb00831c4e3b8e6a8d2755b2bd5e6bd976ac6
Muhammed Shittu <devshittu@gmail.com>
2023-10-02 19:19:11 +0100

    Added pagination types

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,6 +11,25 @@ export type ApiResponse = {
   status: string;
   // data: ApiResponseData;
 };
+
+export type PaginationLinks = {
+  next?: string | null;
+  previous?: string | null;
+};
+
+export type PaginatedListQueryParams = {
+  page?: number | null;
+  page_size?: number | null;
+};
+
+export type PaginatedResponse<T> = {
+  links?: PaginationLinks;
+  count: number;
+  total_pages: number;
+  current_page: number;
+  results: T[];
+};
+
 export type NewsChannel = {
   id: string;
   name: string;
