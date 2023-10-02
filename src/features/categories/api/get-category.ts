098cc20cb89e9d742ee72063fba3672fc0e27dec
Muhammed Shittu <devshittu@gmail.com>
2023-10-02 19:40:10 +0100

    Added compatibility to use uriTemplate utils

--- src/features/categories/api/get-category.ts
+++ src/features/categories/api/get-category.ts
@@ -3,6 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import { apiClient } from '@/lib/api-client';
 
 import { Category } from '../types';
+import {
+  URI_CATEGORIES,
+  URI_CATEGORIES_BY_SLUG,
+} from '@/config/api-constants';
+import { uriTemplate } from '@/utils';
 
 type GetCategoryOptions = {
   category_id: string;
@@ -11,7 +16,11 @@ type GetCategoryOptions = {
 export const getCategory = ({
   category_id,
 }: GetCategoryOptions): Promise<Category> => {
-  return apiClient.get(`/categories/${category_id}`);
+  const uri = uriTemplate(URI_CATEGORIES_BY_SLUG, {
+        slug: category_id,
+      });
+      
+  return apiClient.get(`${uri}`);
 };
 
 export const useCategory = ({ category_id }: GetCategoryOptions) => {
