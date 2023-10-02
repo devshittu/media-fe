d766e3cd79403b73474d166f65d9b84bc34e0d5b
Muhammed Shittu <devshittu@gmail.com>
2023-10-02 19:14:00 +0100

    Updated the bookmark-list to use the pagination

--- src/features/bookmarks/components/blocks/bookmark-list.tsx
+++ src/features/bookmarks/components/blocks/bookmark-list.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
-import { Bookmark, BookmarkListProps, BookmarkResponse } from '../../types';
+import { Bookmark, BookmarkListProps, BookmarkListResponse } from '../../types';
 import { BookmarkItem } from './bookmark-item';
 import { useInfiniteBookmarks } from '../../api/get-bookmarks';
 import { useInView } from 'react-intersection-observer';
 import { useUserFeedsStore } from '@/stores/feeds';
+import { InteractiveLoader } from '@/components/loading';
 
 export const BookmarkList = ({
-  data = {} as BookmarkResponse,
+  data = {} as BookmarkListResponse,
   queryParams,
 }: BookmarkListProps) => {
   const { ref, inView } = useInView();
@@ -18,8 +19,6 @@ export const BookmarkList = ({
     hasNextPage,
     isFetchingNextPage,
   } = useInfiniteBookmarks({ initialData: data, params: queryParams });
-
-  console.log('dataFromBookmarks', dataFromBookmarks);
   useEffect(() => {
     if (inView) {
       fetchNextPage();
@@ -38,27 +37,24 @@ export const BookmarkList = ({
 
   return (
     <>
-      <ol className="mt-3 divide-y divider-slate-200 dark:divide-slate-700">
+      <div className="mt-3 divide-y divider-slate-200 dark:divide-slate-700">
         {dataFromBookmarks?.pages.map((page, i) => (
           <React.Fragment key={i}>
-            {page?.bookmarks?.map((bookmark: Bookmark) => (
+            {page?.results?.map((bookmark: Bookmark) => (
               <BookmarkItem key={bookmark.id} bookmark={bookmark} />
             ))}
           </React.Fragment>
         ))}
-      </ol>
+      </div>
+
       <div>
-        <button
+        <InteractiveLoader
           ref={ref}
-          onClick={() => fetchNextPage()}
-          disabled={!hasNextPage || isFetchingNextPage}
-        >
-          {isFetchingNextPage
-            ? 'Loading more...'
-            : hasNextPage
-            ? 'Load Newer'
-            : 'Nothing more to load'}
-        </button>
+          isLoading={isFetchingNextPage}
+          hasNextPage={hasNextPage || false}
+          onClick={fetchNextPage}
+          // loadingPlaceholder={<StoryListLoadingPlaceholder />}
+        />
       </div>
     </>
   );
