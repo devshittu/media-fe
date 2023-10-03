86f3a176f5e828e121e2da8858defc9d52b391eb
Muhammed Shittu <devshittu@gmail.com>
2023-10-03 19:14:08 +0100

    Added storylines types

---
+++ src/features/storylines/types/index.ts
@@ -0,0 +1,24 @@
+import { Story } from '@/features/stories';
+import {
+  PaginatedListQueryParams,
+  PaginatedResponse,
+  PaginationLinks,
+} from '@/types';
+
+export type Storyline = {
+  id: string;
+  description: string;
+  summary: string;
+  subject: string;
+  hashtags: string;
+  stories_count: number;
+};
+
+
+export type StorylineListProps = {
+  data: StorylineListResponse;
+  queryParams: PaginatedListQueryParams;
+};
+
+export type StorylineListResponse = PaginatedResponse<Storyline>;
+
