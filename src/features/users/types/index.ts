f9ae28c67215318216f9816c7ac7c4a5344e8b53
Muhammed Shittu <devshittu@gmail.com>
2023-10-15 04:13:05 +0100

    Added follow relationship response.

--- src/features/users/types/index.ts
+++ src/features/users/types/index.ts
@@ -1,17 +1,6 @@
+import { User } from '@/features/auth';
 import { NewsChannel, PaginatedResponse } from '@/types';
 
-export type User = {
-  id: string;
-  username: string;
-  email: string;
-  name: string;
-  created_at: string;
-  updated_at: string;
-  last_login: string;
-  roles: string[];
-  avatar_url?: string | null;
-  news_channel?: NewsChannel; // Or you could use id's here like: newsChannelIds: string[];
-};
 
 export type UserListResponse = PaginatedResponse<User>;
 
@@ -24,4 +13,10 @@ export type UserListItemProps = {
   onDelete?: (id: string) => void;
   onFollowSuccess?: (id: string) => void;
   onFollowFailure?: (id: string) => void;
+};
+
+export type FollowerRelationshipResponse = {
+  id: number;
+  follower: User;
+  followed: User;
 };
