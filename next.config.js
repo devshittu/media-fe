6945a279598fc752217fbbc7191ca486a79086fb
Muhammed Shittu <devshittu@gmail.com>
2023-10-16 00:09:01 +0100

    Configured next config with NEXT_SERVER_API_URL to get the seo friendly requests.

--- next.config.js
+++ next.config.js
@@ -35,6 +35,12 @@ const nextConfig = {
       },
     ],
   },
+  serverRuntimeConfig: {
+    apiUrl: process.env.NEXT_SERVER_API_URL,
+  },
+  publicRuntimeConfig: {
+    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 module.exports = nextConfig;
