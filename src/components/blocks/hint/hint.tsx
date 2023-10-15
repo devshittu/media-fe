9d9c038372cfab63a639d3fb6e9b5dd7b454c869
Muhammed Shittu <devshittu@gmail.com>
2023-10-15 03:06:25 +0100

    Added hint to token page.

---
+++ src/components/blocks/hint/hint.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { useToggle } from '@/hooks';
+import { Badge, BadgeSize, BadgeType } from '@/components/blocks/badge';
+import { InfoIcon } from '@/components/illustrations';
+type HintProps = {
+  description: string;
+  notes?: string;
+};
+export const Hint = ({ description, notes }: HintProps) => {
+  const { isVisible, toggleVisibility } = useToggle();
+  const handleClick = () => {
+    toggleVisibility();
+  };
+
+  return (
+    <>
+      <Badge
+        id="show-hint-token"
+        type={BadgeType.INFO}
+        size={BadgeSize.LARGE}
+        onClick={handleClick}
+        rounded
+        icon={<InfoIcon className="w-4 h-4 stroke-[3]" />}
+      >
+        <h2 className="font-bold text-base">Hint</h2>
+      </Badge>
+      <div
+        className={`hint ${
+          isVisible ? 'block' : 'hidden'
+        } bg-sky-200 dark:bg-sky-800 p-2 rounded-sm mt-3`}
+      >
+        <p>{description}</p>
+        {notes && (
+          <p>
+            <strong>Note:</strong> {notes}
+          </p>
+        )}
+      </div>
+    </>
+  );
+};
