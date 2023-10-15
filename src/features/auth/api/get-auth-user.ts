edc574efefc5c5816041330c70f0575e6b2362bb
Muhammed Shittu <devshittu@gmail.com>
2023-10-15 03:39:17 +0100

    Added getAuthUser and error handler.

--- src/features/auth/api/get-auth-user.ts
+++ src/features/auth/api/get-auth-user.ts
@@ -5,19 +5,29 @@ import { apiClient } from '@/lib/api-client';
 import { AuthUser } from '../types';
 
 import { QUERY_KEYS } from '@/config/query';
+import { URI_AUTH_ME } from '@/config/api-constants';
 const { AUTH_USER } = QUERY_KEYS;
 
-export const getAuthUser = (): Promise<AuthUser> => {
-  return apiClient.get('/auth/me');
+export const getAuthUser = async (): Promise<AuthUser> => {
+  return apiClient.get(`${URI_AUTH_ME}`);
 };
 
-export const useUser = () => {
-  const { data, isLoading } = useQuery({
+export const useAuthUser = () => {
+  const { data, isLoading, error } = useQuery({
     queryKey: [AUTH_USER],
     queryFn: () => getAuthUser(),
+
+    onError: (error: any) => {
+      // Handle the error here if needed
+      console.error(error);
+
+      throw new Error(error.response?.data?.message || "Failed to fetch auth user");
+  
+    },
   });
 
-  return { data, isLoading };
+  return { data, isLoading, error };
 };
+
 
 // src/features/auth/api/get-auth-user.ts
