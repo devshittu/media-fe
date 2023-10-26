7203252615de0bcb1f881bf585e1cd92e0d2ce18
Muhammed Shittu <devshittu@gmail.com>
2023-10-26 08:56:32 +0100

    Added path and expanded props

--- src/components/blocks/popup/popup.tsx
+++ src/components/blocks/popup/popup.tsx
@@ -2,13 +2,17 @@ import * as React from 'react';
 import { usePopup } from './hooks/usePopup';
 import { PopupOptions } from './types';
 import { PopupContext } from './hooks/usePopupContext';
-export const Popup: React.FC<
-  {
-    children: React.ReactNode;
-  } & PopupOptions
-> = ({ children, ...options }) => {
-  const dialog = usePopup(options);
+export const Popup = ({
+  children,
+  modal = false,
+  ...restOptions
+}: {
+  children: React.ReactNode;
+} & PopupOptions) => {
+  const popup = usePopup({ modal, ...restOptions });
   return (
-    <PopupContext.Provider value={dialog}>{children}</PopupContext.Provider>
+    <PopupContext.Provider value={popup}>{children}</PopupContext.Provider>
   );
 };
+
+// Path: src/components/blocks/popup/popup.tsx
