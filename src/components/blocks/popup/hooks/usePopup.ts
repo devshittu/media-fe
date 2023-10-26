ecb2181772487069a74d3ca96994e4d0cd959650
Muhammed Shittu <devshittu@gmail.com>
2023-10-26 09:04:00 +0100

    Added few customistions but not yet exposed

--- src/components/blocks/popup/hooks/usePopup.ts
+++ src/components/blocks/popup/hooks/usePopup.ts
@@ -1,17 +1,26 @@
 import * as React from 'react';
 import {
   useFloating,
+  autoUpdate,
+  offset,
+  flip,
+  shift,
   useClick,
   useDismiss,
   useRole,
   useInteractions,
+  useHover,
+  safePolygon,
 } from '@floating-ui/react';
 import { PopupOptions } from '../types';
 
 export const usePopup = ({
   initialOpen = false,
+  placement = 'bottom',
+  modal = true,
   open: controlledOpen,
   onOpenChange: setControlledOpen,
+  onHoverEnabled = false,
 }: PopupOptions = {}) => {
   const [uncontrolledOpen, setUncontrolledOpen] = React.useState(initialOpen);
   const [labelId, setLabelId] = React.useState<string | undefined>();
@@ -23,8 +32,21 @@ export const usePopup = ({
   const setOpen = setControlledOpen ?? setUncontrolledOpen;
 
   const data = useFloating({
+    placement,
     open,
     onOpenChange: setOpen,
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset(5),
+      flip({
+        crossAxis: placement.includes('-'),
+        fallbackAxisSideDirection: 'end',
+        padding: 50,
+      }),
+      shift({
+        padding: 5,
+      }),
+    ],
   });
 
   const context = data.context;
@@ -33,13 +55,28 @@ export const usePopup = ({
     enabled: controlledOpen == null,
   });
   const dismiss = useDismiss(context, {
-    outsidePress: false,
+    // outsidePress: false,
     // outsidePressEvent: 'mousedown',
-    escapeKey: false,
+    // escapeKey: false,
   });
   const role = useRole(context);
 
-  const interactions = useInteractions([click, dismiss, role]);
+  const interactions = useInteractions([
+    click,
+    dismiss,
+    role,
+    useHover(context, {
+      enabled: onHoverEnabled,
+      // both open and close in ms
+      delay: 100,
+      // waits 150 ms once the user's cursor is at rest
+      restMs: 150,
+      handleClose: safePolygon({
+        requireIntent: false,
+      }),
+    }),
+    // useHover(context)
+  ]);
 
   return React.useMemo(
     () => ({
@@ -47,11 +84,14 @@ export const usePopup = ({
       setOpen,
       ...interactions,
       ...data,
+      modal,
       labelId,
       descriptionId,
       setLabelId,
       setDescriptionId,
     }),
-    [open, setOpen, interactions, data, labelId, descriptionId],
+    [open, setOpen, interactions, data, modal, labelId, descriptionId],
   );
 };
+
+//Path: src/components/blocks/popup/hooks/usePopup.ts
