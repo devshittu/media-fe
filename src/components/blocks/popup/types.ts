ecd4d2d2315726094c663ce3e7a69e3923795878
Muhammed Shittu <devshittu@gmail.com>
2023-10-26 08:57:18 +0100

    expanded props

--- src/components/blocks/popup/types.ts
+++ src/components/blocks/popup/types.ts
@@ -1,5 +1,12 @@
+import { Placement } from '@floating-ui/react';
+
 export type PopupOptions = {
   initialOpen?: boolean;
+  onHoverEnabled?: boolean;
+  placement?: Placement;
+  modal?: boolean;
   open?: boolean;
   onOpenChange?: (open: boolean) => void;
 };
+
+// Path: src/components/blocks/popup/types.ts
