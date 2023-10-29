e8474e0aea4b1fea73d13b1567baba5c14278cca
Muhammed Shittu <devshittu@gmail.com>
2023-10-29 11:57:27 +0000

    Expanded props

--- src/features/hashtags/components/types.ts
+++ src/features/hashtags/components/types.ts
@@ -3,6 +3,7 @@ import { Hashtag } from '../types';
 export type HashtagItemProps = {
   hashtag: Hashtag;
   closable?: boolean;
+  size?: 'small' | 'base' | 'large';
 };
 export type HashtagListProps = {
   dataItems?: Hashtag[];
