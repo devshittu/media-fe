8b8c2948d71a2df4dffeb0cbc5c7cb6c7e0dc2d6
Muhammed Shittu <devshittu@gmail.com>
2023-10-29 11:57:05 +0000

    Added ExpandableHashtagListProps

--- src/features/hashtags/components/list.tsx
+++ src/features/hashtags/components/list.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo } from 'react';
+import React, { memo, useMemo, useState } from 'react';
 import { HashtagListItem } from './list-item';
 import { HashtagListProps } from './types';
 import { Hashtag, useHashtags } from '@/features/hashtags';
 import { LoadingButtonTextList } from '@/components/loading/loading-button-text-list';
+import { Button } from '@/components/button';
 
 export const HashtagList = React.memo(
   ({ dataItems = [] }: HashtagListProps) => {
@@ -30,3 +31,51 @@ export const HashtagList = React.memo(
 );
 
 HashtagList.displayName = 'HashtagList';
+
+type ExpandableHashtagListProps = {
+  hashtags: Hashtag[];
+  size?: 'small' | 'base' | 'large';
+  initialVisibleCount?: number; // Prop to set the initial number of visible hashtags
+  isLoading?: boolean; // Prop to show a loading state
+};
+export const ExpandableHashtagList = memo(
+  ({
+    hashtags,
+    initialVisibleCount = 5,
+    size = 'base',
+    isLoading = false,
+  }: ExpandableHashtagListProps) => {
+    const [expanded, setExpanded] = useState(false);
+    const visibleHashtags = expanded
+      ? hashtags
+      : hashtags.slice(0, initialVisibleCount);
+
+    if (isLoading) {
+      return <LoadingButtonTextList wrapped />;
+    }
+
+    return (
+      <div title="Hashtags" className="flex justify-start mb-4 md:mb-8 ">
+        {hashtags.length > 0 ? (
+          <div className="flex gap-4 flex-wrap">
+            {visibleHashtags.map((hashtag, i) => (
+              <HashtagListItem key={i} hashtag={hashtag} size={size} />
+            ))}
+            {hashtags.length > initialVisibleCount && (
+              <Button
+                onClick={() => setExpanded(!expanded)}
+                className="mt-2x underline"
+              >
+                {expanded ? 'See less' : 'See more'}
+              </Button>
+            )}
+          </div>
+        ) : (
+          <p>No hashtag found</p>
+        )}
+      </div>
+    );
+  },
+);
+
+ExpandableHashtagList.displayName = 'ExpandableHashtagList';
