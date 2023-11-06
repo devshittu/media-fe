e4a990fce341f0e16f8529503a2a70aab94e1528
Muhammed Shittu <devshittu@gmail.com>
2023-11-06 22:28:00 +0000

    Exported get-stories-by-category, post-like-story and hooks

--- src/features/stories/index.ts
+++ src/features/stories/index.ts
@@ -2,9 +2,15 @@
 export * from './api/get-story';
 export * from './api/get-stories';
 export * from './api/get-stories-by-hashtag';
+export * from './api/get-stories-by-category';
+// export * from './api/get-storylines';
+export * from './api/post-like-story';
 
 // components:
 export * from './components/';
+
+// hooks:
+export * from './hooks/';
 
 // types:
 export * from './types';
