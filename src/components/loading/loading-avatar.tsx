a426d3851bf8e0a0d41747ac1fa37954db42bec0
Muhammed Shittu <devshittu@gmail.com>
2023-11-06 21:28:13 +0000

    Added props to LoadingAvatar

--- src/components/loading/loading-avatar.tsx
+++ src/components/loading/loading-avatar.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Icon } from '../illustrations';
 import { AvatarIcon } from '../illustrations/icons/others';
 
-export const LoadingAvatar = () => {
+export const LoadingAvatar = ({ ...props }) => {
   return (
     <Icon
       icon={<AvatarIcon />}
       className="text-slate-200 w-14 h-14 dark:text-slate-700"
+      {...props}
     />
   );
 };
