1af03ae533bdcc56698a55564c145a30553ede06
Muhammed Shittu <devshittu@gmail.com>
2023-11-06 21:21:35 +0000

    Refactored PromptPopup

--- src/components/blocks/popup/blocks/prompt-popup.tsx
+++ src/components/blocks/popup/blocks/prompt-popup.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { PopupProps } from '@/stores/ui';
 import {
   Dialog,
   DialogBody,
@@ -9,104 +8,97 @@ import {
   DialogOverlay,
 } from '@/components/blocks/dialog';
 import { Button } from '@/components/button';
-import { usePopupContext } from '@/components/blocks/popup';
 import { AlertTriangleIcon, Icon } from '@/components/illustrations';
+import { Prompt, usePrompts } from '@/stores/ui/prompts';
+import { Popup } from '../popup';
+import { usePopupContext } from '../hooks';
+import { ToastType } from '../../toast';
+import { PopupProps } from '@/stores/ui';
+import { AttentionType } from '@/types';
+
+// export const PromptPop = () => {
+//   const { prompt, dismissPrompt } = usePrompts();
+  // if (!prompt) return null;
+//   return (
+//     <>
+//     {/* <Popup> */}
+//     <PromptPopup key={prompt.id} prompt={prompt} onDismiss={dismissPrompt} />
+//     {/* </Popup> */}
+//     </>
+//   );
+// };
+
+type PromptComponentProps = PopupProps & {
+  // prompt: Omit<Prompt, 'duration'>;
+
+  type?: AttentionType;
+  title?: string;
+  duration?: number;
+  message?: string;
+  onOk: () => void;
+  onOkComplete?: () => void;
+  // onCancel?: () => void;
+};
 
 export const PromptPopup = ({
+  // prompt,
+
   title,
-  subtitle,
-  children,
+  message,
   onOk,
   onClose,
-}: PopupProps & {
-  children?: React.ReactElement;
-  title?: string | null;
-  subtitle?: string | null;
-  onOk: () => void;
-}) => {
+  onOkComplete,
+  type = AttentionType.WARNING,
+}: PromptComponentProps) => {
   const { setOpen } = usePopupContext();
 
-  const handleOk = () => {
-    if (onOk) {
-      onOk();
+  // const { onOkComplete, onOk, onCancel } = prompt;
+
+  const handleOk = async () => {
+    try {
+      await onOk?.(); // Execute the onOk function
+    } finally {
+      onClose?.(); // Close the prompt
+      setOpen(false);
+      onOkComplete?.(); // Execute the onOkComplete callback
     }
-    return setOpen(false); //close the popup from the usePopupContext which is the floating-ui/react library.
   };
   const handleClose = () => {
-    if (onClose) {
-      onClose();
-    }
-    return setOpen(false); //close the popup from the usePopupContext which is the floating-ui/react library.
+    onClose?.();
+    setOpen(false);
+    // onDismiss(prompt.id);
   };
-
-  // Ensure that children is a single React element
-  if (React.Children.count(children) !== 1) {
-    throw new Error(
-      'PromptPopup expects a single child component make sure that children is not conditional which may make it undefined such as {someCondition && <SomeComponent/>.}',
-    );
-  }
 
   return (
     <Dialog>
       <DialogOverlay />
       <DialogContainer width="medium">
         <DialogHeader>
-          <>
-            <div className="flex justify-between items-start">
-              <nav className="flex items-center space-x-2 md:space-x-4">
-                <Icon
-                  icon={<AlertTriangleIcon />}
-                  className="stroke-[2.5] w-10 h-10 text-amber-500"
-                />
-              </nav>
-              <DialogCloseButton onClose={handleClose} />
-            </div>
-          </>
+          <div className="flex justify-between items-start">
+            <Icon
+              icon={<AlertTriangleIcon />}
+              className="stroke-[2.5] w-10 h-10 text-amber-500"
+            />
+            <DialogCloseButton onClose={handleClose} />
+          </div>
         </DialogHeader>
         <DialogBody>
-          {children ? (
-            React.cloneElement(children || <></>, { onCancel: handleClose })
-          ) : (
-            <div className="flex justify-between items-start">
-              <nav className="flex flex-col items-start space-y-2 md:space-y-4">
-                <h1 className="sm:text-2xl text-xl tracking-normal md:tracking-wide leading-6 md:leading-8 font-extrabold m-0 text-slate-900 dark:text-slate-100">
-                  {title || `Are you sure?`}
-                </h1>
-                <p className=" text-base md:text-lg leading-5 m-0 mt-1 whitespace-pre-wrap text-slate-900">
-                  {subtitle || 'Please double-check before moving on.'}
-                </p>
-              </nav>
-            </div>
-          )}
-          <>
-            <br />
-
-            <div className="flex space-x-3">
-              <Button
-                type="primary"
-                //   loading={!!isLoading}
-                //   disabled={isLoading}
-                nativeType="submit"
-                // className="justify-center font-semibold mt-4 w-full md:h-12"
-                onClick={handleOk}
-              >
-                <span className="opacity-100 transition-opacity font-extrabold text-xl">
-                  Proceed
-                </span>
-              </Button>
-              &nbsp;&nbsp;
-              <Button
-                nativeType="submit"
-                outlined
-                type={`adaptive`}
-                onClick={handleClose}
-              >
-                <span className="opacity-100 transition-opacity font-extrabold text-xl">
-                  Cancel
-                </span>
-              </Button>
-            </div>
-          </>
+          <div className="flex flex-col items-start space-y-2 md:space-y-4">
+            <h1 className="sm:text-2xl text-xl tracking-normal md:tracking-wide leading-6 md:leading-8 font-extrabold m-0 text-slate-900 dark:text-slate-100">
+              {title || `Are you sure?`}
+            </h1>
+            <p className="text-base md:text-lg leading-5 m-0 mt-1 whitespace-pre-wrap text-slate-900 dark:text-slate-200">
+              {message || 'Please double-check before moving on.'}
+            </p>
+          </div>
+          <div className="flex space-x-3 mt-4">
+            <Button type="primary" onClick={handleOk}>
+              <span className="font-extrabold text-xl">Proceed</span>
+            </Button>
+            <Button outlined type="adaptive" onClick={handleClose}>
+              <span className="font-extrabold text-xl">Cancel</span>
+            </Button>
+          </div>
         </DialogBody>
       </DialogContainer>
     </Dialog>
