83569c5b9449293dddd525926e434cbdf4eb05d5
Muhammed Shittu <devshittu@gmail.com>
2023-11-12 00:01:48 +0000

    Support feature: added getLegalDocuments

---
+++ src/features/support/api/get-legal-document.ts
@@ -0,0 +1,50 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { apiClient } from '@/lib/api-client';
+
+import { LegalDocumentResponse } from '../types';
+import {
+  URI_SUPPORT_BY_VERSION_PRIVACY_POLICIES,
+  URI_SUPPORT_BY_VERSION_TERMS_AND_CONDITIONS,
+} from '@/config/api-constants';
+import { QUERY_KEYS } from '@/config/query';
+import { uriTemplate } from '@/utils';
+const { GET_LEGAL_DOCUMENT } = QUERY_KEYS;
+
+export type LegalURIParams = {
+  document: 'privacy' | 'terms' | 'faq';
+  version?: string | null;
+};
+type GetLegalDocumentsOptions = {
+  params: LegalURIParams;
+  initialData?: any;
+};
+
+export const getLegalDocuments = ({
+  params,
+}: GetLegalDocumentsOptions): Promise<LegalDocumentResponse> => {
+  const uri = uriTemplate(
+    params.document === 'privacy'
+      ? URI_SUPPORT_BY_VERSION_PRIVACY_POLICIES
+      : URI_SUPPORT_BY_VERSION_TERMS_AND_CONDITIONS,
+    {
+      version: params.version?.toString() as string,
+    },
+  );
+  return apiClient.get(`${uri}`, {
+    params,
+  });
+};
+
+export const useGetLegalDocuments = ({ params }: GetLegalDocumentsOptions) => {
+  const { data, isFetching, isFetched } = useQuery({
+    queryKey: [GET_LEGAL_DOCUMENT, params],
+    queryFn: () => getLegalDocuments({ params }),
+    initialData: {} as LegalDocumentResponse,
+  });
+
+  return {
+    data,
+    isLoading: isFetching && !isFetched,
+  };
+};
