11d159c7169fa782e92d435cf087a887c0788743
Muhammed Shittu <devshittu@gmail.com>
2023-11-12 23:47:07 +0000

    Support feature: applied the auto breadcrumb generator

--- src/components/labs/public-page/hero.tsx
+++ src/components/labs/public-page/hero.tsx
@@ -15,10 +15,11 @@ export const Hero: React.FC<HeroProps> = ({
   breadcrumbTrail,
 }) => {
   return (
-    <section className="bg-slate-50 dark:bg-gray-950 text-slate-900 dark:text-slate-100">
+    <section className="bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-100">
       <div className="container mx-auto flex flex-col gap-4 items-start px-4 py-5 md:py-10 md:px-10 lg:px-32 xl:max-w-3xlx">
         {/* Render the Breadcrumb component */}
-        {breadcrumbTrail && <Breadcrumb trails={breadcrumbTrail} />}
+        {/* {breadcrumbTrail && <Breadcrumb trails={breadcrumbTrail} />} */}
+        {breadcrumbTrail && <Breadcrumb />}
 
         <h1 className="text-4xl font-bold md:leading-normal sm:text-5xl">
           {title}
@@ -30,3 +31,5 @@ export const Hero: React.FC<HeroProps> = ({
 };
 
 export default Hero;
+
+// Path: src/components/labs/public-page/hero.tsx
