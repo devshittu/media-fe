8ce73bfbacf3202235df5bdd9defd48c6ccb419a
Muhammed Shittu <devshittu@gmail.com>
2023-11-17 23:02:58 +0000

    Added FE_URI_GET_SUPPORT_ARTICLES_BY_CATEGORY_SLUG to uri for frontend

--- src/config/frontend-uri-constants.ts
+++ src/config/frontend-uri-constants.ts
@@ -1,4 +1,6 @@
-export const FE_URI_GET_SUPPORT_ARTICLES_BY_SLUG =
+export const FE_URI_GET_SUPPORT_ARTICLE_BY_SLUG =
   '/support/{{version}}/articles/{{slug}}';
+export const FE_URI_GET_SUPPORT_ARTICLES_BY_CATEGORY_SLUG =
+  '/support/{{version}}/articles/category/{{category_slug}}';
 
 // Path: src/config/frontend-uri-constants.ts
