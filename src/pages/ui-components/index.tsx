d7c8a069a03835fe2d7bca5657281e4a42809a63
Muhammed Shittu <devshittu@gmail.com>
2023-11-17 23:21:16 +0000

    Fixed form error

--- src/pages/ui-components/index.tsx
+++ src/pages/ui-components/index.tsx
@@ -1,11 +1,12 @@
 import { Seo } from '@/components/seo';
 import { Button } from '@/components/button';
-import { FieldError, InputField } from '@/components/form';
+import { InputField } from '@/components/form';
 import { ReactElement, useState } from 'react';
 import { Link } from '@/components/labs/typography';
 import CheckboxGroup from '@/components/form/checkbox-group';
 import RadioGroup from '@/components/form/radio-group';
 import UserLayout from '@/layouts/user-layout';
+import { FieldError } from 'react-hook-form';
 
 // const inter = Inter({ subsets: ['latin'] });
 
@@ -44,11 +45,11 @@ export default function Home() {
     setEmail(value);
 
     // Example validation
-    if (value.trim() === '') {
-      setEmailError({ message: 'Email is required' });
-    } else {
-      setEmailError(null);
-    }
+    // if (value.trim() === '') {
+    //   setEmailError({ message: 'Email is required' });
+    // } else {
+    //   setEmailError(null);
+    // }
   };
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -129,7 +130,7 @@ export default function Home() {
           <InputField
             id="1"
             name="Email"
-            error={emailError}
+            // error={emailError}
             placeholder="@example.com"
           />
           <InputField
@@ -138,8 +139,8 @@ export default function Home() {
             name="EmailField"
             placeholder="Enter your email"
             value={email}
-            onChange={handleEmailChange}
-            error={emailError}
+            // onChange={handleEmailChange}
+            // error={emailError}
           />
 
           <Button nativeType="submit">Submit</Button>
@@ -156,8 +157,8 @@ export default function Home() {
                 name="EmailField"
                 placeholder="Enter your email"
                 value={email}
-                onChange={handleEmailChange}
-                error={emailError}
+                // onChange={handleEmailChange}
+                // error={emailError}
               />
               <InputField
                 id="4"
@@ -165,8 +166,8 @@ export default function Home() {
                 name="Small Field"
                 placeholder="Enter your email"
                 value={email}
-                onChange={handleEmailChange}
-                error={emailError}
+                // onChange={handleEmailChange}
+                // error={emailError}
                 size="small"
               />
 
@@ -176,8 +177,8 @@ export default function Home() {
                 name="Base Field"
                 placeholder="Enter your email"
                 value={email}
-                onChange={handleEmailChange}
-                error={emailError}
+                // onChange={handleEmailChange}
+                // error={emailError}
               />
             </div>
 
@@ -188,8 +189,8 @@ export default function Home() {
               name="EmailField"
               placeholder="Enter your email"
               value={email}
-              onChange={handleEmailChange}
-              error={emailError}
+              // onChange={handleEmailChange}
+              // error={emailError}
               size="large"
             />
             <InputField
@@ -198,8 +199,8 @@ export default function Home() {
               name="Small Field"
               placeholder="Enter your email"
               value={email}
-              onChange={handleEmailChange}
-              error={emailError}
+              // onChange={handleEmailChange}
+              // error={emailError}
               size="small"
             />
 
@@ -209,8 +210,8 @@ export default function Home() {
               name="Base Field"
               placeholder="Enter your email"
               value={email}
-              onChange={handleEmailChange}
-              error={emailError}
+              // onChange={handleEmailChange}
+              // error={emailError}
             />
             <InputField
               id="9"
@@ -218,8 +219,8 @@ export default function Home() {
               name="Password"
               placeholder="Enter your email"
               value={email}
-              onChange={handleEmailChange}
-              error={emailError}
+              // onChange={handleEmailChange}
+              // error={emailError}
             />
             <InputField
               id="10"
@@ -227,8 +228,8 @@ export default function Home() {
               name="Confirm Password"
               placeholder="Enter your email"
               value={email}
-              onChange={handleEmailChange}
-              error={emailError}
+              // onChange={handleEmailChange}
+              // error={emailError}
             />
             <div className="flex items-start mb-6">
               <div className="flex items-center h-5">
@@ -236,13 +237,13 @@ export default function Home() {
                   id="remember"
                   type="checkbox"
                   value=""
-                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                  className="w-4 h-4 border border-slate-300 rounded bg-slate-50 focus:ring-3 focus:ring-blue-300 dark:bg-slate-700 dark:border-slate-600 dark:focus:ring-blue-600 dark:ring-offset-slate-800"
                   required
                 />
               </div>
               <label
                 htmlFor="remember"
-                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                className="ml-2 text-sm font-medium text-slate-900 dark:text-slate-400"
               >
                 I agree with the{' '}
                 <Link
@@ -266,13 +267,13 @@ export default function Home() {
             These input field components is part of a larger, open-source
             library of Tailwind CSS components. Learn more by going to the
             official{' '}
-            <a
+            <Link
               className="text-blue-600 hover:underline"
               href="https://example.com/docs/getting-started/introduction/"
               target="_blank"
             >
               Flowbite Documentation
-            </a>
+            </Link>
             .
           </p>
         </div>
@@ -285,19 +286,19 @@ export default function Home() {
         </button>
         <button
           type="button"
-          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-slate-900 focus:outline-none bg-white rounded-full border border-slate-200 hover:bg-slate-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-slate-200 dark:focus:ring-slate-700 dark:bg-slate-800 dark:text-slate-400 dark:border-slate-600 dark:hover:text-white dark:hover:bg-slate-700"
         >
           Alternative
         </button>
         <button
           type="button"
-          className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          className="text-white bg-slate-800 hover:bg-slate-900 focus:outline-none focus:ring-4 focus:ring-slate-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-slate-800 dark:hover:bg-slate-700 dark:focus:ring-slate-700 dark:border-slate-700"
         >
           Dark
         </button>
         <button
           type="button"
-          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+          className="text-slate-900 bg-white border border-slate-300 focus:outline-none hover:bg-slate-100 focus:ring-4 focus:ring-slate-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-slate-800 dark:text-white dark:border-slate-600 dark:hover:bg-slate-700 dark:hover:border-slate-600 dark:focus:ring-slate-700"
         >
           Light
         </button>
