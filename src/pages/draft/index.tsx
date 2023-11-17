31c2864bfaa977323285f0e7005cedeae7bf6f1a
Muhammed Shittu <devshittu@gmail.com>
2023-11-17 23:19:48 +0000

    Gray to slate

--- src/pages/draft/index.tsx
+++ src/pages/draft/index.tsx
@@ -10,50 +10,50 @@ const Index = () => {
       {/* <article className={styles.draft_main}> */}
       <article className={`col-span-8 border-rx`}>
         <div className="p-4 sm:ml-64x">
-          <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+          <div className="p-4 border-2 border-slate-200 border-dashed rounded-lg dark:border-slate-700">
             <div className="grid grid-cols-3 gap-4 mb-4">
-              <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center h-24 rounded bg-slate-50 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
-              <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center h-24 rounded bg-slate-50 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
-              <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center h-24 rounded bg-slate-50 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
             </div>
-            <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-              <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+            <div className="flex items-center justify-center h-48 mb-4 rounded bg-slate-50 dark:bg-slate-800">
+              <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
             </div>
             <div className="grid grid-cols-2 gap-4 mb-4">
-              <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center rounded bg-slate-50 h-28 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
-              <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center rounded bg-slate-50 h-28 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
-              <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center rounded bg-slate-50 h-28 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
-              <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center rounded bg-slate-50 h-28 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
             </div>
-            <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-              <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+            <div className="flex items-center justify-center h-48 mb-4 rounded bg-slate-50 dark:bg-slate-800">
+              <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
             </div>
             <div className="grid grid-cols-2 gap-4">
-              <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center rounded bg-slate-50 h-28 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
-              <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center rounded bg-slate-50 h-28 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
-              <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center rounded bg-slate-50 h-28 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
-              <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+              <div className="flex items-center justify-center rounded bg-slate-50 h-28 dark:bg-slate-800">
+                <p className="text-2xl text-slate-400 dark:text-slate-500">+</p>
               </div>
             </div>
           </div>
