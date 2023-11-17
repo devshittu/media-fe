0953491d5432b66cb0724f34103e1a2be08ea51d
Muhammed Shittu <devshittu@gmail.com>
2023-11-17 23:04:24 +0000

    Rewritten the content-section

--- src/components/labs/public-page/content-section.tsx
+++ src/components/labs/public-page/content-section.tsx
@@ -5,15 +5,13 @@ type ContentSectionProps = {
 };
 
 export const ContentSection: React.FC<ContentSectionProps> = ({ children }) => {
+
   return (
-    <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-slate-900">
-      <div className="flex justify-between px-4 mx-auto max-w-screen-xl">
-        <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
-          {children}
-        </article>
+    <section className="text-slate-600 body-font">
+      <div className="container mx-auto flex flex-col gap-4 items-start px-4 py-5 md:py-10 md:px-10 lg:px-32 xl:max-w-3xlx">
+        <div className=" w-full 2xl:w-4/5  lg:my-4">{children}</div>
       </div>
-    </main>
-  );
+    </section>)
 };
 
 type TopicContentSectionProps = {
@@ -31,15 +29,14 @@ export const TopicContentSection = ({
 }: TopicContentSectionProps) => {
   return (
     <section className="text-slate-600 body-font">
-      {/* <div className="container px-5 py-24 mx-auto"> */}
       <div className="container mx-auto flex flex-col gap-4 items-start px-4 py-5 md:py-10 md:px-10 lg:px-32 xl:max-w-3xlx">
         <h1 className="mb-4 text-3xl font-extrabold tracking-tight text-slate-900 sm:text-4xl dark:text-white">
           {title}
         </h1>
-        <p className="text-base sm:text-lg md:text-xl leading-relaxed w-full xl:w-3/4 lg:w-4/5 text-slate-700 dark:text-slate-300">
+        <p className="text-base sm:text-lg md:text-xl leading-relaxed w-full xl:w-3/4 lg:w-4/5 mb-2 lg:mb-4 text-slate-700 dark:text-slate-300">
           {description}
         </p>
-        <div className=" w-full lg:w-4/5">{children}</div>
+        <div className=" w-full 2xl:w-4/5  lg:my-4">{children}</div>
 
         {hint && (
           <div className=" max-w-screen-sm text-sm text-left text-slate-500 dark:text-slate-300">
