c6bb74582a81206d4eb6ba46a168acaa672ff7bc
Muhammed Shittu <devshittu@gmail.com>
2023-11-17 23:10:54 +0000

    removed unwanted comments

--- src/features/support/components/blocks/extra-articles.tsx
+++ src/features/support/components/blocks/extra-articles.tsx
@@ -3,25 +3,18 @@ import { SupportArticle, useGetSupportArticles } from '../../';
 import React, { useMemo } from 'react';
 import { Link } from '@/components/labs/typography';
 import { APP_SUPPORT_VERSION } from '@/config/constants';
-import { uriTemplate } from '@/utils';
-import { FE_URI_GET_SUPPORT_ARTICLES_BY_SLUG } from '@/config/frontend-uri-constants';
+import { uriTemplate, word_pluralize } from '@/utils';
+import { FE_URI_GET_SUPPORT_ARTICLE_BY_SLUG } from '@/config/frontend-uri-constants';
 
 type ExtraArticlesProps = {
   heading: string;
-  // articles: {
-  //   imageUrl: string;
-  //   title: string;
-  //   description: string;
-  //   readTime: string;
-  //   url: string;
-  // }[];
 };
 
 export const ExtraArticles = ({ heading }: ExtraArticlesProps) => {
   const { data: responseData, isLoading } = useGetSupportArticles({
     params: {
-      page_size: 15,
-      document: 'article',
+      page_size: 4,
+      // document: 'article',
       version: `${APP_SUPPORT_VERSION}`,
     },
   }); // Use data and isLoading directly from the hook
@@ -45,21 +38,11 @@ export const ExtraArticles = ({ heading }: ExtraArticlesProps) => {
           {stableSupportArticles?.length > 0 &&
             stableSupportArticles.map(
               (article: SupportArticle, index: number) => (
-                // {articles.map((article, index) => (
                 <article key={index} className="max-w-xs">
-                  {/* <a href={article.url}>
-                <img
-                  width={500}
-                  height={500}
-                  src={article.imageUrl}
-                  className="mb-5 rounded-lg"
-                  alt={` ${index + 1}`}
-                />
-              </a> */}
                   <h2 className="mb-2 text-xl font-bold leading-tight text-slate-900 dark:text-white">
                     <Link
                       href={
-                        uriTemplate(`${FE_URI_GET_SUPPORT_ARTICLES_BY_SLUG}`, {
+                        uriTemplate(`${FE_URI_GET_SUPPORT_ARTICLE_BY_SLUG}`, {
                           version: APP_SUPPORT_VERSION as string,
                           slug: article.slug,
                         }) || '#'
@@ -69,11 +52,11 @@ export const ExtraArticles = ({ heading }: ExtraArticlesProps) => {
                     </Link>
                   </h2>
                   <p className="mb-4 font-light text-slate-500 dark:text-slate-400">
-                    {article.content}
+                    {article.summary}
                   </p>
                   <Link
                     href={
-                      uriTemplate(`${FE_URI_GET_SUPPORT_ARTICLES_BY_SLUG}`, {
+                      uriTemplate(`${FE_URI_GET_SUPPORT_ARTICLE_BY_SLUG}`, {
                         version: APP_SUPPORT_VERSION as string,
                         slug: article.slug,
                       }) || '#'
@@ -81,7 +64,11 @@ export const ExtraArticles = ({ heading }: ExtraArticlesProps) => {
                     className="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline"
                   >
                     {/* Read in {article.readTime} */}
-                    Read in {article.slug}
+                    Read in{' '}
+                    {`${article.reading_time} ${word_pluralize(
+                      'minute',
+                      article.reading_time,
+                    )}`}
                   </Link>
                 </article>
               ),
