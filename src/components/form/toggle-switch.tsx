8a93e9ed72b1feb3bc404c623df7476333a6b99d
Muhammed Shittu <devshittu@gmail.com>
2023-12-06 00:32:19 +0000

    Supported the light mode

--- src/components/form/toggle-switch.tsx
+++ src/components/form/toggle-switch.tsx
@@ -71,8 +71,8 @@ export const ToggleSwitch = forwardRef<HTMLInputElement, ToggleSwitchProps>(
             }}
             {...restProps}
           />
-          <div className="w-10 h-6 rounded-full shadow-inner dark:bg-slate-400 peer-checked:dark:bg-cyan-400"></div>
-          <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-slate-800"></div>
+          <div className="w-10 h-6 rounded-full shadow-inner  bg-slate-600 peer-checked:bg-cyan-600 dark:bg-slate-400 peer-checked:dark:bg-cyan-400"></div>
+          <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto bg-slate-200 dark:bg-slate-800"></div>
         </span>
         {/* {rightLabel && (<h4 className={`${labelClasses}`}>{rightLabel}</h4>)} */}
         {rightLabel && (
