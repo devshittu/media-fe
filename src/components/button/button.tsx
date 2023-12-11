2c6f659b6218a0f675ca72dd3c50f0fd290f8268
Muhammed Shittu <devshittu@gmail.com>
2023-12-11 18:56:23 +0000

    Added forwardRef for button for even more control

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,5 @@
-import React, { MouseEventHandler, ReactNode } from 'react';
+import React, { ForwardedRef, MouseEventHandler, ReactNode } from 'react';
+import { Icon, LoaderIcon } from '../illustrations';
 
 export type ButtonProps = {
   rounded?: boolean;
@@ -35,31 +36,38 @@ export type ButtonProps = {
   as?: 'a' | 'button';
   className?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
+
+  ref?: ForwardedRef<HTMLButtonElement>;
 };
 
-export const Button = ({
-  outlined = false,
-  title = '',
-  rounded = false,
-  disabled = false,
-  loading = false,
-  nativeType = 'button',
-  type = null,
-  icon,
-  iconPosition = 'left',
-  size = 'medium',
-  expand = false,
-  badge,
-  badgeType = 'success',
-  children,
-  className,
-  onClick,
-}: ButtonProps) => {
-  const getButtonTypeClasses = (): string => {
-    let classes = ``;
-    switch (type) {
-      case 'info':
-        classes += ` 
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      outlined = false,
+      title = '',
+      rounded = false,
+      disabled = false,
+      loading = false,
+      nativeType = 'button',
+      type = null,
+      icon,
+      iconPosition = 'left',
+      size = 'medium',
+      expand = false,
+      badge,
+      badgeType = 'success',
+      children,
+      className,
+      onClick,
+      ...rest
+    }: ButtonProps,
+    ref,
+  ) => {
+    const getButtonTypeClasses = (): string => {
+      let classes = ``;
+      switch (type) {
+        case 'info':
+          classes += ` 
         ${
           !outlined
             ? `bg-sky-500 hover:bg-sky-600 text-white`
@@ -71,9 +79,9 @@ export const Button = ({
                   : 'border'
               }`
         } focus:ring-sky-300 `;
-        break;
-      case 'secondary':
-        classes += ` 
+          break;
+        case 'secondary':
+          classes += ` 
         ${
           !outlined
             ? `bg-slate-500 hover:bg-slate-600 text-white`
@@ -85,9 +93,9 @@ export const Button = ({
                   : 'border'
               }`
         } focus:ring-slate-300 `;
-        break;
-      case 'success':
-        classes += ` 
+          break;
+        case 'success':
+          classes += ` 
         ${
           !outlined
             ? `bg-emerald-500 hover:bg-emerald-600 text-white`
@@ -99,9 +107,9 @@ export const Button = ({
                   : 'border'
               }`
         } focus:ring-emerald-300 `;
-        break;
-      case 'danger':
-        classes += ` 
+          break;
+        case 'danger':
+          classes += ` 
         ${
           !outlined
             ? `bg-rose-500 hover:bg-rose-600 text-white`
@@ -113,9 +121,9 @@ export const Button = ({
                   : 'border'
               }`
         } focus:ring-rose-300 `;
-        break;
-      case 'warning':
-        classes += ` 
+          break;
+        case 'warning':
+          classes += ` 
         ${
           !outlined
             ? `bg-amber-500 hover:bg-amber-600 text-white`
@@ -127,9 +135,9 @@ export const Button = ({
                   : 'border'
               }`
         } focus:ring-amber-300 `;
-        break;
-      case 'dark':
-        classes += ` 
+          break;
+        case 'dark':
+          classes += ` 
         ${
           !outlined
             ? `bg-neutral-900 hover:bg-neutral-950 text-white`
@@ -141,10 +149,10 @@ export const Button = ({
                   : 'border'
               }`
         } focus:ring-neutral-300 `;
-        break;
+          break;
 
-      case 'adaptive':
-        classes += ` 
+        case 'adaptive':
+          classes += ` 
         ${
           !outlined
             ? `bg-neutral-900 dark:bg-neutral-300 hover:bg-neutral-950 dark:hover:bg-neutral-200 dark:text-black text-white`
@@ -156,10 +164,10 @@ export const Button = ({
                   : 'border'
               }`
         } dark:focus:ring-neutral-300 focus:ring-neutral-700  `;
-        break;
+          break;
 
-      case 'primary':
-        classes += ` 
+        case 'primary':
+          classes += ` 
         ${
           !outlined
             ? `bg-cyan-500 hover:bg-cyan-600 text-white`
@@ -171,41 +179,41 @@ export const Button = ({
                   : 'border'
               }`
         } focus:ring-cyan-300 `;
-        break;
-      default:
-        classes += ``;
-    }
-    return classes;
-  };
+          break;
+        default:
+          classes += ``;
+      }
+      return classes;
+    };
 
-  const buttonTypeClasses = getButtonTypeClasses();
+    const buttonTypeClasses = getButtonTypeClasses();
 
-  const getBadgeTypeClasses = (): string => {
-    let classes = ``;
-    switch (badgeType) {
-      case 'info':
-        classes += `bg-sky-500 text-sky-900`;
-        break;
-      case 'secondary':
-        classes += ` bg-slate-500 text-slate-900`;
-        break;
-      case 'success':
-        classes += ` bg-emerald-500 text-emerald-900`;
-        break;
-      case 'danger':
-        classes += ` bg-rose-500 text-rose-900`;
-        break;
-      case 'warning':
-        classes += ` bg-amber-500 text-amber-900`;
-        break;
-      case 'primary':
-      default:
-        classes += ` bg-neutral-900 text-neutral-950 text-white`;
-    }
-    return `${classes} `;
-  };
-  const badgeTypeClasses = getBadgeTypeClasses();
-  const buttonClasses = `relative inline-flex items-center
+    const getBadgeTypeClasses = (): string => {
+      let classes = ``;
+      switch (badgeType) {
+        case 'info':
+          classes += `bg-sky-500 text-sky-900`;
+          break;
+        case 'secondary':
+          classes += ` bg-slate-500 text-slate-900`;
+          break;
+        case 'success':
+          classes += ` bg-emerald-500 text-emerald-900`;
+          break;
+        case 'danger':
+          classes += ` bg-rose-500 text-rose-900`;
+          break;
+        case 'warning':
+          classes += ` bg-amber-500 text-amber-900`;
+          break;
+        case 'primary':
+        default:
+          classes += ` bg-neutral-900 text-neutral-950 text-white`;
+      }
+      return `${classes} `;
+    };
+    const badgeTypeClasses = getBadgeTypeClasses();
+    const buttonClasses = `relative inline-flex items-center
     ${buttonTypeClasses} ${type !== null && 'focus:z-10 focus:ring-4'} 
     ${rounded ? 'rounded-full' : ''}
     ${disabled ? 'opacity-50 cursor-not-allowed' : 'opacity-100 cursor-pointer'}
@@ -220,87 +228,78 @@ export const Button = ({
     ${expand ? 'w-full' : ''}
     ${className ?? ''}
   `
-    .trim()
-    .replace(/[\n\t]/g, '')
-    .replace(/\s+/g, ' '); // to remove extra spaces.
+      .trim()
+      .replace(/[\n\t]/g, '')
+      .replace(/\s+/g, ' '); // to remove extra spaces.
 
-  const iconClasses = `${
-    size === 'large' ? ' h-8 w-8' : size === 'medium' ? ' h-5 w-5' : ' h-4 w-4'
-  }`
-    .trim()
-    .replace(/[\n\t]/g, '')
-    .replace(/\s+/g, ' ');
+    const iconClasses = `${
+      size === 'large'
+        ? ' h-8 w-8'
+        : size === 'medium'
+        ? ' h-5 w-5'
+        : ' h-4 w-4'
+    }`
+      .trim()
+      .replace(/[\n\t]/g, '')
+      .replace(/\s+/g, ' ');
 
-  const buttonContent = (
-    <>
-      {icon && iconPosition === 'left' && (
-        <span className="mr-2">
-          {React.cloneElement(icon, {
-            className: iconClasses,
-          })}
-        </span>
-      )}
-      {children}
-      {icon && iconPosition === 'right' && (
-        <span className="ml-2">
-          {React.cloneElement(icon, {
-            className: iconClasses,
-          })}
-        </span>
-      )}
-    </>
-  );
-
-  const buttonWithBadge = (
-    <>
-      {buttonContent}
-      {badge && (
-        <span
-          className={`absolute -top-2 -right-2 px-2 py-1 text-xs ${badgeTypeClasses} rounded-full`}
-        >
-          {badge}
-        </span>
-      )}
-    </>
-  );
-  return (
-    <button
-      type={nativeType}
-      className={buttonClasses}
-      disabled={disabled}
-      onClick={onClick}
-      title={title}
-    >
-      {loading ? (
-        <>
-          <span className="mx-3">
-            <svg
-              className={`animate-spin -ml-1 ${iconClasses}`}
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
+    const buttonContent = (
+      <>
+        {icon && iconPosition === 'left' && (
+          <span className="mr-2">
+            {React.cloneElement(icon, {
+              className: iconClasses,
+            })}
           </span>
-        </>
-      ) : (
-        <>{buttonWithBadge}</>
-      )}
-    </button>
-  );
-};
+        )}
+        {children}
+        {icon && iconPosition === 'right' && (
+          <span className="ml-2">
+            {React.cloneElement(icon, {
+              className: iconClasses,
+            })}
+          </span>
+        )}
+      </>
+    );
 
+    const buttonWithBadge = (
+      <>
+        {buttonContent}
+        {badge && (
+          <span
+            className={`absolute -top-2 -right-2 px-2 py-1 text-xs ${badgeTypeClasses} rounded-full`}
+          >
+            {badge}
+          </span>
+        )}
+      </>
+    );
+    return (
+      <button
+        type={nativeType}
+        className={buttonClasses}
+        disabled={disabled}
+        onClick={onClick}
+        title={title}
+        {...rest}
+      >
+        {loading ? (
+          <>
+            <span className="mx-3">
+              <Icon
+                icon={<LoaderIcon />}
+                className={`animate-spin -ml-1 ${iconClasses}`}
+              />
+            </span>
+          </>
+        ) : (
+          <>{buttonWithBadge}</>
+        )}
+      </button>
+    );
+  },
+);
+
+Button.displayName = 'Button';
 //Path src/components/button/button.tsx
