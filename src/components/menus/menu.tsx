66111972d54ad1914d2c3db90041b8cfeb846424
Muhammed Shittu <devshittu@gmail.com>
2023-12-15 02:56:48 +0000

    Applied button id

--- src/components/menus/menu.tsx
+++ src/components/menus/menu.tsx
@@ -118,6 +118,7 @@ export const MenuButtonItem = ({
   return (
     <>
       <Button
+        id={`menu-id-${label}`}
         nativeType="button"
         className={`relative inline-flex items-center w-full px-4 py-2 text-base xl:text-lg font-medium hover:bg-slate-100 hover:text-cyan-600 focus:z-10 focus:ring-2 focus:ring-cyan-600 focus:text-cyan-600 dark:border-slate-800 dark:hover:bg-slate-800 dark:hover:text-white dark:focus:ring-slate-500 dark:focus:text-cyan-500 ${
           disabled ? 'opacity-50 cursor-not-allowed' : ''
