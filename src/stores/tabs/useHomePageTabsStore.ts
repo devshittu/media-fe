66ba4d96fc3a7e4a99873c2edacd3c28beb43af8
Muhammed Shittu <devshittu@gmail.com>
2023-12-16 20:08:40 +0000

    Swapped following for discover

--- src/stores/tabs/useHomePageTabsStore.ts
+++ src/stores/tabs/useHomePageTabsStore.ts
@@ -2,12 +2,12 @@ import { createTabStore, useTabStore } from './hooks';
 
 const HomePageTabs = createTabStore({
   initialTabs: [
+    { id: 'discover', label: 'Discover' },
     { id: 'for-you', label: 'For You' },
-    { id: 'following', label: 'Following' },
     // { id: 'saved', label: 'Save' },
     // Add more tabs as needed
   ],
-  initialActiveTab: 'for-you',
+  initialActiveTab: 'discover',
 });
 
 export const useHomePageTabs = () => useTabStore(HomePageTabs);
