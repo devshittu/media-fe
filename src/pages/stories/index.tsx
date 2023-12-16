decc3d12ad0e3bf18df229a23a48ffce4cadc675
Muhammed Shittu <devshittu@gmail.com>
2023-12-16 20:09:54 +0000

    Added Discover

--- src/pages/stories/index.tsx
+++ src/pages/stories/index.tsx
@@ -1,15 +1,22 @@
-import { ReactElement } from 'react';
+import { ReactElement, useMemo } from 'react';
 import UserLayout from '@/layouts/user-layout';
 import { StoriesPageHeader } from '@/components/blocks/headers';
 import { StoryList } from '@/features/stories/components';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
-import { StoriesQueryParams, Story, getStories } from '@/features/stories';
+import {
+  StoriesQueryParams,
+  Story,
+  getStories,
+  getUserFeedStories,
+  useUserFeedStories,
+} from '@/features/stories';
 import { PAGINATE_STORIES_LIMIT } from '@/config/constants';
 import { StoriesPageFrame } from '@/components/frames';
 import { NotFound } from '@/components/not-found';
 import { cleanObject } from '@/utils';
 import { useTabContentManager } from '@/components/blocks/tab';
 import { useHomePageTabs } from '@/stores/tabs';
+import { Discover } from '@/features/trends/components/blocks/discover';
 
 type PublicStoriesPageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
@@ -17,21 +24,50 @@ type PublicStoriesPageProps = InferGetServerSidePropsType<
 
 const StoriesPage = ({
   stories,
+  userFeed,
   queryParams,
   error,
 }: PublicStoriesPageProps) => {
-  // Define asynchronous functions for each tab
-  const fetchDataForYou = async () => {
-    // Async operation for the "For You" tab
-    console.log('TabTest: Fetching data for you');
-  };
+  // console.log('userFeed', userFeed);
 
-  const fetchDataFollowing = async () => {
-    // Async operation for the "Following" tab
-    console.log('TabTest: Fetching data  following ');
-  };
+  const { data: responseData, isLoading } = useUserFeedStories({
+    params: { page_size: 100 },
+  }); // Use data and isLoading directly from the hook
+
+  const stableUserFeedStories = useMemo(
+    () => responseData?.results,
+    [responseData?.results],
+  );
+  // Define asynchronous functions for each tab
+  const fetchDataForYou = useMemo(
+    () => async () => {
+      // Async operation for the "For You" tab
+      console.log('TabTest: Fetching data for you');
+    },
+    [],
+  );
+  const fetchDataDiscover = useMemo(
+    () => async () => {
+      // Async operation for the "Discover" tab
+      console.log('TabTest: Fetching data discover');
+    },
+    [],
+  );
 
   const tabsConfig = {
+    discover: {
+      content: (
+        <>
+          <Discover />
+          {JSON.stringify(stableUserFeedStories?.length)}
+          {!stories.results && <NotFound />}
+          {stories.results?.length > 0 && (
+            <StoryList data={stories} queryParams={queryParams} />
+          )}
+        </>
+      ),
+      fetchData: fetchDataDiscover,
+    },
     'for-you': {
       content: (
         <>
@@ -43,18 +79,6 @@ const StoriesPage = ({
         </>
       ),
       fetchData: fetchDataForYou,
-    },
-    following: {
-      content: (
-        <>
-          <h1>Following</h1>
-          {!stories.results && <NotFound />}
-          {stories.results?.length > 0 && (
-            <StoryList data={stories} queryParams={queryParams} />
-          )}
-        </>
-      ),
-      fetchData: fetchDataFollowing,
     },
     // ... other tabs
   };
@@ -96,21 +120,106 @@ StoriesPage.getLayout = function getLayout(page: ReactElement) {
 
 export const getServerSideProps = async ({
   params,
+  req,
 }: GetServerSidePropsContext) => {
   const queryParams: StoriesQueryParams = cleanObject({
     page: 1,
-    per_page: PAGINATE_STORIES_LIMIT,
+    page_size: PAGINATE_STORIES_LIMIT,
   });
+  console.log(
+    'req.cookies:',
+    req.cookies,
+    'req.headers.cookie:',
+    req.headers.cookie,
+  );
+
+  // try {
+  //   const userFeed = await getUserFeedStories({ params: queryParams });
+  //   const stories = await getStories({ params: queryParams });
+
+  //   console.log('getServerSideProps: userFeed://', userFeed);
+  //   // Check if the results are empty
+  //   if (
+  //     (!stories.results || stories.results.length === 0) &&
+  //     (!userFeed.results || userFeed.results.length === 0)
+  //   ) {
+  //     return {
+  //       props: {
+  //         error: 'No stories found.',
+  //         stories: {
+  //           links: {},
+  //           count: 0,
+  //           total_pages: 0,
+  //           current_page: 0,
+  //           results: [] as Story[],
+  //         },
+  //         userFeed: {
+  //           links: {},
+  //           count: 0,
+  //           total_pages: 0,
+  //           current_page: 0,
+  //           results: [] as Story[],
+  //         },
+  //         queryParams,
+  //       },
+  //     };
+  //   }
+
+  //   return {
+  //     props: {
+  //       stories,
+  //       userFeed,
+  //       queryParams,
+  //     },
+  //   };
+  // } catch (error) {
+  //   console.error('Error fetching stories in getServerSideProps:', error);
+
+  //   return {
+  //     props: {
+  //       error:
+  //         'There was an error fetching the stories. Please try again later.',
+  //       stories: {
+  //         links: {},
+  //         count: 0,
+  //         total_pages: 0,
+  //         current_page: 0,
+  //         results: [] as Story[],
+  //       },
+  //       userFeed: {
+  //         links: {},
+  //         count: 0,
+  //         total_pages: 0,
+  //         current_page: 0,
+  //         results: [] as Story[],
+  //       },
+  //       queryParams,
+  //     },
+  //   };
+  // }
 
   try {
-    const stories = await getStories({ params: queryParams });
+    const [userFeedResponse, storiesResponse] = await Promise.all([
+      getUserFeedStories({ params: queryParams }),
+      getStories({ params: queryParams }),
+    ]);
 
     // Check if the results are empty
-    if (!stories.results || stories.results.length === 0) {
+    if (
+      (!storiesResponse.results || storiesResponse.results.length === 0) &&
+      (!userFeedResponse.results || userFeedResponse.results.length === 0)
+    ) {
       return {
         props: {
           error: 'No stories found.',
           stories: {
+            links: {},
+            count: 0,
+            total_pages: 0,
+            current_page: 0,
+            results: [] as Story[],
+          },
+          userFeed: {
             links: {},
             count: 0,
             total_pages: 0,
@@ -124,18 +233,25 @@ export const getServerSideProps = async ({
 
     return {
       props: {
-        stories,
+        stories: storiesResponse,
+        userFeed: userFeedResponse,
         queryParams,
       },
     };
   } catch (error) {
     console.error('Error fetching stories in getServerSideProps:', error);
-
     return {
       props: {
         error:
           'There was an error fetching the stories. Please try again later.',
         stories: {
+          links: {},
+          count: 0,
+          total_pages: 0,
+          current_page: 0,
+          results: [] as Story[],
+        },
+        userFeed: {
           links: {},
           count: 0,
           total_pages: 0,
