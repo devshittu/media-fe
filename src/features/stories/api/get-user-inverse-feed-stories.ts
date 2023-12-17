217ce77927ccb4aaa2833188daf9657cc5aba9cf
Muhammed Shittu <devshittu@gmail.com>
2023-12-17 23:46:14 +0000

    Added the getUserInvertedFeedStories

--- src/features/stories/api/get-user-inverse-feed-stories.ts
+++ src/features/stories/api/get-user-inverse-feed-stories.ts
@@ -17,18 +17,19 @@ type GetStoriesOptions = {
   initialData?: any;
 };
 
-export const getStories = ({
+export const getUserInvertedFeedStories = ({
   params,
 }: GetStoriesOptions): Promise<StoryListResponse> => {
   return apiClient.get(`${URI_USER_INVERSE_FEED}`, {
     params,
+    requiresAuth: true,
   });
 };
 
-export const useStories = ({ params }: GetStoriesOptions) => {
+export const useUserInvertedFeedStories = ({ params }: GetStoriesOptions) => {
   const { data, isFetching, isFetched } = useQuery({
     queryKey: [GET_USER_INVERSE_FEED_STORIES, params],
-    queryFn: () => getStories({ params }),
+    queryFn: () => getUserInvertedFeedStories({ params }),
     // enabled: !!params?.category_id,
     initialData: {} as StoryListResponse,
   });
@@ -39,7 +40,7 @@ export const useStories = ({ params }: GetStoriesOptions) => {
   };
 };
 
-export const useInfiniteStories = ({
+export const useInfiniteUserInvertedFeedStories = ({
   params,
   initialData,
 }: GetStoriesOptions) => {
@@ -47,7 +48,7 @@ export const useInfiniteStories = ({
     useInfiniteQuery(
       [GET_USER_INVERSE_FEED_STORIES, 'all'],
       async ({ pageParam = 2 }) => {
-        const response = await getStories({
+        const response = await getUserInvertedFeedStories({
           params: { ...params, page: pageParam },
         });
         return response;
@@ -79,62 +80,5 @@ export type PaginatedStoryListResponse = {
   pages: StoryListResponse[];
   pageParams: number[];
 };
-type UpdateStoryProps = Partial<Story>;
-export const useUpdateStoryInCache = () => {
-  const queryClient = useQueryClient();
 
-  const updateStory = (story_id: number | string, actionType: StoryAction) => {
-    // queryClient.setQueryData<StoryListResponse | undefined>(
-    queryClient.setQueryData<PaginatedStoryListResponse | undefined>(
-      [GET_USER_INVERSE_FEED_STORIES, 'all'],
-      (oldData: PaginatedStoryListResponse | undefined) => {
-        console.log(`oldData`, oldData);
-        if (!oldData) return;
-
-        const newData: PaginatedStoryListResponse = {
-          ...oldData,
-          pages: oldData.pages.map((page: StoryListResponse) => ({
-            ...page,
-            results: page.results.map((story) => {
-              if (story.id === story_id) {
-                let updatedStory = { ...story };
-                switch (actionType) {
-                  case StoryAction.LIKE:
-                    updatedStory = {
-                      ...updatedStory,
-                      likes_count: (updatedStory.likes_count || 0) + 1,
-                      has_liked: true,
-                    };
-                    break;
-                  case StoryAction.UNLIKE:
-                    updatedStory = {
-                      ...updatedStory,
-                      likes_count: Math.max(
-                        (updatedStory.likes_count || 0) - 1,
-                        0,
-                      ),
-                      has_liked: false,
-                    };
-                    break;
-                  case StoryAction.DISLIKE:
-                    // Similar logic for dislike
-                    break;
-                  case StoryAction.UNDISLIKE:
-                    // Similar logic for undislike
-                    break;
-                }
-                return updatedStory;
-              }
-              return story;
-            }),
-          })),
-        };
-
-        return newData;
-      },
-    );
-  };
-
-  return updateStory;
-};
 //Path: src/features/stories/api/get-stories.ts
