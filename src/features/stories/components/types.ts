3b03c190b4485a86fec9c950837f24357856b37f
Muhammed Shittu <devshittu@gmail.com>
2023-12-20 00:01:42 +0000

    Added some MultimediaItemType

--- src/features/stories/components/types.ts
+++ src/features/stories/components/types.ts
@@ -1,17 +1,25 @@
+import { PaginatedListQueryParams, PaginatedResponse } from '@/types';
 import { StoriesQueryParams, Story, StoryListResponse } from '../types';
 
 export type StoryListItemProps = {
   story: Story;
   className?: string;
 };
+
 export type StoryListProps = {
+  queryKey: any[];
+  fetchMoreFunction: (params: GetStoriesOptions) => Promise<StoryListResponse>;
   data: StoryListResponse;
   queryParams: StoriesQueryParams;
 };
+
 export type StorylineStoryListProps = StoryListProps & {
   storylineFor: string;
 };
-
+export type GetStoriesOptions = {
+  params?: StoriesQueryParams;
+  initialData?: any;
+};
 export type LikeStoryFormData = {
   story_slug?: string;
   story_id?: number;
@@ -29,3 +37,25 @@ export enum StoryAction {
   DISLIKE = 'dislike',
   UNDISLIKE = 'undislike',
 }
+
+enum MultimediaItemType {
+  PHOTO = 'photo',
+  VIDEO = 'video',
+  AUDIO = 'audio',
+  GIF = 'gif',
+}
+
+export type MultimediaItem = {
+  file: string | null;
+  media_url: string;
+  caption: string;
+  thumbnail: string | null;
+  media_type: MultimediaItemType;
+  id: number;
+  user: number;
+  story: number;
+};
+
+type Multimedia = MultimediaItem[];
+
+// Path: src/features/stories/components/types.ts
