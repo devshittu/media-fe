61a160eaee2c11108eff24d03519308d1b8249cf
Muhammed Shittu <devshittu@gmail.com>
2023-12-23 22:49:00 +0000

    Updated the getUserFeedStories to async

--- src/features/stories/api/get-user-feed-stories.ts
+++ src/features/stories/api/get-user-feed-stories.ts
@@ -1,24 +1,17 @@
 import {
   useInfiniteQuery,
   useQuery,
-  useQueryClient,
 } from '@tanstack/react-query';
 
 import { apiClient } from '@/lib/api-client';
 
 import {
-  PaginatedStoryListResponse,
-  StoriesQueryParams,
-  Story,
   StoryListResponse,
 } from '../types';
 import { QUERY_KEYS } from '@/config/query';
 import { URI_USER_FEED } from '@/config/api-constants';
-import {
-  GetStoriesOptions,
-  InfiniteStoriesResponse,
-} from '../components';
-import { CacheRefType } from '@/types';
+import { GetStoriesOptions, InfiniteStoriesResponse } from '../components';
+import { ApiCallResultType, CacheRefType } from '@/types';
 const { GET_USER_FEED_STORIES } = QUERY_KEYS;
 
 export const getUserFeedStories = ({
@@ -31,7 +24,10 @@ export const getUserFeedStories = ({
 };
 
 export const useUserFeedStories = ({ params }: GetStoriesOptions) => {
-  const queryKey: CacheRefType = [GET_USER_FEED_STORIES, 'all'];
+  const queryKey: CacheRefType = [
+    GET_USER_FEED_STORIES,
+    ApiCallResultType.DISCRETE,
+  ];
   const { data, isFetching, isFetched } = useQuery({
     queryKey,
     queryFn: () => getUserFeedStories({ params }),
@@ -49,28 +45,42 @@ export const useUserFeedStories = ({ params }: GetStoriesOptions) => {
 export const useInfiniteUserFeedStories = ({
   params,
 }: GetStoriesOptions): InfiniteStoriesResponse => {
-  const queryKey: CacheRefType = [GET_USER_FEED_STORIES, 'infinite'];
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useInfiniteQuery<StoryListResponse>(
-      queryKey,
-      ({ pageParam = 1 }) =>
-        getUserFeedStories({ params: { ...params, page: pageParam } }),
-      {
-        getNextPageParam: (lastPage, allPages) => {
-          // Check if there are more pages to load
-          if (lastPage.current_page < lastPage.total_pages) {
-            return lastPage.current_page + 1;
-          }
-          return undefined; // No more pages
-        },
+  const queryKey: CacheRefType = [
+    GET_USER_FEED_STORIES,
+    ApiCallResultType.INFINITE,
+  ];
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isFetched,
+    isFetching,
+  } = useInfiniteQuery<StoryListResponse>(
+    queryKey,
+    async ({ pageParam = 1 }) =>
+      await getUserFeedStories({ params: { ...params, page: pageParam } }),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        // Check if there are more pages to load
+        if (lastPage.current_page < lastPage.total_pages) {
+          return lastPage.current_page + 1;
+        }
+        return undefined; // No more pages
       },
-    );
+      // Keep data fresh for 5 minutes
+      staleTime: 1000 * 60 * 5,
+      // Keep data in cache for 10 minutes
+      cacheTime: 1000 * 60 * 10,
+    },
+  );
   return {
     queryKey,
     data,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
+    isLoading: isFetching && !isFetched,
   };
 };
 
