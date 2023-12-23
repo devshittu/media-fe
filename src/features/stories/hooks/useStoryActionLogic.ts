3ccfa056cba46952bfa06b95d2b77ad1e651e4bc
Muhammed Shittu <devshittu@gmail.com>
2023-12-23 22:14:33 +0000

    Rewritten the useStoryActionLogic

--- src/features/stories/hooks/useStoryActionLogic.ts
+++ src/features/stories/hooks/useStoryActionLogic.ts
@@ -1,28 +1,62 @@
-// hooks/useStoryActionLogic.ts
-import { LikeStoryFormData } from '../components/types';
+import { CacheRefType } from '@/types';
+import { StoryAction } from '../components/types';
 import { useStoryNotification } from './useStoryNotification';
 
-export const useStoryActionLogic = (
-  storyId: number,
-  storySlug: string,
-  action: string,
-  apiFunction: any,
-) => {
-  const { onSuccess, onError } = useStoryNotification(storySlug, action);
+// Base payload type
+export type BasePayload = {
+  story_id: number;
+};
 
-  const { submit: storyActionSubmit, isLoading } = apiFunction({
-    story_id: storyId as unknown as string,
-    onSuccess,
-    onError,
-  });
+// Specific payload types
+export type LikeStoryPayload = BasePayload;
+export type AddBookmarkPayload = BasePayload & {
+  bookmark_category: string;
+  note: string;
+};
 
-  const handleStoryAction = () => {
-    const data: LikeStoryFormData = { story_slug: storySlug };
-    storyActionSubmit(data);
+// Union of all payload types
+export type PayloadType = LikeStoryPayload | AddBookmarkPayload;
+
+export type StoryActionLogicParameterType = {
+  basePayload: BasePayload;
+  action: StoryAction;
+  apiFunction: any//(args: any) => { submit: (data: PayloadType) => void; isLoading: boolean };
+  cacheRefQueryKey: CacheRefType;
+};
+
+export const useStoryActionLogic = ({
+  basePayload,
+  action,
+  apiFunction,
+  cacheRefQueryKey,
+}: StoryActionLogicParameterType) => {
+  const { onSuccess, onError } = useStoryNotification(basePayload.story_id, action);
+  const { submit, isLoading } = apiFunction({ story_id:basePayload.story_id, onSuccess, onError, cacheRefQueryKey });
+
+  // For actions that only require the base payload (e.g., story_id)
+  const handleSimpleAction = () => {
+    if (!basePayload.story_id) {
+      console.error('Story ID is required in the payload.');
+      return;
+    }
+    submit(basePayload);
+  };
+
+  // For actions that require additional data
+  const handleActionWithAdditionalData = (additionalData: Partial<PayloadType>) => {
+    if (!basePayload.story_id) {
+      console.error('Story ID is required in the payload.');
+      return;
+    }
+    const fullPayload = { ...basePayload, ...additionalData };
+    submit(fullPayload);
   };
 
   return {
-    handleStoryAction,
+    handleSimpleAction,
+    handleActionWithAdditionalData,
     isLoading,
   };
 };
+
+// Path: src/features/stories/hooks/useStoryActionLogic.ts
