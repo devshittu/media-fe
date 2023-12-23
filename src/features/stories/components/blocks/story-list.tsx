fe8410a085bc3f53888bbf28cbf9934efd2087d9
Muhammed Shittu <devshittu@gmail.com>
2023-12-23 23:03:25 +0000

    Reimplemented StoryList to use the useStoriesHook

--- src/features/stories/components/blocks/story-list.tsx
+++ src/features/stories/components/blocks/story-list.tsx
@@ -1,66 +1,69 @@
 import React, { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
-import { Story, StoryListProps, StoryListResponse } from '@/features/stories';
+import {
+  Story,
+  StoryListProps,
+  StoryListResponse,
+  InfiniteStoriesResponse,
+} from '@/features/stories';
 import { StoryListLoadingPlaceholder } from '@/features/stories/components/loading';
 import { StoryListItem } from './story-list-item';
-import { useUserFeedsStore } from '@/stores/feeds/user-feeds';
-import { useInfiniteStories } from '@/features/stories/api/get-stories';
-import { useCategoryContext } from '@/features/categories/hooks';
 import { InteractiveLoader } from '@/components/loading/';
 
 export const StoryList = ({
-  data = {} as StoryListResponse,
+  useStoriesHook,
   queryParams,
+  isFinite,
+  loadMoreOnScroll = false, // Default value is true
 }: StoryListProps) => {
-  const { ref, inView } = useInView();
-  const { scrollPosition, setScrollPosition } = useUserFeedsStore();
-  // const { categoryLookupTable, categories } = useCategoryContext();
-  const {
-    data: dataFromStories,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-  } = useInfiniteStories({ initialData: data, params: queryParams });
+  const hookResponse = useStoriesHook({ params: queryParams });
+
+  const isResponseInfinite = 'fetchNextPage' in hookResponse;
+  const dataFromStories = isResponseInfinite
+    ? hookResponse.data
+    : { pages: [hookResponse] };
+
+  const { ref, inView } = useInView({
+    skip: isFinite || !loadMoreOnScroll, // Skip inView functionality based on props
+  });
 
   useEffect(() => {
-    if (inView) {
-      fetchNextPage();
+    if (
+      inView &&
+      isResponseInfinite &&
+      hookResponse.hasNextPage &&
+      loadMoreOnScroll
+    ) {
+      hookResponse.fetchNextPage();
     }
-  }, [inView]);
+  }, [inView, hookResponse, loadMoreOnScroll, isResponseInfinite]);
 
-  useEffect(() => {
-    window.scrollTo(0, scrollPosition);
-  }, []);
-
-  useEffect(() => {
-    return () => {
-      setScrollPosition(window.scrollY);
-    };
-  }, []);
+  const renderStories = (page: StoryListResponse) =>
+    page.results.map((story: Story) => (
+      <StoryListItem
+        key={story.id}
+        story={story}
+        cacheRefQueryKey={hookResponse.queryKey}
+      />
+    ));
 
   return (
     <div>
+      {/* {`hookResponse.isLoading: ${hookResponse.isLoading} hookResponse.isFetchingNextPage: ${hookResponse.isFetchingNextPage}`} */}
+      {hookResponse.isLoading && <StoryListLoadingPlaceholder />}
       {dataFromStories?.pages.map((page, i) => (
-        <React.Fragment key={i}>
-          {page?.results?.map((story: Story) => (
-            <StoryListItem
-              key={story.id}
-              story={story}
-              // categories={categoryLookupTable}
-            />
-          ))}
-        </React.Fragment>
+        <React.Fragment key={i}>{renderStories(page)}</React.Fragment>
       ))}
 
-      <div>
+      {!hookResponse.isLoading && !isFinite && (
         <InteractiveLoader
           ref={ref}
-          isLoading={isFetchingNextPage}
-          hasNextPage={hasNextPage || false}
-          onClick={fetchNextPage}
+          isLoading={hookResponse.isFetchingNextPage}
+          hasNextPage={hookResponse.hasNextPage || false}
+          onClick={hookResponse.fetchNextPage}
           loadingPlaceholder={<StoryListLoadingPlaceholder />}
         />
-      </div>
+      )}
     </div>
   );
 };
