cb8163f284b441f4569b4d72e8bb18f2d289403f
Muhammed Shittu <devshittu@gmail.com>
2023-12-24 23:55:03 +0000

    Loading bookmark list

--- src/features/bookmarks/components/blocks/bookmark-list.tsx
+++ src/features/bookmarks/components/blocks/bookmark-list.tsx
@@ -5,56 +5,98 @@ import { useInfiniteBookmarks } from '../../api/get-bookmarks';
 import { useInView } from 'react-intersection-observer';
 import { useUserFeedsStore } from '@/stores/feeds';
 import { InteractiveLoader } from '@/components/loading';
+import { StoryListLoadingPlaceholder } from '@/features/stories';
 
 export const BookmarkList = ({
-  data = {} as BookmarkListResponse,
+  useBookmarksHook,
   queryParams,
+  isFinite,
+  loadMoreOnScroll = false,
 }: BookmarkListProps) => {
-  const { ref, inView } = useInView();
-  const { scrollPosition, setScrollPosition } = useUserFeedsStore();
 
-  const {
-    data: dataFromBookmarks,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-  } = useInfiniteBookmarks({ initialData: data, params: queryParams });
+  // const { ref, inView } = useInView();
+  // const { scrollPosition, setScrollPosition } = useUserFeedsStore();
+
+  // const {
+  //   data: dataFromBookmarks,
+  //   fetchNextPage,
+  //   hasNextPage,
+  //   isFetchingNextPage,
+  // } = useInfiniteBookmarks({ initialData: data, params: queryParams });
+  // useEffect(() => {
+  //   if (inView) {
+  //     fetchNextPage();
+  //   }
+  // }, [inView]);
+
+  // useEffect(() => {
+  //   window.scrollTo(0, scrollPosition);
+  // }, []);
+
+  // useEffect(() => {
+  //   return () => {
+  //     setScrollPosition(window.scrollY);
+  //   };
+  // }, []);
+
+  const hookResponse = useBookmarksHook({ params: queryParams });
+
+  const isResponseInfinite = 'fetchNextPage' in hookResponse;
+  const dataFromBookmarks = isResponseInfinite
+    ? hookResponse.data
+    : { pages: [hookResponse] };
+
+    console.log(`dataFromBookmarks:// `, dataFromBookmarks)
+  const { ref, inView } = useInView({
+    skip: isFinite || !loadMoreOnScroll, // Skip inView functionality based on props
+  });
+
   useEffect(() => {
-    if (inView) {
-      fetchNextPage();
+    if (
+      inView &&
+      isResponseInfinite &&
+      hookResponse.hasNextPage &&
+      loadMoreOnScroll
+    ) {
+      hookResponse.fetchNextPage();
     }
-  }, [inView]);
+  }, [inView, hookResponse, loadMoreOnScroll, isResponseInfinite]);
 
-  useEffect(() => {
-    window.scrollTo(0, scrollPosition);
-  }, []);
-
-  useEffect(() => {
-    return () => {
-      setScrollPosition(window.scrollY);
-    };
-  }, []);
+  const renderStories = (page: BookmarkListResponse) =>
+    page.results.map((bookmark: Bookmark) => (
+      <BookmarkItem
+         key={bookmark.id} bookmark={bookmark}
+        cacheRefQueryKey={hookResponse.queryKey}
+      />
+    ));
 
   return (
     <>
       <div className="mt-3 divide-y divider-slate-200 dark:divide-slate-700">
-        {dataFromBookmarks?.pages.map((page, i) => (
+        {/* {dataFromBookmarks?.pages.map((page, i) => (
           <React.Fragment key={i}>
             {page?.results?.map((bookmark: Bookmark) => (
               <BookmarkItem key={bookmark.id} bookmark={bookmark} />
             ))}
           </React.Fragment>
-        ))}
+        ))} */}
+
+      {hookResponse.isLoading && <StoryListLoadingPlaceholder />}
+      {dataFromBookmarks?.pages.map((page, i) => (
+        <React.Fragment key={i}>{renderStories(page)}</React.Fragment>
+      ))}
       </div>
 
       <div>
+      {!hookResponse.isLoading && !isFinite && (
         <InteractiveLoader
           ref={ref}
-          isLoading={isFetchingNextPage}
-          hasNextPage={hasNextPage || false}
-          onClick={fetchNextPage}
+          isLoading={hookResponse.isFetchingNextPage}
+          hasNextPage={hookResponse.hasNextPage || false}
+          onClick={hookResponse.fetchNextPage}
           // loadingPlaceholder={<StoryListLoadingPlaceholder />}
         />
+      )}
       </div>
     </>
   );
