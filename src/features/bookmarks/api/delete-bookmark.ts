41d84b3e14d65c1dccb22a3141f34b424718f3fb
Muhammed Shittu <devshittu@gmail.com>
2023-12-27 22:34:00 +0000

    Switched to updateCachedBookmark

--- src/features/bookmarks/api/delete-bookmark.ts
+++ src/features/bookmarks/api/delete-bookmark.ts
@@ -2,48 +2,48 @@ import { useMutation } from '@tanstack/react-query';
 import { apiClient } from '@/lib/api-client';
 import { QUERY_KEYS } from '@/config/query';
 import { ApiResponse, CacheRefType } from '@/types';
-import { URI_BOOKMARKS_BY_STORY_ID } from '@/config/api-constants';
+import { URI_BOOKMARKS_BY_BOOKMARK_ID } from '@/config/api-constants';
 import { uriTemplate } from '@/utils';
 import { StoryAction } from '@/features/stories/components/types';
 import { useUpdateCachedStory } from '@/features/stories/hooks/useUpdateCachedStory';
 import { DeleteBookmarkFormData } from '../types';
-const { DELETE_BOOKMARK } = QUERY_KEYS;
+import { useUpdateCachedBookmark } from '../hooks/useUpdateCachedBookmark';
+import { BookmarkAction } from '../components/types';
+const { DESTROY_BOOKMARK } = QUERY_KEYS;
 
 export const deleteBookmark = (
   data: DeleteBookmarkFormData,
 ): Promise<ApiResponse> => {
-  const uri = uriTemplate(URI_BOOKMARKS_BY_STORY_ID, {
-    story_id: data.story_id.toString(),
+  const uri = uriTemplate(URI_BOOKMARKS_BY_BOOKMARK_ID, {
+    bookmark_id: data.bookmark_id.toString(),
   });
 
   return apiClient.delete(`${uri}`, { requiresAuth: true });
 };
 type UseDeleteBookmarkOptions = {
-  // data: {
-  //   story_id: string | undefined;
-  // };
   onSuccess?: (response: any) => void;
   onError?: (message: any) => void;
-  story_id: string;
+  bookmark_id: string;
   cacheRefQueryKey: CacheRefType;
 };
 
 export const useDeleteBookmark = ({
-  story_id,
+  bookmark_id,
   onSuccess,
   onError,
   cacheRefQueryKey,
 }: UseDeleteBookmarkOptions) => {
-  const updateCachedStory = useUpdateCachedStory();
-  const mutationKey = [DELETE_BOOKMARK, story_id];
+  // const updateCachedStory = useUpdateCachedStory();
+  const updateCachedBookmark = useUpdateCachedBookmark();
+  const mutationKey = [DESTROY_BOOKMARK, bookmark_id];
   const { mutate: submit, isLoading } = useMutation({
     mutationKey: mutationKey,
     mutationFn: deleteBookmark,
     onSuccess: (response) => {
-      updateCachedStory(
+      updateCachedBookmark(
         cacheRefQueryKey,
-        story_id,
-        StoryAction.DELETE_BOOKMARK,
+        bookmark_id,
+        BookmarkAction.DELETE_BOOKMARK,
       );
       onSuccess?.(response);
     },
