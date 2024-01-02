c84e4817234c105a9cae945bce0df7d8367110fd
Muhammed Shittu <devshittu@gmail.com>
2024-01-02 14:11:30 +0000

    Added Logging analytics

--- src/features/bookmarks/api/post-add-bookmark.ts
+++ src/features/bookmarks/api/post-add-bookmark.ts
@@ -6,6 +6,8 @@ import { URI_BOOKMARKS } from '@/config/api-constants';
 import { AddBookmarkFormData } from '../types';
 import { StoryAction } from '@/features/stories/components/types';
 import { useUpdateCachedStory } from '@/features/stories/hooks/useUpdateCachedStory';
+import { useLogAnalytics } from '@/features/analytics/hooks/useLogAnalytics';
+import { InteractionType } from '@/features/analytics/types';
 const { ADD_BOOKMARK } = QUERY_KEYS;
 
 export const addBookmark = (
@@ -26,6 +28,7 @@ export const useAddBookmark = ({
   onError,
   cacheRefQueryKey,
 }: UseAddBookmarkOptions) => {
+  const { logAnalytics } = useLogAnalytics();
   const updateCachedStory = useUpdateCachedStory();
   const mutationKey = [ADD_BOOKMARK, story_id];
   const { mutate: submit, isLoading } = useMutation({
@@ -36,6 +39,20 @@ export const useAddBookmark = ({
       //   queryClient.invalidateQueries('someQueryKey');
 
       updateCachedStory(cacheRefQueryKey, story_id, StoryAction.ADD_BOOKMARK);
+
+      const analyticsData = {
+        analytics_store_id: '', // This will be generated in the store
+        event: InteractionType.ADD_BOOKMARK,
+        story: story_id,
+        interaction_type: InteractionType.ADD_BOOKMARK,
+        timestamp: Date.now(),
+        metadata: {
+          story_id,
+          bookmark_id: response?.data?.id, // Example bookmark ID
+        },
+      };
+      // Log add bookmark
+      logAnalytics(analyticsData);
 
       onSuccess?.(response);
     },
