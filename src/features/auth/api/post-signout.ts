9d75be8901d24c77f7b8a6c9429948eacaef04e4
Muhammed Shittu <devshittu@gmail.com>
2024-01-02 14:19:00 +0000

    Refactored useSignout

--- src/features/auth/api/post-signout.ts
+++ src/features/auth/api/post-signout.ts
@@ -1,10 +1,5 @@
-import { requireAuth } from '../../../testing/mocks/utils';
 import { useMutation } from '@tanstack/react-query';
-import { removeItem } from '@/utils/localStorage';
-import { apiClient } from '@/lib/api-client';
 import { queryClient } from '@/lib/react-query';
-import { URI_AUTH_LOGOUT } from '@/config/api-constants';
-import { AuthStore } from '@/stores/auth';
 import { signOut } from '@/utils';
 
 type UseSignoutOptions = {
