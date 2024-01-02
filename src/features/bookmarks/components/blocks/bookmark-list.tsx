f5b166bb8a7af7ec0356e02c17cff4f48ea3b68c
Muhammed Shittu <devshittu@gmail.com>
2024-01-02 14:40:48 +0000

    Removed unused codes.

--- src/features/bookmarks/components/blocks/bookmark-list.tsx
+++ src/features/bookmarks/components/blocks/bookmark-list.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
 import { Bookmark, BookmarkListProps, BookmarkListResponse } from '../../types';
 import { BookmarkItem } from './bookmark-item';
-import { useInfiniteBookmarks } from '../../api/get-bookmarks';
 import { useInView } from 'react-intersection-observer';
-import { useUserFeedsStore } from '@/stores/feeds';
 import { InteractiveLoader } from '@/components/loading';
-import { StoryListLoadingPlaceholder } from '@/features/stories';
+import { BookmarkMomentLoadingPlaceholder } from '../loading';
 
 export const BookmarkList = ({
   useBookmarksHook,
@@ -13,32 +11,6 @@ export const BookmarkList = ({
   isFinite,
   loadMoreOnScroll = false,
 }: BookmarkListProps) => {
-
-  // const { ref, inView } = useInView();
-  // const { scrollPosition, setScrollPosition } = useUserFeedsStore();
-
-  // const {
-  //   data: dataFromBookmarks,
-  //   fetchNextPage,
-  //   hasNextPage,
-  //   isFetchingNextPage,
-  // } = useInfiniteBookmarks({ initialData: data, params: queryParams });
-  // useEffect(() => {
-  //   if (inView) {
-  //     fetchNextPage();
-  //   }
-  // }, [inView]);
-
-  // useEffect(() => {
-  //   window.scrollTo(0, scrollPosition);
-  // }, []);
-
-  // useEffect(() => {
-  //   return () => {
-  //     setScrollPosition(window.scrollY);
-  //   };
-  // }, []);
-
   const hookResponse = useBookmarksHook({ params: queryParams });
 
   const isResponseInfinite = 'fetchNextPage' in hookResponse;
@@ -46,7 +18,6 @@ export const BookmarkList = ({
     ? hookResponse.data
     : { pages: [hookResponse] };
 
-    console.log(`dataFromBookmarks:// `, dataFromBookmarks)
   const { ref, inView } = useInView({
     skip: isFinite || !loadMoreOnScroll, // Skip inView functionality based on props
   });
@@ -65,7 +36,8 @@ export const BookmarkList = ({
   const renderStories = (page: BookmarkListResponse) =>
     page.results.map((bookmark: Bookmark) => (
       <BookmarkItem
-         key={bookmark.id} bookmark={bookmark}
+        key={bookmark.id}
+        bookmark={bookmark}
         cacheRefQueryKey={hookResponse.queryKey}
       />
     ));
@@ -73,30 +45,22 @@ export const BookmarkList = ({
   return (
     <>
       <div className="mt-3 divide-y divider-slate-200 dark:divide-slate-700">
-        {/* {dataFromBookmarks?.pages.map((page, i) => (
-          <React.Fragment key={i}>
-            {page?.results?.map((bookmark: Bookmark) => (
-              <BookmarkItem key={bookmark.id} bookmark={bookmark} />
-            ))}
-          </React.Fragment>
-        ))} */}
-
-      {hookResponse.isLoading && <StoryListLoadingPlaceholder />}
-      {dataFromBookmarks?.pages.map((page, i) => (
-        <React.Fragment key={i}>{renderStories(page)}</React.Fragment>
-      ))}
+        {hookResponse.isLoading && <BookmarkMomentLoadingPlaceholder />}
+        {dataFromBookmarks?.pages.map((page, i) => (
+          <React.Fragment key={i}>{renderStories(page)}</React.Fragment>
+        ))}
       </div>
 
       <div>
-      {!hookResponse.isLoading && !isFinite && (
-        <InteractiveLoader
-          ref={ref}
-          isLoading={hookResponse.isFetchingNextPage}
-          hasNextPage={hookResponse.hasNextPage || false}
-          onClick={hookResponse.fetchNextPage}
-          // loadingPlaceholder={<StoryListLoadingPlaceholder />}
-        />
-      )}
+        {!hookResponse.isLoading && !isFinite && (
+          <InteractiveLoader
+            ref={ref}
+            isLoading={hookResponse.isFetchingNextPage}
+            hasNextPage={hookResponse.hasNextPage || false}
+            onClick={hookResponse.fetchNextPage}
+            loadingPlaceholder={<BookmarkMomentLoadingPlaceholder />}
+          />
+        )}
       </div>
     </>
   );
