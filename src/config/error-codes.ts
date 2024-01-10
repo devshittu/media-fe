2deda18186c2dc086cf7cbcdccd47ee3c0b6ef5e
Muhammed Shittu <devshittu@gmail.com>
2024-01-10 23:48:18 +0000

    Added ErrorCodes

---
+++ src/config/error-codes.ts
@@ -0,0 +1,24 @@
+// errorCodes.ts
+
+export enum ErrorCode {
+  AuthenticationFailed = 'authentication_failed',
+  TokenBlacklisted = 'token_blacklisted',
+  TokenNotProvided = 'token_not_provided',
+  AuthCredentialNotProvided = 'auth_credential_not_provided',
+  InvalidRefreshToken = 'invalid_refresh_token',
+  EmailNotFound = 'email_not_found',
+  InvalidOrExpiredToken = 'invalid_or_expired_token',
+  InvalidOtp = 'invalid_otp',
+  InvalidEmail = 'invalid_email',
+  OtpAlreadySent = 'otp_already_sent',
+  InvalidVerificationLink = 'invalid_verification_link',
+  PasswordUpdateFailed = 'password_update_failed',
+  UserUpdateFailed = 'user_update_failed',
+  UserRegistrationFailed = 'user_registration_failed',
+  LogoutFailed = 'logout_failed',
+  InvalidOrExpiredResetToken = 'invalid_or_expired_reset_token',
+  RecentOtpStillValid = 'recent_otp_still_valid',
+  InvalidAccessToken = 'invalid_access_token',
+  BlacklistedRefreshToken = 'blacklisted_refresh_token',
+  // Add more as needed
+}
