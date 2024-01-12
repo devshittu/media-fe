26ee5f42313a124b3f7557a2818400feac7a51f8
Muhammed Shittu <devshittu@gmail.com>
2024-01-12 16:43:27 +0000

    Added the sitemap

---
+++ src/lib/sitemap.ts
@@ -0,0 +1,28 @@
+import { APP_URL } from '@/config/constants';
+import { getStories } from '@/features/stories';
+
+// lib/sitemap.ts
+export async function generateDynamicSitemap() {
+  // Fetch your article data. This could be from a database, API, etc.
+  //   const articles = await fetchAllArticles(); // Replace with actual data fetching logic
+  const articles = await getStories({ params: { page_size: 100 } }); // Replace with actual data fetching logic
+  console.log('sitemap://', articles);
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${articles.results
+        .map(({ id, slug, event_reported_at, storyline_id, created_at, title }) => {
+          return `
+            <url>
+              <title>${title}</title>
+              <loc>${APP_URL}/stories/${slug}</loc>
+              <sline>${APP_URL}/storylines/${storyline_id}</sline>
+              <lastmod>${new Date(created_at).toISOString()}</lastmod>
+              <priority>0.7</priority>
+            </url>
+          `;
+        })
+        .join('')}
+    </urlset>
+  `;
+  return sitemap;
+}
