efad8b8145217a2f98dbe95bb724b462df99b5bd
Muhammed Shittu <devshittu@gmail.com>
2024-01-14 06:12:58 +0000

    Updated the options for the useUIStore

--- src/stores/ui/hooks/uiStoreFactory.ts
+++ src/stores/ui/hooks/uiStoreFactory.ts
@@ -1,4 +1,7 @@
-import { createStore, useStore } from 'zustand';
+// src/stores/ui/hooks/uiStoreFactory.ts
+import { createStore, StateCreator, StoreApi, useStore } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
 import React from 'react';
 
 export type UIContentProps = {
@@ -13,26 +16,64 @@ export type UIStore = {
   show: (content: UIContentComponent) => void;
   close: () => void;
 };
-type UIStoreType = { initialIsOpen?: boolean; closeDelay?: number };
-export const createUIStore = ({
-  initialIsOpen = false,
-  closeDelay = 0,
-}: UIStoreType) => {
-  return createStore<UIStore>((set) => ({
+// type UIStoreType = { initialIsOpen?: boolean; closeDelay?: number };
+type StorageType = 'local' | 'session';
+
+interface UIStoreState {
+  content: UIContentComponent | null;
+  isOpen: boolean;
+  show: (content: UIContentComponent) => void;
+  close: () => void;
+}
+
+interface UIStoreOptions {
+  initialIsOpen?: boolean;
+  closeDelay?: number;
+  persist?: boolean;
+  storageType?: StorageType;
+  name?: string;
+}
+
+export const createUIStore = (
+  options?: Partial<UIStoreOptions>,
+): StoreApi<UIStoreState> => {
+  const {
+    initialIsOpen = false,
+    closeDelay = 0,
+    persist: shouldPersist,
+    storageType = 'local',
+    name = 'ui-store',
+  } = options || {};
+
+  if (shouldPersist && !name) {
+    throw new Error("A unique 'name' is required for persistent stores");
+  }
+
+  const storeCreator: StateCreator<UIStoreState> = (set) => ({
     content: null,
     isOpen: initialIsOpen,
-    show: (content) => {
-      set({ content, isOpen: true });
-    },
+    show: (content) => set({ content, isOpen: true }),
     close: () => {
-      setTimeout(() => {
-        set({ content: null, isOpen: false });
-      }, closeDelay);
+      setTimeout(() => set({ content: null, isOpen: false }), closeDelay);
     },
-  }));
+  });
+
+  if (shouldPersist) {
+    return createStore(
+      persist(storeCreator, {
+        name: name, // Name is mandatory for persistent stores
+        storage: createJSONStorage(() =>
+          storageType === 'local' ? localStorage : sessionStorage,
+        ),
+      }),
+    );
+  }
+
+  return createStore(storeCreator);
 };
 
-export const useUIStore = (store: ReturnType<typeof createUIStore>) =>
-  useStore(store);
+export const useUIStore = <T>(store: StoreApi<T>) => {
+  return useStore(store);
+};
 
 // Path: media-fe/src/stores/ui/hooks/uiStoreFactory.ts
