f61db700003eeb81a0a897bf7f1e897d4d30c3e6
Muhammed Shittu <devshittu@gmail.com>
2024-01-14 21:18:07 +0000

    Get storylines moved to latest requirements

--- src/features/stories/api/get-storylines.ts
+++ src/features/stories/api/get-storylines.ts
@@ -5,6 +5,8 @@ import { apiClient } from '@/lib/api-client';
 import { StoriesQueryParams, StoryListResponse } from '../types';
 import { QUERY_KEYS } from '@/config/query';
 import { URI_STORYLINES } from '@/config/api-constants';
+import { ApiCallResultType, CacheRefType } from '@/types';
+import { InfiniteStoriesResponse } from '../components';
 const { GET_STORYLINES } = QUERY_KEYS;
 
 type GetStorylineOptions = {
@@ -23,14 +25,20 @@ export const getStorylines = ({
 };
 
 export const useStorylines = ({ params }: GetStorylineOptions) => {
+  const queryKey: CacheRefType = [
+    GET_STORYLINES,
+    ApiCallResultType.DISCRETE,
+    params
+  ];
   const { data, isFetching, isFetched } = useQuery({
-    queryKey: [GET_STORYLINES, params],
+    queryKey,
     queryFn: () => getStorylines({ params }),
     // enabled: !!params?.category_id,
     initialData: {} as StoryListResponse,
   });
 
   return {
+    queryKey,
     data,
     isLoading: isFetching && !isFetched,
   };
@@ -40,11 +48,17 @@ export const useInfiniteStorylines = ({
   // storyFor,
   params,
   initialData,
-}: GetStorylineOptions) => {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+}: GetStorylineOptions) : InfiniteStoriesResponse => {
+  const queryKey: CacheRefType = [
+    GET_STORYLINES,
+    ApiCallResultType.INFINITE,
+  ];
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage,
+    isFetched,
+    isFetching, } =
     useInfiniteQuery(
-      ['storylines', params],
-      async ({ pageParam = 2 }) => {
+      queryKey,
+      async ({ pageParam = 1 }) => {
         const response = await getStorylines({
           // storyFor,
           params: { ...params, page: pageParam },
@@ -58,19 +72,24 @@ export const useInfiniteStorylines = ({
             : undefined;
         },
 
-        initialData: { pages: [initialData], pageParams: [1] },
+        // initialData: { pages: [initialData], pageParams: [1] },
         //TODO: Keep data fresh for 5 minutes
         staleTime: 1000 * 60 * 5,
         // Keep data in cache for 10 minutes
         cacheTime: 1000 * 60 * 10,
       },
     );
+  // Extract count from the first page
+  const count = data?.pages[0]?.count;
 
   return {
+    queryKey,
     data,
+    count: count || 0,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
+    isLoading: isFetching && !isFetched,
   };
 };
 //Path: src/features/stories/api/get-storylines.ts
