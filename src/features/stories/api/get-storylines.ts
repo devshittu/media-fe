ba7d64277a53dfa438419bd472e5ba7caccbeed9
Muhammed Shittu <devshittu@gmail.com>
2024-01-16 21:42:22 +0000

    Formatted

--- src/features/stories/api/get-storylines.ts
+++ src/features/stories/api/get-storylines.ts
@@ -28,7 +28,7 @@ export const useStorylines = ({ params }: GetStorylineOptions) => {
   const queryKey: CacheRefType = [
     GET_STORYLINES,
     ApiCallResultType.DISCRETE,
-    params
+    params,
   ];
   const { data, isFetching, isFetched } = useQuery({
     queryKey,
@@ -48,37 +48,38 @@ export const useInfiniteStorylines = ({
   // storyFor,
   params,
   initialData,
-}: GetStorylineOptions) : InfiniteStoriesResponse => {
-  const queryKey: CacheRefType = [
-    GET_STORYLINES,
-    ApiCallResultType.INFINITE,
-  ];
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage,
+}: GetStorylineOptions): InfiniteStoriesResponse => {
+  const queryKey: CacheRefType = [GET_STORYLINES, ApiCallResultType.INFINITE];
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
     isFetched,
-    isFetching, } =
-    useInfiniteQuery(
-      queryKey,
-      async ({ pageParam = 1 }) => {
-        const response = await getStorylines({
-          // storyFor,
-          params: { ...params, page: pageParam },
-        });
-        return response;
+    isFetching,
+  } = useInfiniteQuery(
+    queryKey,
+    async ({ pageParam = 1 }) => {
+      const response = await getStorylines({
+        // storyFor,
+        params: { ...params, page: pageParam },
+      });
+      return response;
+    },
+    {
+      getNextPageParam: (lastPage: StoryListResponse) => {
+        return lastPage.current_page < lastPage.total_pages
+          ? lastPage.current_page + 1
+          : undefined;
       },
-      {
-        getNextPageParam: (lastPage: StoryListResponse) => {
-          return lastPage.current_page < lastPage.total_pages
-            ? lastPage.current_page + 1
-            : undefined;
-        },
 
-        // initialData: { pages: [initialData], pageParams: [1] },
-        //TODO: Keep data fresh for 5 minutes
-        staleTime: 1000 * 60 * 5,
-        // Keep data in cache for 10 minutes
-        cacheTime: 1000 * 60 * 10,
-      },
-    );
+      // initialData: { pages: [initialData], pageParams: [1] },
+      //TODO: Keep data fresh for 5 minutes
+      staleTime: 1000 * 60 * 5,
+      // Keep data in cache for 10 minutes
+      cacheTime: 1000 * 60 * 10,
+    },
+  );
   // Extract count from the first page
   const count = data?.pages[0]?.count;
 
