43e51da3c9f1a1ba5ca7d75767e6d8beff47ccb2
Muhammed Shittu <devshittu@gmail.com>
2024-01-17 21:57:17 +0000

    Added mandatory state to the popup

--- src/stores/ui/popup.ts
+++ src/stores/ui/popup.ts
@@ -1,7 +1,7 @@
 import { createUIStore, useUIStore } from './hooks/uiStoreFactory';
 
 const popupStore = createUIStore({});
-export type PopupProps = { onClose: () => void };
+export type PopupProps = { onClose: () => void, isOpen: boolean};
 export const usePopup = () => useUIStore(popupStore);
 
 //path: src/stores/popup/popup.ts
