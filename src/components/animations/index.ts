5b1a381de80381b7865bf9fa9aa9d672b82dcb58
Muhammed Shittu <devshittu@gmail.com>
2024-01-17 21:47:47 +0000

    Added index for export

---
+++ src/components/animations/index.ts
@@ -0,0 +1,4 @@
+export * from './custom-motion-component';
+export * from './animation-presets';
+export * from './hooks/useCustomAnimation';
+export { default as withMotion } from './hoc/withMotion';
