60bd932e01c7bbbf70d892648f286371755b4d77
Muhammed Shittu <devshittu@gmail.com>
2024-01-26 22:44:59 +0000

    Solved an error

--- src/features/auth/components/protected/protected.tsx
+++ src/features/auth/components/protected/protected.tsx
@@ -1,12 +1,54 @@
-import { ReactNode } from 'react';
+// import { ReactNode } from 'react';
+// import { useRouter } from 'next/router';
+// import { useAuthStore } from '@/stores/auth';
+// import { Loading } from '@/components/loading';
+// import {
+//   NotificationType,
+//   notificationsStore,
+//   useNotifications,
+// } from '@/stores/notifications';
+
+// export type ProtectedProps = {
+//   children: ReactNode;
+// };
+
+// export const Protected = ({ children }: ProtectedProps) => {
+//   const router = useRouter();
+//   // const { showNotification } = useNotifications();
+//   const { authUserDetails, accessToken, isRefreshingToken } = useAuthStore();
+//   // Redirect to login if no access token and not refreshing token
+//   if (!accessToken && !isRefreshingToken) {
+//     router.replace('/auth/signin?redirect=' + router.asPath);
+
+//     notificationsStore.getState().showNotification({
+//       type: NotificationType.WARNING,
+//       title: 'Error',
+//       duration: 5000,
+//       message: 'You must login to continue.',
+//     });
+//     return null;
+//   }
+
+//   // Show loading indicator while token is being refreshed or user details are being fetched
+//   if (isRefreshingToken || !authUserDetails) {
+//     return (
+//       <div className="flex flex-col justify-center h-full">
+//         <Loading />
+//       </div>
+//     );
+//   }
+
+//   // Render children if user is authenticated
+//   return <>{children}</>;
+// };
+
+// // Path: src/features/auth/components/protected/protected.tsx
+
+import { ReactNode, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAuthStore } from '@/stores/auth';
 import { Loading } from '@/components/loading';
-import {
-  NotificationType,
-  notificationsStore,
-  useNotifications,
-} from '@/stores/notifications';
+import { NotificationType, notificationsStore } from '@/stores/notifications';
 
 export type ProtectedProps = {
   children: ReactNode;
@@ -14,22 +56,25 @@ export type ProtectedProps = {
 
 export const Protected = ({ children }: ProtectedProps) => {
   const router = useRouter();
-  // const { showNotification } = useNotifications();
   const { authUserDetails, accessToken, isRefreshingToken } = useAuthStore();
-  // Redirect to login if no access token and not refreshing token
-  if (!accessToken && !isRefreshingToken) {
-    router.replace('/auth/signin?redirect=' + router.asPath);
 
-    notificationsStore.getState().showNotification({
-      type: NotificationType.WARNING,
-      title: 'Error',
-      duration: 5000,
-      message: 'You must login to continue.',
-    });
+  useEffect(() => {
+    if (!accessToken && !isRefreshingToken) {
+      router.replace('/auth/signin?redirect=' + router.asPath);
+
+      notificationsStore.getState().showNotification({
+        type: NotificationType.WARNING,
+        title: 'Error',
+        duration: 5000,
+        message: 'You must login to continue.',
+      });
+    }
+  }, [accessToken, isRefreshingToken, router]);
+
+  if (!accessToken && !isRefreshingToken) {
     return null;
   }
 
-  // Show loading indicator while token is being refreshed or user details are being fetched
   if (isRefreshingToken || !authUserDetails) {
     return (
       <div className="flex flex-col justify-center h-full">
@@ -38,8 +83,6 @@ export const Protected = ({ children }: ProtectedProps) => {
     );
   }
 
-  // Render children if user is authenticated
   return <>{children}</>;
 };
-
 // Path: src/features/auth/components/protected/protected.tsx
