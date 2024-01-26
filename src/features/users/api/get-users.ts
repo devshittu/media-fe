6fd128479ab62425bd297933b6e50f737b6a7547
Muhammed Shittu <devshittu@gmail.com>
2024-01-26 22:51:10 +0000

    And upgraded to use the apiClientAuth

--- src/features/users/api/get-users.ts
+++ src/features/users/api/get-users.ts
@@ -5,6 +5,8 @@ import { apiClient } from '@/lib/api-client';
 import { UserListResponse } from '../types';
 import { QUERY_KEYS } from '@/config/query';
 import { URI_USERS } from '@/config/api-constants';
+import { ApiCallResultType, CacheRefType } from '@/types';
+import useApiClientAuth from '@/features/auth/hooks/useApiClientAuth';
 const { GET_USERS } = QUERY_KEYS;
 
 type GetUsersOptions = {
@@ -22,13 +24,21 @@ export const getUsers = ({
 };
 
 export const useGetUsers = ({ params }: GetUsersOptions) => {
+  const apiClientAuth = useApiClientAuth();
+  const queryKey: CacheRefType = [GET_USERS, ApiCallResultType.DISCRETE];
+  const fetchUsers = async ({
+    params,
+  }: GetUsersOptions): Promise<UserListResponse> => {
+    return await apiClientAuth.get(`${URI_USERS}`, { params });
+  };
   const { data, isFetching, isFetched } = useQuery({
-    queryKey: [GET_USERS, params],
-    queryFn: () => getUsers({ params }),
+    queryKey,
+    queryFn: () => fetchUsers({ params }),
     initialData: {} as UserListResponse,
   });
 
   return {
+    queryKey,
     data,
     isLoading: isFetching && !isFetched,
   };
