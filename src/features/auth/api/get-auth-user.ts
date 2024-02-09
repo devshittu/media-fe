adb4531d4df9beabfa9facde7aeb162e03c25001
Muhammed Shittu <devshittu@gmail.com>
2024-02-09 11:44:01 +0000

    Fixed error

--- src/features/auth/api/get-auth-user.ts
+++ src/features/auth/api/get-auth-user.ts
@@ -5,7 +5,7 @@ import { QUERY_KEYS } from '@/config/query';
 import { URI_AUTH_ME } from '@/config/api-constants';
 import { useAuthStore } from '@/stores/auth';
 import { parseError } from '@/utils';
-import useApiClientAuth from '../hooks/useApiClientAuth';
+import { ApiCallResultType, CacheRefType } from '@/types';
 const { AUTH_USER } = QUERY_KEYS;
 
 export const getAuthUser = async (): Promise<AuthUser> => {
@@ -13,36 +13,31 @@ export const getAuthUser = async (): Promise<AuthUser> => {
 };
 
 export const useAuthUser = () => {
-  const apiClientAuth = useApiClientAuth();
-
-  const fetchAuthUser = async () => {
-    const response = await apiClientAuth.get(`${URI_AUTH_ME}`);
-    return response.data;
-  };
+  const queryKey: CacheRefType = [AUTH_USER, ApiCallResultType.SINGLE];
 
   const { accessToken, setAccessToken, authUserDetails, setAuthUserDetails } =
     useAuthStore();
 
   const { data, isLoading, error } = useQuery({
-    queryKey: [AUTH_USER],
-    queryFn: () => fetchAuthUser(),
+    queryKey,
+    queryFn: () => getAuthUser(),
     enabled: !!accessToken, // Only run if we have an access token
-    onSuccess: (data) => setAuthUserDetails(data),
-    onError: (error: any) => {
-      console.error(
-        'authdebug: Error fetching authenticated user information: error: ',
-        error,
-      );
-      // Handle the error here if needed
-      const parsedError = parseError(error);
-      console.error(
-        'Error fetching authenticated user information: ',
-        parsedError,
-      );
-      throw new Error(
-        error.response?.data?.message || 'Failed to fetch auth user',
-      );
-    },
+    // onSuccess: (data: AuthUser) => setAuthUserDetails(data),
+    // onError: (error: any) => {
+    //   console.error(
+    //     'authdebug: Error fetching authenticated user information: error: ',
+    //     error,
+    //   );
+    //   // Handle the error here if needed
+    //   const parsedError = parseError(error);
+    //   console.error(
+    //     'Error fetching authenticated user information: ',
+    //     parsedError,
+    //   );
+    //   throw new Error(
+    //     error.response?.data?.message || 'Failed to fetch auth user',
+    //   );
+    // },
   });
 
   return { data, isLoading, error };
