9e1f9158fab444e4a97a000ab281745e4f2e52ca
Muhammed Shittu <devshittu@gmail.com>
2024-02-09 11:29:22 +0000

    Removed unneeded comments and codes

--- src/components/frames/stories-page-frame.tsx
+++ src/components/frames/stories-page-frame.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { SidePanel } from '@/components/blocks/side-panel/side-panel';
 import { HashtagList } from '@/features/hashtags';
-import { UserList, UserSuggestionList } from '@/features/users/components';
+import { UserSuggestionList } from '@/features/users/components';
 import { PaneConfig } from '../blocks/side-panel/types';
 import { TrendsList } from '@/features/trends/components/blocks/trends-list';
 
