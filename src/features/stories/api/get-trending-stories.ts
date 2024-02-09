21c971873de3b1987c04f3122a3edccfbb113f81
Muhammed Shittu <devshittu@gmail.com>
2024-02-09 12:12:02 +0000

    Upgraded the react-query syntax to 5.17

--- src/features/stories/api/get-trending-stories.ts
+++ src/features/stories/api/get-trending-stories.ts
@@ -1,17 +1,12 @@
-import {
-  useInfiniteQuery,
-  useQuery,
-  useQueryClient,
-} from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
 import { apiClient } from '@/lib/api-client';
 
-import { StoriesQueryParams, Story, StoryListResponse } from '../types';
+import { StoriesQueryParams, StoryListResponse } from '../types';
 import { QUERY_KEYS } from '@/config/query';
 import { URI_TRENDING_STORIES } from '@/config/api-constants';
 import { InfiniteStoriesResponse, StoryAction } from '../components';
 import { ApiCallResultType, CacheRefType } from '@/types';
-import useApiClientAuth from '@/features/auth/hooks/useApiClientAuth';
 const { GET_TRENDING_STORIES } = QUERY_KEYS;
 
 type GetStoriesOptions = {
@@ -28,19 +23,14 @@ export const getTrendingStories = ({
 };
 
 export const useTrendingStories = ({ params }: GetStoriesOptions) => {
-  const apiClientAuth = useApiClientAuth();
   const queryKey: CacheRefType = [
     GET_TRENDING_STORIES,
     ApiCallResultType.DISCRETE,
   ];
 
-  const fetchTrendingStories = async ({ params }: GetStoriesOptions): Promise<StoryListResponse> => {
-    return await apiClientAuth.get(`${URI_TRENDING_STORIES}`, { params });
-  };
   const { data, isFetching, isFetched } = useQuery({
     queryKey,
-    // queryFn: () => getTrendingStories({ params }),
-    queryFn: () => fetchTrendingStories({ params }),
+    queryFn: () => getTrendingStories({ params }),
     // enabled: !!params?.category_id,
     initialData: {} as StoryListResponse,
   });
@@ -56,21 +46,10 @@ export const useInfiniteTrendingStories = ({
   params,
   initialData,
 }: GetStoriesOptions): InfiniteStoriesResponse => {
-  const apiClientAuth = useApiClientAuth();
   const queryKey: CacheRefType = [
     GET_TRENDING_STORIES,
     ApiCallResultType.INFINITE,
   ];
-
-  const fetchInfiniteTrendingStories = async ({
-    pageParam = 1,
-  }): Promise<StoryListResponse> => {
-    const response = await apiClientAuth.get(`${URI_TRENDING_STORIES}`, {
-      params: { ...params, page: pageParam },
-    });
-    return response as any;
-  };
-
   const {
     data,
     fetchNextPage,
@@ -78,25 +57,26 @@ export const useInfiniteTrendingStories = ({
     isFetchingNextPage,
     isFetched,
     isFetching,
-  } = useInfiniteQuery<StoryListResponse>(
+  } = useInfiniteQuery<StoryListResponse>({
     queryKey,
-    fetchInfiniteTrendingStories,
-    // async ({ pageParam = 1 }) =>
-    //   await getTrendingStories({ params: { ...params, page: pageParam } }),
-    {
-      getNextPageParam: (lastPage, allPages) => {
-        // Check if there are more pages to load
-        if (lastPage?.current_page < lastPage?.total_pages) {
-          return lastPage.current_page + 1;
-        }
-        return undefined; // No more pages
-      },
-      // Keep data fresh for 5 minutes
-      staleTime: 1000 * 60 * 5,
-      // Keep data in cache for 10 minutes
-      cacheTime: 1000 * 60 * 10,
+    queryFn: async ({ pageParam = 1 }) => {
+      // Assert pageParam as number before using it
+      const page = pageParam as number;
+      const response = await getTrendingStories({
+        params: { ...params, page },
+      });
+      return response;
     },
-  );
+
+    getNextPageParam: (lastPage, allPages) => {
+      // Check if there are more pages to load
+      if (lastPage?.current_page < lastPage?.total_pages) {
+        return lastPage.current_page + 1;
+      }
+      return undefined; // No more pages
+    },
+    initialPageParam: 1,
+  });
   // Extract count from the first page
   const count = data?.pages[0]?.count;
   return {
