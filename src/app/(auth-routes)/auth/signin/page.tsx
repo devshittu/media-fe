8adf68e2180c3f17f437931bd65b55779fbcc8dd
Muhammed Shittu <devshittu@gmail.com>
2024-08-14 21:02:14 +0100

    Refactored component to use the new App Router in Next.js 14, moving data fetching to a client component while keeping layout as a server component.

---
+++ src/app/(auth-routes)/auth/signin/page.tsx
@@ -0,0 +1,52 @@
+// import { useRouter } from 'next/router';
+import { SEO } from '@/components/seo';
+import { SigninForm, SigninSection } from '@/features/auth';
+import { NotificationType, useNotifications } from '@/stores/notifications';
+
+const SigninPage = () => {
+  // const router = useRouter();
+  // const { showNotification } = useNotifications();
+
+  // const schemaOrgJSONLD = {
+  //   '@context': 'http://schema.org',
+  //   '@type': 'NewsArticle',
+  //   headline: 'Home',
+  //   description: 'Home page',
+  //   // ... other structured data properties
+  // };
+
+  // const onSuccess = () => {
+  //   showNotification({
+  //     type: NotificationType.SUCCESS,
+  //     title: 'Success',
+  //     duration: 5000,
+  //     message: 'Sign in success!',
+  //   });
+  //   console.log('Sign in success', 'redirect');
+  //   const redirect = router.query.redirect as string;
+  //   router.replace(redirect || '/stories');
+  // };
+
+  return (
+    <>
+      {/* <SEO
+        title="Signup"
+        description="Signup page"
+        schemaOrgJSONLD={schemaOrgJSONLD}
+        canonicalUrl={'/auth/signup'}
+      /> */}
+      <div className="container mx-auto flex flex-col px-5 py-14 justify-center items-center">
+        {/* <SigninForm onSuccess={onSuccess} /> */}
+        <SigninSection />
+      </div>
+    </>
+  );
+};
+
+// SigninPage.getLayout = function getLayout(
+//   page: ReactElement
+// ) {
+//   return <AuthLayout title="Log In">{page}</AuthLayout>;
+// };
+
+export default SigninPage;
