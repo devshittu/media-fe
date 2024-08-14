59923571b15623c25151cfe9175a9f7381141530
Muhammed Shittu <devshittu@gmail.com>
2024-08-14 06:48:52 +0100

    Refactored component to use the new App Router in Next.js 14, moving data fetching to a client component while keeping layout as a server component.

--- src/components/blocks/tour/tour-sequence.tsx
+++ src/components/blocks/tour/tour-sequence.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect, useState } from 'react';
 import TourBubble from './tour-bubble';
 import { TourPopperType } from './tour-popper';
--- src/components/blocks/tour/tour.tsx
+++ src/components/blocks/tour/tour.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState } from 'react';
 import TourSequence from './tour-sequence';
 import { TourPopperType } from './tour-popper';
