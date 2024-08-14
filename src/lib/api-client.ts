3874427799c8d7957a9c03f1f5773a78b8382956
Muhammed Shittu <devshittu@gmail.com>
2024-08-14 07:16:09 +0100

    Modify for Next.js 14 using the App Router and Server Component architecture.

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,4 +1,4 @@
-import Axios from 'axios';
+import Axios, { AxiosResponse } from 'axios';
 import { NotificationType, notificationsStore } from '@/stores/notifications';
 import { AuthStore } from '@/stores/auth';
 import {
@@ -8,22 +8,25 @@ import {
 import getConfig from 'next/config';
 import { handleLogoutAndRedirect, handleTokenRefresh } from '@/utils';
 import { ErrorCode } from '@/config/error-codes';
+import logger from '@/utils/logger';
+// import { auth } from '@/auth';
+import nookies from 'nookies';
+import { signOut } from 'next-auth/react';
+// import { useRouter } from 'next/navigation';
 
-// Get our configuration of our runtimes
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
 // Use the correct url depending on if it's server or public
-const apiUrl = serverRuntimeConfig.apiUrl || publicRuntimeConfig.apiUrl;
 
-export const refresherApiClient = Axios.create({
-  baseURL: apiUrl,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+const apiUrl =
+  typeof window === 'undefined'
+    ? process.env.NEXT_SERVER_API_URL
+    : process.env.NEXT_PUBLIC_API_URL;
 
-  // Add withCredentials here if you want it to be the default for all requests
-  withCredentials: true,
-});
+// Ensure that apiUrl is available
+if (!apiUrl) {
+  throw new Error('API URL is not defined');
+}
+
 export const apiClient = Axios.create({
   baseURL: apiUrl,
   headers: {
@@ -35,265 +38,178 @@ export const apiClient = Axios.create({
   // withCredentials: true,
 });
 
-export const apiClientAuth = Axios.create({
-  baseURL: apiUrl,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  requiresAuth: true,
-});
+apiClient.interceptors.request.use(
+  async (config) => {
+    if (typeof window !== 'undefined') {
 
-//Path: src/lib/api-client.ts
+      // Get all cookies using nookies
+      const allCookies = nookies.get(null);
+      console.log(`apiClient.interceptors.allCookies: `, allCookies)
+      const accessToken = allCookies['__Dev-mediaapp.access-token'];
+      console.log(`apiClient.interceptors.accessToken: `, accessToken)
 
-// interface QueueItem {
-//   resolve: (token: string) => void; // Accepts string only
-//   reject: (error: any) => void;
-// }
+      if (accessToken && config.requiresAuth) {
+        config.headers['Authorization'] = `Bearer ${accessToken}`;
+      }
+      
+        // if (config.requiresAuth) {
+        //   // Check if requiresAuth flag is set
+        //   // Ensure this only runs in the browser
+        //   try {
+        //     // const session = await auth();  // Fetch the session client-side
+        //     // console.log(`From the fetchClient ${JSON.stringify(session.accessToken)}`);
 
-// let failedQueue: QueueItem[] = [];
-
-// const processQueue = (error: any, token: string | null = null): void => {
-//   failedQueue.forEach((prom) => {
-//     if (error || token === null) {
-//       prom.reject(error || new Error("Token refresh failed"));
-//     } else {
-//       prom.resolve(token); // token is guaranteed to be non-null here
-//     }
-//   });
-
-//   failedQueue = [];
-// };
-
-// apiClient.interceptors.request.use(
-//   async (config) => {
-//     const { accessToken, isRefreshingToken } = AuthStore.getState();
-
-//     if (isRefreshingToken) {
-//       try {
-//         const tokenPromise = new Promise((resolve, reject) => {
-//           failedQueue.push({ resolve, reject });
-//         });
-
-//         const newToken = await tokenPromise;
-//         config.headers['Authorization'] = 'Bearer ' + newToken;
-//         return config;
-//       } catch (error) {
-//         return Promise.reject(error);
-//       }
-//     } else {
-//       if (accessToken) {
-//         config.headers['Authorization'] = `Bearer ${accessToken}`;
-//       }
-//       return config;
-//     }
-//   },
-//   (error) => Promise.reject(error),
-// );
-
-// apiClient.interceptors.response.use(
-//   (response) => response, // Success responses pass through
-//   async (error) => {
-//     const originalRequest = error.config;
-//     const {
-//       accessToken,
-//       expiresAt,
-//       isRefreshingToken,
-//       setIsRefreshingToken,
-//       setAccessToken,
-//     } = AuthStore.getState();
-
-//     // Check if error is due to expired token
-//     if (error.response?.status === 401 && !originalRequest._retry) {
-//       // Check if the token is within the buffer period for refresh and not already refreshing
-//       if (!isRefreshingToken && calculateBufferStatus(expiresAt, 15)) {
-//         // Assuming 15% as buffer
-//         setIsRefreshingToken(true);
-//         originalRequest._retry = true; // Mark the request for retry
-
-//         try {
-//           const response = await refreshToken(); // Implement this to actually refresh the token
-//           const newAccessToken = response?.access_token ?? null; // Ensures string or null
-//           const newAccessTokenExpiresAt =
-//             response?.access_token_expires_at ??
-//             DEFAULT_ACCESS_TOKEN_KEY_EXPIRES_AT; // Ensures number or null
-
-//           // Update the store with the new token and expiry time
-//           setAccessToken(newAccessToken, newAccessTokenExpiresAt);
-//           AuthStore.getState().setIsRefreshingToken(false);
-
-//           // Update the authorization header for the original request
-//           originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-//           processQueue(null, newAccessToken); // Process the queue with the new token
-//           // return apiClient(originalRequest);
-//           return apiClient(originalRequest); // Retry the original request with the new token
-//         } catch (refreshError) {
-//           processQueue(refreshError, null); // Process the queue with the error
-
-//         console.error('Failed to refresh token:', refreshError);
-//         setIsRefreshingToken(false);
-//         AuthStore.getState().clearAuth();
-//         // Router.push('/auth/signin');
-//           return Promise.reject(refreshError); // Refresh failed, reject the promise
-//         }
-//       } else if (isRefreshingToken) {
-//         // If the token is already refreshing, queue this request
-//         return new Promise((resolve, reject) => {
-//           failedQueue.push({
-//             resolve: (token: string) => {
-//               originalRequest.headers['Authorization'] = `Bearer ${token}`;
-//               resolve(apiClient(originalRequest));
-//             },
-//             reject,
-//           });
-//         });
-//       }
-//     }
-
-//     // For errors not related to token expiration, just reject the promise
-//     return Promise.reject(error);
-//   },
-// );
-
-apiClient.interceptors.request.use((config) => {
-  if (config.requiresAuth) {
-    // Check if requiresAuth flag is set
-    const token = AuthStore.getState().accessToken;
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+        //     // if (session?.accessToken) {
+        //     //   config.headers['Authorization'] = `Bearer ${session.accessToken}`;
+        //     // }
+        //   } catch (error) {
+        //     console.error('Error fetching session for authorization', error);
+        //     // Handle the error if session fetching fails
+        //   }
+        // }
     }
-    // Set withCredentials for requests that require authentication
-    config.withCredentials = true;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
 
+
+// Response interceptor (if needed)
 apiClient.interceptors.response.use(
-  (response) => response.data,
+  (response: AxiosResponse) => response.data,
   async (error) => {
+
     const originalRequest = error.config;
     const errorCode = error.response?.data?.error?.code;
+    // const router = useRouter();
 
-    // Check if the URL contains the path for refresh token and logout endpoints
-    const isRefreshTokenUrl = originalRequest.url.includes(
-      URI_AUTH_TOKEN_REFRESH,
-    );
-    const isLogoutUrl = originalRequest.url.includes(URI_AUTH_LOGOUT);
 
     // Handle specific error code 'token_not_provided'
     if (errorCode === ErrorCode.TokenNotProvided) {
       // If the error is not from the refresh token or logout endpoints
-      if (!isRefreshTokenUrl && !isLogoutUrl) {
-        // Quietly refresh the token and retry the original request
-        return handleTokenRefresh(originalRequest);
-      } else {
-        // If the error is from the refresh token or logout endpoints, handle logout
-        handleLogoutAndRedirect();
-        return Promise.reject(error);
-      }
+      console.log('when code is:', errorCode, error.response)  // Check for specific token errors and sign out
+      
+        await signOut({});
+        // router.push('/auth/signin');
+      
     }
-    // if (
-    //   (errorCode === ErrorCode.InvalidAccessToken ||
-    //     errorCode === ErrorCode.AuthCredentialNotProvided ||
-    //     errorCode === ErrorCode.TokenNotProvided) &&
-    //   (!isRefreshTokenUrl || !isLogoutUrl)
-    // ) {
-    //   console.error(
-    //     `(errorCode === ErrorCode.TokenNotProvided) = ${
-    //       errorCode === ErrorCode.TokenNotProvided
-    //     }
-    //     !isRefreshTokenUrl || !isLogoutUrl`,
-    //   );
-    //   // Handle the specific case of 'token_not_provided' during token refresh
-    //   originalRequest._retry = true; // Set the retry flag
-    //   // const { setIsRefreshingToken } = AuthStore.getState();
-
-    //   // setIsRefreshingToken(false);
-    //   return handleTokenRefresh(originalRequest);
-    //   // return Promise.reject(error);
-    // }
-
-    // // Check if the error is 'token_not_provided' during a token refresh operation
-    // if (errorCode === ErrorCode.TokenNotProvided && isRefreshTokenUrl) {
-    //   // Handle the specific case of 'token_not_provided' during token refresh
-    //   AuthStore.getState().clearAuth();
-    //   Router.push('/auth/signin');
-    //   notificationsStore.getState().showNotification({
-    //     type: NotificationType.ERROR,
-    //     title: 'Session Expired',
-    //     message: 'Please log in again.',
-    //     duration: 5000,
-    //   });
-    //   return Promise.reject(error);
-    // }
-
-    // Prevent retrying the refresh token and logout endpoints
-    if (isRefreshTokenUrl || isLogoutUrl) {
-      console.error(
-        `isRefreshTokenUrl || isLogoutUrl: stop redirecting to ${'another page'}`,
-      );
-      if (errorCode === ErrorCode.TokenNotProvided) {
-        console.log('authdebug: Token not provided');
-      }
-
-      return Promise.reject(error);
-    }
-
-    // Check for expired access token error code
-    // if code is "invalid_access_token" access token is invalid, return then retry refresh the token it means it is expired
-
-    // Check for the error code
-    // if the code returned is "auth_credential_not_provided",
-    // it means the access token was not provided
-    // then try using the refresh token to get a new access and retry
-    // only after then and it is unsuccessful then,
-    //redirect to the '/' for user name and password input from the user.
-    // Provide helpful message
-
-    // Check for expired or invalid access token error code
-    if (
-      (errorCode === ErrorCode.InvalidAccessToken ||
-        errorCode === ErrorCode.AuthCredentialNotProvided) &&
-      !originalRequest._retry
-    ) {
-      originalRequest._retry = true; // Set the retry flag
-      return handleTokenRefresh(originalRequest);
-    }
-
-    // Check for refresh token error code
-    // if the code returned is "token_not_provided",
-    // it means the refresh token was not provided
-    // then redirect to the '/' for user name and password input from the user.
-    // Provide helpful message
-    // If it's a retry and still fails, or if the token was not provided, handle the error without retrying
-
-    if (originalRequest._retry || errorCode === ErrorCode.TokenNotProvided) {
-      handleLogoutAndRedirect();
-      return Promise.reject(error);
-    }
-
-    let message = error.response?.data?.message || error.message;
-
-    // Check for status code 400 and specific error structure
-    if (
-      error.response &&
-      error.response.status === 400 &&
-      error.response.data.error &&
-      error.response.data.error.detail
-    ) {
-      message = error.response.data.error.detail[0];
-    }
-
-    notificationsStore.getState().showNotification({
-      type: NotificationType.ERROR,
-      title: 'Error',
-      duration: 5000,
-      message,
-    });
-    if (error.response?.data) {
-      return Promise.reject(error.response.data);
-    }
+    // Handle response errors here
     return Promise.reject(error);
-  },
+  }
 );
+
+//Path: src/lib/api-client.ts
+
+// apiClient.interceptors.request.use((config) => {
+  // if (config.requiresAuth) {
+  //   // Check if requiresAuth flag is set
+  //   const token = AuthStore.getState().accessToken;
+  //   if (token) {
+  //     config.headers['Authorization'] = `Bearer ${token}`;
+  //   }
+  //   // Set withCredentials for requests that require authentication
+  //   config.withCredentials = true;
+  // }
+//   return config;
+// });
+
+// apiClient.interceptors.response.use(
+//   (response) => response.data,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     const errorCode = error.response?.data?.error?.code;
+
+//     // Check if the URL contains the path for refresh token and logout endpoints
+//     const isRefreshTokenUrl = originalRequest.url.includes(
+//       URI_AUTH_TOKEN_REFRESH,
+//     );
+//     const isLogoutUrl = originalRequest.url.includes(URI_AUTH_LOGOUT);
+
+//     // Handle specific error code 'token_not_provided'
+//     if (errorCode === ErrorCode.TokenNotProvided) {
+//       // If the error is not from the refresh token or logout endpoints
+//       if (!isRefreshTokenUrl && !isLogoutUrl) {
+//         // Quietly refresh the token and retry the original request
+//         return handleTokenRefresh(originalRequest);
+//       } else {
+//         // If the error is from the refresh token or logout endpoints, handle logout
+//         handleLogoutAndRedirect();
+//         return Promise.reject(error);
+//       }
+//     }
+
+//     // Prevent retrying the refresh token and logout endpoints
+//     if (isRefreshTokenUrl || isLogoutUrl) {
+//       console.error(
+//         `isRefreshTokenUrl || isLogoutUrl: stop redirecting to ${'another page'}`,
+//       );
+//       if (errorCode === ErrorCode.TokenNotProvided) {
+//         console.log('authdebug: Token not provided');
+//       }
+
+//       return Promise.reject(error);
+//     }
+
+//     // Check for expired access token error code
+//     // if code is "invalid_access_token" access token is invalid, return then retry refresh the token it means it is expired
+
+//     // Check for the error code
+//     // if the code returned is "auth_credential_not_provided",
+//     // it means the access token was not provided
+//     // then try using the refresh token to get a new access and retry
+//     // only after then and it is unsuccessful then,
+//     //redirect to the '/' for user name and password input from the user.
+//     // Provide helpful message
+
+//     // Check for expired or invalid access token error code
+//     if (
+//       (errorCode === ErrorCode.InvalidAccessToken ||
+//         errorCode === ErrorCode.AuthCredentialNotProvided) &&
+//       !originalRequest._retry
+//     ) {
+//       originalRequest._retry = true; // Set the retry flag
+//       return handleTokenRefresh(originalRequest);
+//     }
+
+//     // Check for refresh token error code
+//     // if the code returned is "token_not_provided",
+//     // it means the refresh token was not provided
+//     // then redirect to the '/' for user name and password input from the user.
+//     // Provide helpful message
+//     // If it's a retry and still fails, or if the token was not provided, handle the error without retrying
+
+//     if (originalRequest._retry || errorCode === ErrorCode.TokenNotProvided) {
+//       handleLogoutAndRedirect();
+//       return Promise.reject(error);
+//     }
+
+//     let message = error.response?.data?.message || error.message;
+
+//     // Check for status code 400 and specific error structure
+//     if (
+//       error.response &&
+//       error.response.status === 400 &&
+//       error.response.data.error &&
+//       error.response.data.error.detail
+//     ) {
+//       message = error.response.data.error.detail[0];
+//     }
+
+//     notificationsStore.getState().showNotification({
+//       type: NotificationType.ERROR,
+//       title: 'Error',
+//       duration: 5000,
+//       message,
+//     });
+//     if (error.response?.data) {
+//       return Promise.reject(error.response.data);
+//     }
+//     return Promise.reject(error);
+//   },
+// );
+
+
 
 //Path: src/lib/api-client.ts
