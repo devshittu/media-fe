52e9be0d65d7d96243630b8c9d20c17bf4cf5985
Muhammed Shittu <devshittu@gmail.com>
2024-08-14 21:10:49 +0100

    Refactored component to use the new App Router in Next.js 14, moving data fetching to a client component while keeping layout as a server component.

---
+++ src/app/(protected-routes)/bookmarks/_components/BookmarkContent.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { BookmarkList } from '@/features/bookmarks/components';
+import { Bookmark, BookmarkCategory, useInfiniteBookmarks } from '@/features/bookmarks';
+import { useTabContentManager } from '@/components/blocks/tab';
+import { useBookmarkPageTabs } from '@/stores/tabs';
+import React from 'react';
+
+type BookmarkContentProps = {
+  bookmarks?: Bookmark[];
+  queryParams: Record<string, any>;
+};
+
+export default function BookmarkContent({ bookmarks, queryParams }: BookmarkContentProps) {
+  const tabsConfig = {
+    save: {
+      content: (
+        <BookmarkList
+          useBookmarksHook={useInfiniteBookmarks}
+          queryParams={{ ...queryParams, category: BookmarkCategory.Save }}
+          loadMoreOnScroll
+        />
+      ),
+      fetchData: async () => {
+        console.log('Fetching data for Save tab');
+      },
+    },
+    'read-later': {
+      content: (
+        <BookmarkList
+          useBookmarksHook={useInfiniteBookmarks}
+          queryParams={{ ...queryParams, category: BookmarkCategory.ReadLater }}
+          loadMoreOnScroll
+        />
+      ),
+      fetchData: async () => {
+        console.log('Fetching data for Read Later tab');
+      },
+    },
+    favorite: {
+      content: (
+        <BookmarkList
+          useBookmarksHook={useInfiniteBookmarks}
+          queryParams={{ ...queryParams, category: BookmarkCategory.Favorites }}
+          loadMoreOnScroll
+        />
+      ),
+      fetchData: async () => {
+        console.log('Fetching data for Favorites tab');
+      },
+    },
+  };
+
+  const { handleScroll, contentRef, renderTabContent } = useTabContentManager(
+    useBookmarkPageTabs,
+    tabsConfig,
+  );
+
+  return (
+    <div ref={contentRef} onScroll={handleScroll} className="tab-content">
+      {renderTabContent()}
+    </div>
+  );
+}
