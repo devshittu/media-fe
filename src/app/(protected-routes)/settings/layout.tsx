b6e82b91d1e50fda2195927491272a14f04b386e
Muhammed Shittu <devshittu@gmail.com>
2024-08-23 00:25:40 +0100

    Refactored SettingsSection to Next.js 14 with server-side rendering and dynamic client-side section rendering.

--- src/app/(protected-routes)/settings/layout.tsx
+++ src/app/(protected-routes)/settings/layout.tsx
@@ -1,11 +1,23 @@
 import { ReactNode } from 'react';
 import UserLayout from '@/layouts/user-layout';
 import { StoriesPageFrame } from '@/components/frames';
+import { PaneConfig } from '@/components/blocks/side-panel/types';
+import { SettingsLinks } from '@/features/settings';
 
 export default function StoriesLayout({ children }: { children: ReactNode }) {
+
+const sidePanelSections: PaneConfig[] = [
+  {
+    id: 'settings-links',
+    title: 'Settings',
+    showLink: false,
+    component: <SettingsLinks />,
+  },
+];
+
   return (
     <UserLayout>
-      <StoriesPageFrame>{children}</StoriesPageFrame>
+      <StoriesPageFrame sidePanelSections={sidePanelSections}>{children}</StoriesPageFrame>
     </UserLayout>
   );
 }
