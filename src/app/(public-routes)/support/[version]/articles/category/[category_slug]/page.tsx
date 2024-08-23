e7fe824a3610fdeac42562a7d9110d738653817a
Muhammed Shittu <devshittu@gmail.com>
2024-08-23 19:15:09 +0100

    Expose metadata to properly title the page for SEO foundation

--- src/app/(public-routes)/support/[version]/articles/category/[category_slug]/page.tsx
+++ src/app/(public-routes)/support/[version]/articles/category/[category_slug]/page.tsx
@@ -3,7 +3,41 @@ import PublicLayout from '@/layouts/public-layout';
 import { LegalURIParams } from '@/features/support/api/get-legal-document';
 import { APP_SUPPORT_VERSION } from '@/config/constants';
 import SupportArticleListPageContent from '@/app/(public-routes)/support/_components/SupportArticleListPageContent';
+import { Metadata, ResolvingMetadata } from 'next';
+import { getSupportArticleBySlug } from '@/features/support';
 
+ 
+type PageProps = {
+  params: { id: string, version: string; category_slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+ 
+export async function generateMetadata(
+  { params, searchParams }: PageProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+
+  const { version, category_slug } = params;
+
+  // Fetch the support article using the slug and version
+  const article = await getSupportArticleBySlug({
+    params: { version, document: 'article', slug: category_slug },
+  });
+
+  console.log('Fetched Article: ',article);
+
+  // Generate metadata using the article data
+  return {
+    title: article.title || 'Support Article',
+    description: article.summary || 'Support article details.',
+    openGraph: {
+      title: article.title,
+      description: article.summary,
+      // images: article.images || [],
+    },
+  };
+}
 export default function SupportArticleListPage({
   params,
 }: {
@@ -23,3 +57,6 @@ export default function SupportArticleListPage({
 SupportArticleListPage.getLayout = function getLayout(page: ReactElement) {
   return <PublicLayout>{page}</PublicLayout>;
 };
+
+
+// src/app/(public-routes)/support/[version]/articles/category/[category_slug]/page.tsx
