e3e9635f1e24267b1a9b99afed8f4248a3b8c827
Muhammed Shittu <devshittu@gmail.com>
2024-08-27 04:13:18 +0100

    Modify not-found

--- src/components/not-found/not-found.tsx
+++ src/components/not-found/not-found.tsx
@@ -1,31 +1,105 @@
+
+'use client';
+import { Button } from '@/components/button';
 import React from 'react';
 import { Link } from '../labs/typography';
 
-export const NotFound = () => {
+export type NotFoundProps = {
+  errorCode?: string;
+  title?: string;
+  subtitle?: string;
+  description?: string;
+  primaryAction?: {
+    label: string;
+    href?: string;
+    onClick?: () => void;
+  };
+  secondaryAction?: {
+    label: string;
+    href?: string;
+    onClick?: () => void;
+  };
+};
+
+export const NotFound: React.FC<NotFoundProps> = ({
+  errorCode = '404',
+  title = `Sorry, we couldn't find this page.`,
+  subtitle = 'Error',
+  description = 'But don’t worry, you can find plenty of other things on our homepage.',
+  primaryAction = {
+    label: 'Back to homepage',
+    href: '/',
+  },
+  secondaryAction,
+}) => {
+
   return (
-    <>
-      <section className="flex items-center h-full p-16 dark:bg-slate-900 dark:text-slate-100">
-        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-          <div className="max-w-md text-center">
-            <h2 className="mb-8 font-extrabold text-9xl dark:text-slate-600">
-              <span className="sr-only">Error</span>404
-            </h2>
-            <p className="text-2xl font-semibold md:text-3xl">
-              {`Sorry, we couldn't find this page.`}
-            </p>
-            <p className="mt-4 mb-8 dark:text-slate-400">
-              But dont worry, you can find plenty of other things on our
-              homepage.
-            </p>
+    <section className="flex items-center h-full p-16 dark:bg-slate-900 dark:text-slate-100">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-9xl dark:text-slate-600">
+            <span className="sr-only">{subtitle}</span>
+            {errorCode}
+          </h2>
+          <p className="text-2xl font-semibold md:text-3xl">{title}</p>
+          <p className="mt-4 mb-8 dark:text-slate-400">{description}</p>
+          <div className='flex justify-evenly'>
+
+
+          {primaryAction.href ? (
             <Link
-              href="/"
-              className="px-8 py-3 font-semibold rounded dark:bg-cyan-400 dark:text-slate-900"
+              href={primaryAction.href}
+              className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-slate-50 dark:text-slate-900 transition duration-200 shadow-md bg-cyan-400 hover:bg-cyan-700 focus:shadow-outline focus:outline-none"
+              aria-label={primaryAction.label}
+              onClick={primaryAction.onClick}
             >
-              Back to homepage
+              <span className="font-inter font-extrabold">{primaryAction.label}</span>
+              
             </Link>
-          </div>
+          ) : (
+
+            <Button
+              id={`primary-button-error-page`}
+              type="primary"
+              nativeType="button"
+              className="justify-center font-semibold md:h-12"
+              onClick={primaryAction.onClick}
+            >
+              <span className="opacity-100 transition-opacity font-extrabold text-xl font-inter ">
+              {primaryAction.label}
+              </span>
+              </Button>
+          )}
+
+          {secondaryAction &&  (
+            <>
+            <Button
+              id={`secondary-button-error-page`}
+              nativeType="button"
+              className="justify-center font-semibold md:h-12"
+              onClick={secondaryAction.onClick}
+            >
+              <span className="opacity-100 transition-opacity font-extrabold text-xl">
+              {secondaryAction.label}
+              </span>{' '}
+              </Button>
+              
+            {/* <Link
+              href={secondaryAction.href || '/'}
+              className="mt-4 px-8 py-3 font-semibold rounded dark:bg-slate-700 dark:text-slate-100"
+              
+              aria-label={secondaryAction.label}
+            >
+              <span className="opacity-100 transition-opacity font-extrabold text-xl">
+              {secondaryAction.label}
+              </span>
+            </Link> */}
+              </>
+          )}</div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
+
+// src/components/not-found/not-found.tsx
