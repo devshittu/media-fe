df18a02ce553445d1e9b50d67adf6da70c4647ba
Muhammed Shittu <devshittu@gmail.com>
2024-08-27 04:04:41 +0100

    Add metadata info

--- src/app/(auth-routes)/auth/signup/page.tsx
+++ src/app/(auth-routes)/auth/signup/page.tsx
@@ -1,26 +1,18 @@
 
 import React from 'react';
-
 import { SEO } from '@/components/seo';
 import { SignupSection } from '@/features/auth/components/signup-form/signup-section';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Sign Up',
+  description: "Register a new account.",
+  
+}
 const SignupPage = () => {
   
-  const schemaOrgJSONLD = {
-    '@context': 'http://schema.org',
-    '@type': 'NewsArticle',
-    headline: 'Sign up',
-    description: 'Sign up page',
-    // ... other structured data properties
-  };
   return (
     <div className="h-screen">
-      <SEO
-        title="Signup"
-        description="Signup page"
-        schemaOrgJSONLD={schemaOrgJSONLD}
-        canonicalUrl={'/auth/signup'}
-      />
       <div className="container mx-auto flex flex-col px-5 py-14 justify-center items-center">
         <SignupSection />
       </div>
