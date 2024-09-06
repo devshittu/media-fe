03ab40e3c26bbf4c1b65099d7850869e62b2e985
Muhammed Shittu <devshittu@gmail.com>
2024-09-06 22:09:22 +0100

    Apply format

--- src/app/(auth-routes)/auth/signin/page.tsx
+++ src/app/(auth-routes)/auth/signin/page.tsx
@@ -3,10 +3,9 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Sign In',
-  description: "Sign in to your account.",
-}
+  description: 'Sign in to your account.',
+};
 const SigninPage = () => {
-
   return (
     <>
       <div className="container mx-auto flex flex-col px-5 py-14 justify-center items-center">
