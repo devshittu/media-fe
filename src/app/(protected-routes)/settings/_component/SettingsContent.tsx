f9d4542365839d7344553134311b77e58c8245d6
Muhammed Shittu <devshittu@gmail.com>
2024-09-06 22:13:41 +0100

    Apply format

--- src/app/(protected-routes)/settings/_component/SettingsContent.tsx
+++ src/app/(protected-routes)/settings/_component/SettingsContent.tsx
@@ -8,11 +8,16 @@ import { Loading } from '@/components/loading';
 import { useUserSettings } from '@/features/settings/api/get-user-settings';
 import { StoriesPageHeader } from '@/components/blocks/headers';
 import { SettingsFieldLink } from '@/features/settings/components/blocks/settings-field-link';
+import { useSession } from 'next-auth/react';
+import { SettingsFieldLinkSkeleton } from '@/features/settings/components/loading/SettingsFieldLinkSkeleton';
 
 export const SettingsContent = () => {
   const headerRef = useRef<HTMLElement>(null);
+  const { data: session } = useSession();
+
+  const userId = session?.user?.id || undefined;
   const { data: userSettings, isLoading } = useUserSettings({
-    params: { user_id: '1' },
+    params: { user_id: userId },
   });
 
   const openMainMenuDrawer = () => {
@@ -32,51 +37,58 @@ export const SettingsContent = () => {
     drawer.open();
   };
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return (
-    <div
-      className={`flex flex-col flex-shrink-0 basis-auto flex-grow relative p-0 min-w-0 min-h-0 m-0 border-x max-w-full lg:max-w-[640px] box-border border-slate-100 dark:border-slate-800`}
-    >
-      <StoriesPageHeader pageTitle="Settings" />
-      <section>
-        <div className={`mt-28 lg:mt-7`}>
-          <div className={`flex flex-col space-y-10`}>
-            <SettingsFieldLink
-              id={'account-settings-link'}
-              title="Account Settings"
-              linkHref={`/settings/account`}
-              description={`Personal details and contact information associated with the user's account.`}
-            />
-            <SettingsFieldLink
-              id={'notification-settings-link'}
-              title="Notification Settings"
-              linkHref={`/settings/notifications`}
-              description={`Choose which types of notifications you'd like to receive and how.`}
-            />
-            <SettingsFieldLink
-              id={'personal-settings-link'}
-              title="Personal Settings"
-              linkHref={`/settings/personal`}
-              description={`Customize your experience based on your interests.`}
-            />
-            <SettingsFieldLink
-              id={'system-settings-link'}
-              title="System Settings"
-              linkHref={`/settings/system`}
-              description={`Settings related to the system's appearance and language.`}
-            />
-            <SettingsFieldLink
-              id={'security-settings-link'}
-              title="Security Settings"
-              linkHref={`/settings/security`}
-              description={`Settings related to the system's appearance and language.`}
-            />
+    <>
+      <div
+        className={`flex flex-col flex-shrink-0 basis-auto flex-grow relative p-0 min-w-0 min-h-0 m-0 border-x max-w-full lg:max-w-[640px] box-border border-slate-100 dark:border-slate-800`}
+      >
+        <StoriesPageHeader pageTitle="Settings" />
+        <section>
+          <div className={`mt-28 lg:mt-7`}>
+            <div className={`flex flex-col space-y-10`}>
+              {isLoading ? (
+                <>
+                  <SettingsFieldLinkSkeleton />
+                  <SettingsFieldLinkSkeleton />
+                </>
+              ) : (
+                <>
+                  <SettingsFieldLink
+                    id={'account-settings-link'}
+                    title="Account Settings"
+                    linkHref={`/settings/account`}
+                    description={`Personal details and contact information associated with the user's account.`}
+                  />
+                  <SettingsFieldLink
+                    id={'notification-settings-link'}
+                    title="Notification Settings"
+                    linkHref={`/settings/notifications`}
+                    description={`Choose which types of notifications you'd like to receive and how.`}
+                  />
+                  <SettingsFieldLink
+                    id={'personal-settings-link'}
+                    title="Personal Settings"
+                    linkHref={`/settings/personal`}
+                    description={`Customize your experience based on your interests.`}
+                  />
+                  <SettingsFieldLink
+                    id={'system-settings-link'}
+                    title="System Settings"
+                    linkHref={`/settings/system`}
+                    description={`Settings related to the system's appearance and language.`}
+                  />
+                  <SettingsFieldLink
+                    id={'security-settings-link'}
+                    title="Security Settings"
+                    linkHref={`/settings/security`}
+                    description={`Settings related to the system's appearance and language.`}
+                  />
+                </>
+              )}
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 };
