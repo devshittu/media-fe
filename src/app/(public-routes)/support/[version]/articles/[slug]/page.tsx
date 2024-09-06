70fa8d9ad11fe7664f1affa045e07957584cdabc
Muhammed Shittu <devshittu@gmail.com>
2024-09-06 22:28:03 +0100

    Apply format

--- src/app/(public-routes)/support/[version]/articles/[slug]/page.tsx
+++ src/app/(public-routes)/support/[version]/articles/[slug]/page.tsx
@@ -121,3 +121,4 @@ export default async function SupportArticlePage({
 SupportArticlePage.getLayout = function getLayout(page: React.ReactElement) {
   return <PublicLayout>{page}</PublicLayout>;
 };
+// src/app/(public-routes)/support/[version]/articles/[slug]/page.tsx
