ac544e8c22388b0924a89def2903c858ea849061
Muhammed Shittu <devshittu@gmail.com>
2024-09-06 22:09:36 +0100

    Apply format

--- src/app/(auth-routes)/auth/signup/page.tsx
+++ src/app/(auth-routes)/auth/signup/page.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { SEO } from '@/components/seo';
 import { SignupSection } from '@/features/auth/components/signup-form/signup-section';
@@ -6,11 +5,9 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Sign Up',
-  description: "Register a new account.",
-  
-}
+  description: 'Register a new account.',
+};
 const SignupPage = () => {
-  
   return (
     <div className="h-screen">
       <div className="container mx-auto flex flex-col px-5 py-14 justify-center items-center">
