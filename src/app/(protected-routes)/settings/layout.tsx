f4ffc6c367b08e62306e64724f3f233f8ef6e4a3
Muhammed Shittu <devshittu@gmail.com>
2024-09-06 22:11:41 +0100

    Apply format

--- src/app/(protected-routes)/settings/layout.tsx
+++ src/app/(protected-routes)/settings/layout.tsx
@@ -5,19 +5,20 @@ import { PaneConfig } from '@/components/blocks/side-panel/types';
 import { SettingsLinks } from '@/features/settings';
 
 export default function StoriesLayout({ children }: { children: ReactNode }) {
-
-const sidePanelSections: PaneConfig[] = [
-  {
-    id: 'settings-links',
-    title: 'Settings',
-    showLink: false,
-    component: <SettingsLinks />,
-  },
-];
+  const sidePanelSections: PaneConfig[] = [
+    {
+      id: 'settings-links',
+      title: 'Settings',
+      showLink: false,
+      component: <SettingsLinks />,
+    },
+  ];
 
   return (
     <UserLayout>
-      <StoriesPageFrame sidePanelSections={sidePanelSections}>{children}</StoriesPageFrame>
+      <StoriesPageFrame sidePanelSections={sidePanelSections}>
+        {children}
+      </StoriesPageFrame>
     </UserLayout>
   );
 }
