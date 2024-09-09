4c9827f5ffb8fcac415c46ee13a97326b87686cb
Muhammed Shittu <devshittu@gmail.com>
2024-09-09 12:02:33 +0100

    Format code

--- src/components/loading/loading-photo-failed.tsx
+++ src/components/loading/loading-photo-failed.tsx
@@ -9,25 +9,20 @@ type LoadingPhotoFailedProps = {
 export const LoadingPhotoFailed = forwardRef<
   HTMLDivElement,
   LoadingPhotoFailedProps
->(
-  (
-    { className = '', ...props },
-    ref,
-  ) => {
-    return (
-      <div
-        ref={ref}
-        className={`flex items-center justify-center w-full h-full bg-slate-300 dark:bg-slate-700 ${className}`}
-        {...props}
-      >
-        <Icon
-          icon={<CloudOffIcon />}
-          className="w-12 h-12 text-slate-200 dark:text-slate-600"
-        />
-      </div>
-    );
-  },
-);
+>(({ className = '', ...props }, ref) => {
+  return (
+    <div
+      ref={ref}
+      className={`flex items-center justify-center w-full h-full bg-slate-300 dark:bg-slate-700 ${className}`}
+      {...props}
+    >
+      <Icon
+        icon={<CloudOffIcon />}
+        className="w-12 h-12 text-slate-200 dark:text-slate-600"
+      />
+    </div>
+  );
+});
 
 LoadingPhotoFailed.displayName = 'LoadingPhotoFailed';
 
