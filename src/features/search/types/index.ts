c61aea5837de881169ee0e271afd45a6da7f86e9
Muhammed Shittu <devshittu@gmail.com>
2024-09-10 01:45:18 +0100

    Refactored to another search feature location

---
+++ src/features/search/types/index.ts
@@ -0,0 +1,31 @@
+
+import {
+  PaginatedListQueryParams,
+  PaginatedResponse,
+} from '@/types';
+
+export type SearchHistory = {
+  query: string;
+  searched_at: number;
+  hits?: number;
+}
+export type Autocomplete = {
+  story_id: string;
+  title: string;
+  body_excerpt: string;
+  probable_keywords?: string[];
+}
+
+export type SearchHistoryResponse = PaginatedResponse<SearchHistory>;
+export type AutocompleteResponse = PaginatedResponse<Autocomplete>;
+
+export type PaginatedSearchHistoryResponse = {
+  pages: SearchHistoryResponse[];
+  pageParams: number[];
+};
+
+
+
+export type StoriesQueryParams = PaginatedListQueryParams & {
+  q?: string | undefined;
+};
