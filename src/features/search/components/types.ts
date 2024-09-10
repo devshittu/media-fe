abf0d752b3c430ea7a144a427d38f195b37dfe0d
Muhammed Shittu <devshittu@gmail.com>
2024-09-10 01:49:07 +0100

    Refactored to another search feature location

---
+++ src/features/search/components/types.ts
@@ -0,0 +1,39 @@
+import {
+  CacheRefType,
+} from '@/types';
+import {
+  AutocompleteResponse,
+  SearchHistoryResponse,
+} from '../types';
+import { InfiniteData, QueryKey } from '@tanstack/react-query';
+
+
+export type InfiniteSearchHistoryResponse = {
+  queryKey: CacheRefType;
+  data: InfiniteData<SearchHistoryResponse> | undefined;
+  fetchNextPage: () => void;
+  hasNextPage: boolean | undefined;
+  isFetchingNextPage: boolean;
+  isLoading: boolean;
+  count: number;
+  error?: Error | null;
+  refetch?: () => void;
+};
+
+export type InfiniteAutocompleteResponse = {
+  queryKey: CacheRefType;
+  data: InfiniteData<AutocompleteResponse> | undefined;
+  fetchNextPage: () => void;
+  hasNextPage: boolean | undefined;
+  isFetchingNextPage: boolean;
+  isLoading: boolean;
+  count: number;
+  error?: Error | null;
+  refetch?: () => void;
+};
+export type LikeStoryFormData = {
+  story_slug?: string;
+  story_id: number;
+};
+
+// Path: src/features/stories/components/types.ts
