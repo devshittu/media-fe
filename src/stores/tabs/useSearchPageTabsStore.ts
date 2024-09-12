f5754ab154e19dd05993ee0493b51ce82801b88e
Muhammed Shittu <devshittu@gmail.com>
2024-09-12 22:22:08 +0100

    Add useSearchPageTabs

---
+++ src/stores/tabs/useSearchPageTabsStore.ts
@@ -0,0 +1,16 @@
+'use client';
+import { createTabStore, useTabStore } from './hooks';
+
+const SearchPageTabs = createTabStore({
+  initialTabs: [
+    { id: 'searchStories', label: 'Stories' },
+    { id: 'searchStorylines', label: 'Storylines' },
+    // { id: 'saved', label: 'Save' },
+    // Add more tabs as needed
+  ],
+  initialActiveTab: 'searchStories',
+});
+
+export const useSearchPageTabs = () => useTabStore(SearchPageTabs);
+
+//path: src/stores/tabs/useSearchPageTabsStore.ts
