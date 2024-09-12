07188dedb36b122aa5dc3edd57a366da54c02ebf
Muhammed Shittu <devshittu@gmail.com>
2024-09-12 22:20:56 +0100

    Add Search page

---
+++ src/app/(protected-routes)/search/page.tsx
@@ -0,0 +1,54 @@
+import { ReactElement } from 'react';
+import UserLayout from '@/layouts/user-layout';
+import { StoriesPageHeader } from '@/components/blocks/headers';
+import { StoriesPageFrame } from '@/components/frames';
+import { SEO } from '@/components';
+import { Story, getStories, getUserFeedStories } from '@/features/stories';
+import { cleanObject } from '@/utils';
+import { PAGINATE_STORIES_LIMIT } from '@/config/constants';
+import ClientStoriesContent from './_component/ClientStoriesContent'; // Import Client Component
+import { useHomePageTabs } from '@/stores/tabs';
+
+type SearchPageProps = {
+  stories: Story[];
+  userFeed: Story[];
+  error?: string;
+};
+
+export default async function SearchPage() {
+  const queryParams = cleanObject({
+    page: 1,
+    page_size: PAGINATE_STORIES_LIMIT,
+  });
+
+  try {
+    const [userFeedResponse, storiesResponse] = await Promise.all([
+      getUserFeedStories({ params: queryParams }),
+      getStories({ params: queryParams }),
+    ]);
+
+    const props: SearchPageProps = {
+      stories: storiesResponse.results || [],
+      userFeed: userFeedResponse.results || [],
+    };
+
+    return (
+      <>
+        <StoriesPageHeader
+          pageTitle="Search Results"
+          showTab
+          parallax
+          tabStore={useHomePageTabs}
+        />
+        <ClientStoriesContent {...props} />
+      </>
+    );
+  } catch (error) {
+    console.error('Error fetching stories:', error);
+    return (
+      <div>
+        <p>There was an error fetching the stories. Please try again later.</p>
+      </div>
+    );
+  }
+}
