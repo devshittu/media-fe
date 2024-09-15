719745a07fb6cce4e6c352929a45e490d607fe70
Muhammed Shittu <devshittu@gmail.com>
2024-09-15 20:19:15 +0100

    Modify Autocomplete

--- src/features/search/types/index.ts
+++ src/features/search/types/index.ts
@@ -9,12 +9,34 @@ export type SearchHistory = {
   searched_at: number;
   hits?: number;
 }
+// export type Autocomplete = {
+//   story_id: string;
+//   title: string;
+//   body_excerpt: string;
+//   probable_keywords?: string[];
+// }
+
 export type Autocomplete = {
-  story_id: string;
+  slug: string;
+  user: {
+    id: number;
+    username: string;
+  };
+  category: {
+    id: number;
+    title: string;
+  };
+  parent_story: {
+    id: number;
+    title: string;
+  };
   title: string;
-  body_excerpt: string;
-  probable_keywords?: string[];
-}
+  body: string;
+  source_link: string;
+  event_reported_at: string;
+  probable_next_words: string[];
+  story_keywords: string[];
+};
 
 export type SearchHistoryResponse = PaginatedResponse<SearchHistory>;
 export type AutocompleteResponse = PaginatedResponse<Autocomplete>;
