84f6a6752a5029ac50d1dd5a08be42038cb815ae
Muhammed Shittu <devshittu@gmail.com>
2024-09-20 00:19:03 +0100

    Modify SearchPage

--- src/stores/tabs/useSearchPageTabsStore.ts
+++ src/stores/tabs/useSearchPageTabsStore.ts
@@ -4,7 +4,7 @@ import { createTabStore, useTabStore } from './hooks';
 const SearchPageTabs = createTabStore({
   initialTabs: [
     { id: 'searchStories', label: 'Stories' },
-    { id: 'searchStorylines', label: 'Storylines' },
+    // { id: 'searchStorylines', label: 'Storylines' },
     // { id: 'saved', label: 'Save' },
     // Add more tabs as needed
   ],
